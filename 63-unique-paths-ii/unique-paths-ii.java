2c03ed0a5ca74f103f384651003d17e4d3e13330
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-09-06 01:53:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (28.10%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] arr) {
+        int[][] dp= new int[arr.length][arr[0].length];
+        for(int []a:dp){
+            Arrays.fill(a,-1);
+        }
+        return solve(arr,0,0,dp);
+    }
+    public static int solve(int[][] arr, int cr, int cc,int[][]dp){
+        if(cr>=arr.length || cc>=arr[0].length || arr[cr][cc]==1){
+            return 0;
+        }
+        if(cr==arr.length-1 && cc==arr[0].length-1){
+            return 1;
+        }
+        if(arr[cr][cc]==1){
+            dp[cr][cc]=-1;
+        }
+        int path=0;
+        if(dp[cr][cc]!=-1){
+            return dp[cr][cc];
+        }
+        int f= solve(arr,cr+1,cc,dp);
+        int s= solve(arr,cr,cc+1,dp);
+        path=f+s;
+        return dp[cr][cc]=path;   
+    }
+}
