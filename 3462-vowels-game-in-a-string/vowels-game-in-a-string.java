078768b7b57d0ad8f983ade2cd43308cccf679cb
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-09-12 10:07:40 +0530

    Time: 21 ms (12.07%) | Memory: 45.6 MB (45.41%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -9,10 +9,10 @@ class Solution {
         return true;
     }
     public static int vowelCount(String s){
-        String vow="aeiou";
+        // String vow="aeiou";
         int count=0;
         for(int i=0;i<s.length();i++){
-            if(vow.indexOf(s.charAt(i))!=-1){
+            if(s.charAt(i)=='a' || s.charAt(i)=='e'||s.charAt(i)=='i'|| s.charAt(i)=='o' || s.charAt(i)=='u'){
                 count++;
             }
         }
