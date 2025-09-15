3124593a0400cb793c69458e11380b798242027f
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-09-16 00:20:18 +0530

    Time: 2 ms (100.00%) | Memory: 41.3 MB (97.92%) - LeetSync

--- 1403-palindrome-partitioning-iii/palindrome-partitioning-iii.java
+++ 1403-palindrome-partitioning-iii/palindrome-partitioning-iii.java
@@ -1,40 +1,34 @@
 class Solution {
-    int n;
     int[][] palindrome;
-    int[] [] dp;
-
+    int[][] dp; // idx se n tak ki substring ko K parts me divide karne par cost
+    int n;
     public int palindromePartition(String s, int k) {
         n=s.length();
-        palindrome= new int[n][n];
+        palindrome=new int[n][n];
         for(int len=2;len<=n;len++){
             for(int i=0;i<=n-len;i++){
                 int j=i+len-1;
-                
                 palindrome[i][j]=palindrome[i+1][j-1]+(s.charAt(i)==s.charAt(j)? 0:1);
-
             }
         }
         dp= new int[n][k+1];
         for(int [] a:dp){
-            Arrays.fill(a,-1);
+        Arrays.fill(a,-1);
         }
-        return solve(0,k);
-
+        return Solve(0,k);
     }
-    public  int solve(int idx, int k){
+    public int Solve(int idx, int k){
         if(k==1){
-            return palindrome[idx][n-1]; //No Partition -> return cost of making full substr a palindrome
+            return  palindrome[idx][n-1];
         }
         if(dp[idx][k]!=-1){
             return dp[idx][k];
         }
-        int ans=n; // change all chars
-        for(int i=idx;i<=n-k;i++){   // s="abcdef" last index of i for k=3 -> 3
-            int res=palindrome[idx][i];
-            res+=solve(i+1,k-1); // 1 partition done so k-1
-            ans=Math.min(ans,res);
+        int ans=n;
+        for(int i=idx;i<=n-k;i++){
+            int cost=palindrome[idx][i]+Solve(i+1,k-1);
+            ans=Math.min(ans,cost);
         }
-
         return dp[idx][k]=ans;
     }
 }
