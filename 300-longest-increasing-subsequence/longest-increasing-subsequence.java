c366b5129f0c2071927739b61cba65d828e679b4
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-09-17 12:06:18 +0530

    Time: 3 ms (95.72%) | Memory: 44.8 MB (30.54%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,19 +1,35 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int [] dp= new int[nums.length];
-        Arrays.fill(dp,1);
-        for(int i=1;i<nums.length;i++){
-            for(int j=i-1;j>=0;j--){
-                if(nums[j]<nums[i]){
-                    int x= dp[j];
-                dp[i]=Math.max(dp[i],x+1);
-                }
+        return LIS(nums);
+    }
+
+    public static int LIS(int[] arr){
+        int[] dp= new int[arr.length];
+        int len=1;
+        dp[0]=arr[0];
+        for(int i=1;i<dp.length;i++){
+            if(arr[i]>dp[len-1]){
+                dp[len]=arr[i];
+                len++;
+            }else{
+                int idx=BinarySearch(dp,0,len-1,arr[i]);
+                dp[idx]=arr[i];
             }
         }
-        // Arrays.sort(dp);
-        // return dp[dp.length-1];
-
-        return Arrays.stream(dp).max().getAsInt();
+        return len;
+    }
+    public static int BinarySearch(int[] dp, int lo, int hi,int item){
         
+        int idx=0;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(dp[mid]>=item){
+                idx=mid;
+                hi=mid-1;
+            }else{
+                lo=mid+1;
+            }
+        }
+        return idx;
     }
 }
