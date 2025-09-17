8c584e2905c3bc918621c14915b4d790f6b4e648
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-09-18 02:41:03 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (65.97%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,26 +1,29 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] arr) {
-        int[][] dp= new int[arr.length][arr[0].length];
-        for(int []a:dp){
+        int [][] dp= new int[arr.length][arr[0].length];
+        for(int [] a:dp){
             Arrays.fill(a,-1);
         }
-        return solve(arr,0,0,dp);
+        return Paths(arr,0,0,dp);
     }
-    public static int solve(int[][] arr, int cr, int cc,int[][]dp){
-        if(cr>=arr.length || cc>=arr[0].length || arr[cr][cc]==1){
+    public static int Paths(int [][] grid, int cr, int cc, int [][]dp){
+        if(cc>=grid[0].length || cr>=grid.length){
             return 0;
         }
-        if(cr==arr.length-1 && cc==arr[0].length-1){
+        if(grid[cr][cc]==1){
+            return 0;
+        }
+        if(cr==grid.length-1 && cc==grid[0].length-1){
             return 1;
         }
-        
-        int path=0;
         if(dp[cr][cc]!=-1){
             return dp[cr][cc];
         }
-        int f= solve(arr,cr+1,cc,dp);
-        int s= solve(arr,cr,cc+1,dp);
-        path=f+s;
-        return dp[cr][cc]=path;   
+
+        int a=Paths(grid,cr,cc+1,dp);
+        int b=Paths(grid,cr+1,cc,dp);
+        return dp[cr][cc]=a+b;
+        
+
     }
 }
