7a73856d1420b319f9658a940118c7b2abad2522
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-09-26 19:28:16 +0530

    Time: 0 ms (100.00%) | Memory: 45.9 MB (85.59%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,22 +1,45 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int[] ans= new int[2];
-        Arrays.fill(ans,-1);
-        int i=0;
-        for(int j=i;j<nums.length;j++){
-            if(nums[j]==target){
-                ans[0]=j;
-                break;
-            }
-        }
-        
-        int l=nums.length-1;
-        for(int j=l;j>=0;j--){
-            if(nums[j]==target){
-                ans[1]=j;
-                break;
-            }
-        }
+        ans[0]=firstOccurence(nums, target);
+        ans[1]=lastOccurence(nums, target);
         return ans;
+    }
+    public static int firstOccurence(int [] nums,int target){
+        int idx=-1;
+        int lo=0;
+        int hi=nums.length-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid]==target){
+                idx=mid;
+                hi=mid-1;
+            }else if(nums[mid]>target){
+                hi=mid-1;
+            }else{
+                lo=mid+1;
+            }
+        }
+        return idx;
+
+    }
+    public static int lastOccurence(int [] nums, int target){
+        int idx=-1;
+        int lo=0;
+        int hi=nums.length-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid]==target){
+                idx=mid;
+                lo=mid+1;
+            }else if(nums[mid]<target){
+                lo=mid+1;
+            }
+            else{
+                hi=mid-1;
+            }
+        }
+        return idx;
+
     }
 }
