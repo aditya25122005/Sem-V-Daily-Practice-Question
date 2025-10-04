6a3dbb6b5d13c00120775319521e4b2a18c1d757
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-10-04 23:16:14 +0530

    Time: 139 ms (60.29%) | Memory: 41.6 MB (63.17%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,35 +1,32 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        boolean hai= false;
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(board[i][j]==word.charAt(0)){
-                    hai=Dekho(board, word, i, j,0);
-                    if(hai) return true;
-                }
-            }
-        }
-        return hai;
+        return Solve(board,word);
     }
-    public static boolean Dekho(char[][] board, String word,int cr, int cc, int idx){
+    public static boolean Solve(char[][] board,String word){
+        for(int i=0;i<board.length;i++){
+        for(int j=0;j<board[0].length;j++){
+            if(board[i][j]==word.charAt(0)){
+                boolean ans=Find(board,word,i,j,0);
+                if(ans) return true;
+            }
+
+        }
+      }
+      return false;
+    }
+    public static boolean Find(char[][]board, String word, int cr, int cc,int idx){
         if(idx==word.length()){
             return true;
         }
-        if(cc<0 || cc>=board[0].length || cr<0 || cr>=board.length||board[cr][cc] != word.charAt(idx)){
+        if(cr<0 || cr>=board.length || cc<0 || cc>=board[0].length || board[cr][cc]!=word.charAt(idx)){
             return false;
         }
         board[cr][cc]='*';
-
-        if(Dekho(board,word,cr+1,cc,idx+1)) return true;
-        if(Dekho(board,word,cr-1,cc,idx+1)) return true;
-        if(Dekho(board,word,cr,cc+1,idx+1)) return true;
-        if(Dekho(board,word,cr,cc-1,idx+1)) return true;
-        
-
+        if(Find(board,word,cr+1,cc,idx+1)) return true;
+        if(Find(board, word, cr-1, cc, idx+1)) return true;
+        if(Find(board, word, cr, cc+1, idx+1)) return true;
+        if(Find(board, word, cr, cc-1, idx+1)) return true;
         board[cr][cc]=word.charAt(idx);
         return false;
-
-
     }
-    
 }
