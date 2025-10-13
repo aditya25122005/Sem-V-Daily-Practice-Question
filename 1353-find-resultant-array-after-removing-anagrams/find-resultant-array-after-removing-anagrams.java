92c4864c2465f57634782434acba6356a3472a84
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-10-13 19:50:04 +0530

    Time: 7 ms (14.17%) | Memory: 45.4 MB (6.30%) - LeetSync

---
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<String> removeAnagrams(String[] words) {
+        List<String> ll= new ArrayList<>();
+        int i=0;
+        int j=1;
+        ll.add(words[0]);
+        while(i<words.length && j<words.length){
+            if(isAnabelle(words[i],words[j])){
+                j++;
+            }
+            else{
+                ll.add(words[j]);
+                i=j;
+                j++;
+            }
+        }
+        return ll;
+
+
+    }
+    public static boolean isAnabelle(String s1, String s2){
+        HashMap<Character,Integer> map1= new HashMap<>();
+        HashMap<Character,Integer> map2= new HashMap<>();
+        for(char ch:s1.toCharArray()){
+            map1.put(ch,map1.getOrDefault(ch,0)+1);
+        }
+        for(char ch:s2.toCharArray()){
+            map2.put(ch,map2.getOrDefault(ch,0)+1);
+        }
+        return map1.equals(map2);
+
+    }
+}
