964167aa770d6b10e81c32da669b06095c0ac36d
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-10-16 11:46:15 +0530

    Time: 8 ms (6.36%) | Memory: 45.9 MB (7.67%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -0,0 +1,51 @@
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        return CheckBipartite(graph);
+    }
+
+    public  boolean CheckBipartite(int[][] graph){
+        Queue<BipartitePair> q= new LinkedList<>();
+        HashMap<Integer,Integer> visited= new HashMap<>();
+        for(int i=0;i<graph.length;i++){// vtx
+            if(visited.containsKey(i)){
+                continue;
+            }
+            q.add(new BipartitePair(i,0));
+            while(!q.isEmpty()){
+                // 1. remove
+                BipartitePair rp= q.poll();
+                // 2. Ignore if already visited 
+                if(visited.containsKey(rp.vtx)){
+                    if(visited.get(rp.vtx) != rp.dis){
+                        return false;
+                    }
+                    continue;
+                }
+                // 3. Mark Visited
+                visited.put(rp.vtx,rp.dis);
+
+                // 4. Self Work
+
+                // 5. Add unvisited nbrs
+                for(int nbrs:graph[rp.vtx]){
+                    if(!visited.containsKey(nbrs)){
+                        q.add(new BipartitePair(nbrs, rp.dis+1));
+                    }
+                }
+
+            }
+        }
+        return true;
+
+
+    }
+    
+    class BipartitePair{
+        int vtx;
+        int dis;
+        public BipartitePair(int vtx, int dis){
+            this.vtx=vtx;
+            this.dis=dis;
+        }
+    }
+}
