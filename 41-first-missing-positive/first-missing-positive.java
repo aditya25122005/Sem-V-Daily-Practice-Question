23398457ca384c1bd23fec22e885fe19623bf6dd
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-10-17 23:21:53 +0530

    Time: 20 ms (9.41%) | Memory: 61 MB (6.02%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        HashMap<Integer,Integer> map= new HashMap<>();
+        for(int num:nums){
+            if(num>0){
+                map.put(num,map.getOrDefault(num,0)+1);
+            }
+        }
+        int check=1;
+        for(int key:map.keySet()){
+            if(!map.containsKey(check)){
+                return check;
+            }
+            else{
+                check++;
+            }
+        }
+        return check;
+    }
+}
