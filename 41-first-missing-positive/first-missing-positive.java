5b8d619fed3763d6dbc54f32abf0f81d66667eef
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-10-17 23:25:57 +0530

    Time: 17 ms (12.04%) | Memory: 56.1 MB (95.57%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -3,7 +3,7 @@ class Solution {
         HashMap<Integer,Integer> map= new HashMap<>();
         for(int num:nums){
             if(num>0){
-                map.put(num,map.getOrDefault(num,0)+1);
+                map.put(num,1);
             }
         }
         int check=1;
