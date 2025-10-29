e26bc4a28b93666e1b0e36715b5b250ab1ae9239
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-10-29 16:27:55 +0530

    Time: 3 ms (37.50%) | Memory: 45.1 MB (43.59%) - LeetSync

---
+++ 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
+        PriorityQueue<Integer> pq= new PriorityQueue<>(Collections.reverseOrder());
+        if(startFuel>=target){
+            return 0;
+        }
+        int i=0;
+        int stop=0;
+        while(startFuel<target){
+            while(i<stations.length && startFuel>=stations[i][0]){
+                pq.add(stations[i][1]);
+                i++;
+            }
+            if(pq.isEmpty()){
+                return -1;
+            }
+            startFuel+= pq.poll();
+            stop++;
+        }
+        return stop;
+
+        
+    }
+}
