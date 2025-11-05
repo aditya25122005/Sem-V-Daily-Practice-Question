20808edb379f089f64b4288f42177efbc17d8c8c
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-11-05 16:46:54 +0530

    Time: 4 ms (35.83%) | Memory: 45.5 MB (6.06%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,28 +1,42 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] ans= new int[nums1.length];
-        HashMap<Integer,Integer> map= new HashMap<>();
-        int idx=0;
-        for(int num:nums2){
-            map.put(num,idx);
-            idx++;
-        }
-        int k=0;
-        for(int i=0;i<nums1.length;i++){
-            int id=map.get(nums1[i])+1;
+        // int[] ans= new int[nums1.length];
+        // HashMap<Integer,Integer> map= new HashMap<>();
+        // int idx=0;
+        // for(int num:nums2){
+        //     map.put(num,idx);
+        //     idx++;
+        // }
+        // int k=0;
+        // for(int i=0;i<nums1.length;i++){
+        //     int id=map.get(nums1[i])+1;
             
-            int nextGreater=-1;
-            for(int j=id;j<nums2.length;j++){
-                if(nums2[j]>nums1[i]) {
-                    nextGreater = nums2[j];
-                    break;
-                }
+        //     int nextGreater=-1;
+        //     for(int j=id;j<nums2.length;j++){
+        //         if(nums2[j]>nums1[i]) {
+        //             nextGreater = nums2[j];
+        //             break;
+        //         }
                 
-            }
-            ans[k]=nextGreater;
-            k++;
-            }
+        //     }
+        //     ans[k]=nextGreater;
+        //     k++;
+        //     }
         
+        // return ans;
+
+        Stack<Integer> st= new Stack<>();
+        HashMap<Integer,Integer> map= new HashMap<>();
+        for(int num:nums2){
+            while(!st.isEmpty() && st.peek()<num){
+                map.put(st.pop(),num);
+            }
+            st.push(num);
+        }
+        int[] ans= new int[nums1.length];
+        for(int i=0;i<ans.length;i++){
+            ans[i]=map.getOrDefault(nums1[i],-1);
+        }
         return ans;
 
     }
