6e9fe585e9ae91c93e5dabcaa0313f2828a029c3
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-11-14 22:49:59 +0530

    Time: 22 ms (44.30%) | Memory: 77.2 MB (18.86%) - LeetSync

--- 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
@@ -7,9 +7,17 @@ class Solution {
             int er=Q[2];
             int ec=Q[3];
             for(int i=sr;i<=er;i++){
-                for(int j=sc;j<=ec;j++){
-                    ans[i][j]++;
+                ans[i][sc]+=1;
+                if(ec+1<n){
+                    ans[i][ec+1]-=1;
                 }
+            }
+        }
+        for(int i=0;i<n;i++){
+            int pre=0;
+            for(int j=0;j<n;j++){
+                pre+=ans[i][j];
+                ans[i][j]= pre;
             }
         }
         return ans;
