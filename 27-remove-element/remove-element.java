e6f2a23ae8de632a698451a1588be44e1f11eab0
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-11-14 14:47:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (5.61%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,23 +1,10 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        // for(int i=0;i<nums.length;i++){
-        //     if(nums[i]==val){
-        //         i
-        //     }
-        // }
-
         int idx=0;
         int i=0;
         while(i<nums.length){
-            if(nums[i]!=val){
-                nums[idx]=nums[i];
-                idx++;
-                i++;
-            }
-            else{
-
-                i++;
-            }
+            if(nums[i]!=val) nums[idx++]=nums[i];
+            i++;
         }
         return idx;
     }
