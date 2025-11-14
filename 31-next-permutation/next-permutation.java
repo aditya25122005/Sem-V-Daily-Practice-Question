ad38153929a534247113a657fadba778d85d4474
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-11-14 21:49:34 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (7.85%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -2,32 +2,27 @@ class Solution {
     public void nextPermutation(int[] nums) {
         int n= nums.length;
         int i=n-2;
-      
-        
-        while(i >= 0 && nums[i] >= nums[i+1]) {
-                i--;
+        while(i>=0 &&nums[i]>=nums[i+1]){
+            i--;
+        }
+        if(i>=0){
+
+            int j=n-1;
+            while(nums[j]<=nums[i]){
+                j--;
             }
-
-            if(i >= 0) {
-            int j = n - 1;
-            while(nums[j] <= nums[i]) j--;
-
-            // swap
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
         }
-            
-            reverse(nums,i+1,n-1);
+        sort(nums,i+1,n-1);
 
-        }
-        
-    
-    public static void reverse(int[] arr, int i, int j){
+    }
+    public static void sort(int [] nums, int i, int j){
         while(i<j){
-            int temp= arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
+            int temp= nums[j];
+            nums[j]=nums[i];
+            nums[i]=temp;
             i++;
             j--;
         }
