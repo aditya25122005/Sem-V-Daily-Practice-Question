6bc1f326bd5571681de4a337ecc6e466348e05e2
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-11-26 21:26:57 +0530

    Time: 12 ms (13.72%) | Memory: 54.2 MB (24.76%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,37 +1,32 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        if(trust.length==0 && n>1) return -1;
-        HashSet<Integer> hh= new HashSet<>();
-        boolean first=false;
-        int judgeCan=-1;
-        for(int [] a:trust){
-            hh.add(a[0]);
-        }
-        for(int i=1;i<=n;i++){
-            if(!hh.contains(i)){
-                judgeCan=i;
-                first=true;
-            }
-        }
-        if(!first){
+        if(trust.length==0 && n>1){
             return -1;
         }
-        HashMap<Integer,Integer> map= new HashMap<>();
-        for(int[]a:trust){
-            
-            map.put(a[0],a[1]);
-            
+        HashMap<Integer,List<Integer>> map= new HashMap<>();
+        for(int i=1;i<=n;i++){
+            map.put(i,new ArrayList<>());
         }
-        for(int[]a:trust){
-            if(a[1]==judgeCan){
-            map.put(a[0],a[1]);
+        for(int i=0;i<trust.length;i++){
+            int [] a= trust[i];
+            map.get(a[0]).add(a[1]);
+        }
+        for(int i=1;i<=n;i++){
+            if(map.get(i).size()==0 && check(i,map)){
+                return i;
             }
         }
+        return -1;
+    }
+    public static boolean check(int i,HashMap<Integer,List<Integer>> map){ // check if i exists in every relation
         for(int key:map.keySet()){
-            if(key==judgeCan) return -1;
-            if(map.get(key)!=judgeCan) return -1;
+            if(key==i){
+                continue;
+            }
+            if(!map.get(key).contains(i)){
+                return false;
+            }
         }
-        return judgeCan;
-        
+        return true;
     }
 }
