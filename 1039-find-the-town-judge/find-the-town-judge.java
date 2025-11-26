5db54b5290d18a002ba9c69a75729a55d0e48fa8
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-11-26 21:30:02 +0530

    Time: 3 ms (69.45%) | Memory: 53.6 MB (67.51%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,32 +1,16 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        if(trust.length==0 && n>1){
-            return -1;
-        }
-        HashMap<Integer,List<Integer>> map= new HashMap<>();
-        for(int i=1;i<=n;i++){
-            map.put(i,new ArrayList<>());
-        }
+        int [] indegree= new int[n+1];
+        int [] outdegree= new int[n+1];
         for(int i=0;i<trust.length;i++){
-            int [] a= trust[i];
-            map.get(a[0]).add(a[1]);
+            outdegree[trust[i][0]]++;
+            indegree[trust[i][1]]++;
         }
         for(int i=1;i<=n;i++){
-            if(map.get(i).size()==0 && check(i,map)){
+            if(indegree[i]==n-1 && outdegree[i]==0){
                 return i;
             }
         }
         return -1;
-    }
-    public static boolean check(int i,HashMap<Integer,List<Integer>> map){ // check if i exists in every relation
-        for(int key:map.keySet()){
-            if(key==i){
-                continue;
-            }
-            if(!map.get(key).contains(i)){
-                return false;
-            }
-        }
-        return true;
     }
 }
