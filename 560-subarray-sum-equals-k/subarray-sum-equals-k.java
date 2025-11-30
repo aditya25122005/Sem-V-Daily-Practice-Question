0c6549cbce3d2ad87059dc5c7ef5ed051b397db9
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-11-30 17:01:01 +0530

    Time: 1112 ms (14.38%) | Memory: 49.3 MB (8.12%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,22 +1,16 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int[] pre= new int[nums.length];
-        pre[0]=nums[0];
-        for(int i=1;i<nums.length;i++){
-            pre[i]=pre[i-1]+nums[i];
+        int[] pre= new int[nums.length+1];
+        pre[0]=0;
+        for(int i=1;i<=nums.length;i++){
+            pre[i]=pre[i-1]+nums[i-1];
         }
         int c=0;
           // 1  2  3
-          for(int i=0;i<nums.length;i++){
-            for(int j=i;j<nums.length;j++){
-                int curr=0;
-                if(i!=0){
-                curr= pre[j]-pre[i-1];
-                }
-                else{
-                    curr=pre[j];
-                }
-                if(curr==k)c++;
+          for(int i=0;i<pre.length;i++){
+            for(int j=i+1;j<pre.length;j++){
+                if(pre[j]-pre[i]==k) c++;
+                
             }
           }
           return c;
