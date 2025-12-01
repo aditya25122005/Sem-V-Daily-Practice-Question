6618855ed53c5712917c37873870f2870862bfd3
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-12-01 20:13:20 +0530

    Time: 49 ms (16.64%) | Memory: 57.3 MB (20.76%) - LeetSync

--- 630-course-schedule-iii/course-schedule-iii.java
+++ 630-course-schedule-iii/course-schedule-iii.java
@@ -2,20 +2,22 @@ class Solution {
     public int scheduleCourse(int[][] courses) {
         Arrays.sort(courses,(a,b)->a[1]-b[1]);
         PriorityQueue<Integer> pq= new PriorityQueue<>(Collections.reverseOrder());
-        int total_time=0;
+        int Total=0;
         int c=0;
         for(int i=0;i<courses.length;i++){
-            int duration=courses[i][0];
-            int deadline=courses[i][1];
-            total_time += duration;
-            pq.add(duration);
-            if(total_time<=deadline){
+            int dura= courses[i][0];
+            int end= courses[i][1];
+
+            Total+=dura;
+            pq.add(dura);
+            if(Total<=end){
                 c++;
             }
             else{
-                int rm=pq.poll();
-                total_time-=rm;
+                Total-=pq.poll();
+                // c--;
             }
+
         }
         return c;
     }
