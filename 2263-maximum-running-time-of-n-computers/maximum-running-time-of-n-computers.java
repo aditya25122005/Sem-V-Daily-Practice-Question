cffc39e07c82e27b25a94d0d4bdfc8c3f4978ca3
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-12-01 10:38:17 +0530

    Time: 43 ms (6.04%) | Memory: 72.5 MB (46.70%) - LeetSync

---
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.java
@@ -0,0 +1,40 @@
+class Solution {
+    public long maxRunTime(int n, int[] batteries) {
+        Arrays.sort(batteries);
+        long sum=0;
+        for(int num:batteries){
+            sum+= (long)num;
+        }
+        long lo=1;
+        long hi=sum;
+        long ans=0;
+
+        while(lo<=hi){
+            long mid= lo+(hi-lo)/2;
+            if(isPossible(batteries,n,mid)){
+                ans=mid;
+                lo=mid+1;
+            }
+            else{
+                hi=mid-1;
+            }
+        }
+        return ans;
+    }
+    public static boolean isPossible(int[] arr, int comp, long M){
+       long sum=0;
+       for(int num:arr){
+        sum+=Math.min((long)num,M);
+       }
+       if(sum>= comp*M){
+        return true;
+       }
+        
+        return false;
+    }
+}
+// 333
+// 223 ->
+// 113 ->102
+// 102
+// 
