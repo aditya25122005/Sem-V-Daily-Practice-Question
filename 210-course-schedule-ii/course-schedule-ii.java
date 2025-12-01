041bde083653b8632cbde136074d3fe8660ba551
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-12-01 19:57:30 +0530

    Time: 9 ms (14.33%) | Memory: 47.7 MB (13.54%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,57 +1,44 @@
 class Solution {
-    private HashMap<Integer,List<Integer>> map;
-    public int[] findOrder(int num, int[][] req) {
-        map= new HashMap<>();
+    public int[] findOrder(int num, int[][] preq) {
+        int[] ans=new int[num];
+        int idx=0;
+        HashMap<Integer,List<Integer>> map= new HashMap<>();
         for(int i=0;i<num;i++){
             map.put(i,new ArrayList<>());
         }
-        for(int i=0;i<req.length;i++){
-            int v1=req[i][0];
-            int v2=req[i][1];
+        for(int i=0;i<preq.length;i++){
+            int v1= preq[i][0];
+            int v2= preq[i][1];
             map.get(v2).add(v1);
         }
-        List<Integer> ans= topological_sort();
-        int[] arr= new int[ans.size()];
-        for(int i=0;i<ans.size();i++){
-            arr[i]=ans.get(i);
-        }
-        return arr;
-
-    }
-    public int[] indegree(){
-        int [] in= new int[map.size()];
-        for(int v2:map.keySet()){
-            for(int v1:map.get(v2)){ // v2 -> v1
-                in[v1]++;
+        int [] in= new int[num];
+        for(int v1:map.keySet()){
+            for(int v2:map.get(v1)){
+                in[v2]++;
             }
         }
-        return in;
-    }
-    public List<Integer> topological_sort(){
-        int [] in= indegree();
         Queue<Integer> q= new LinkedList<>();
-        List<Integer> ll= new ArrayList<>();
-        int c=0;
         for(int i=0;i<in.length;i++){
             if(in[i]==0){
                 q.add(i);
             }
         }
         while(!q.isEmpty()){
-            int r= q.poll();
-            c++;
-            ll.add(r);
-            for(int nbrs:map.get(r)){
+            int rm=q.poll();
+            ans[idx]=rm;
+            idx++;
+
+            for(int nbrs:map.get(rm)){
                 in[nbrs]--;
                 if(in[nbrs]==0){
                     q.add(nbrs);
                 }
             }
         }
-        if(c==map.size()){
-            return ll;
-        }else{
-            return new ArrayList<>();
+        if(idx==ans.length){
+            return ans;
         }
+        return new int[]{};
+
     }
 }
