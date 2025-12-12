3fd066be4dba54acb578587ef63ababc7aee943c
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-12-12 23:06:23 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (51.25%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -3,32 +3,20 @@ class Solution {
         if(image[sr][sc]==color){
             return image;
         }
+        // image[sr][sc]=color;
         Solve(image,sr,sc,color,image[sr][sc]);
         return image;
     }
     public static void Solve(int[][] arr, int cr, int cc, int color, int startColor){
-
-        Queue<int[]> q= new LinkedList<>();
-        arr[cr][cc]=color;
-        q.add(new int[]{cr,cc});
-        int [][]dir={{-1,0},{1,0},{0,1},{0,-1}};
-        while(!q.isEmpty()){
-            int[] P= q.poll();
-            int r=P[0];
-            int c=P[1];
-            for(int[] d:dir){
-                int nr= r+d[0];
-                int nc= c+d[1];
-                if(nr>=0 && nr<arr.length && nc>=0 && nc<arr[0].length && arr[nr][nc]==startColor){
-                    arr[nr][nc]=color;
-                    q.add(new int[]{nr,nc});
-                }   
-            }
-
-
-
+        if(cr<0 || cc<0 || cr>=arr.length || cc>= arr[0].length || arr[cr][cc]!=startColor){
+            return;
         }
-      
+        arr[cr][cc]= color;
+    
+        Solve(arr,cr+1,cc,color,startColor);
+        Solve(arr,cr,cc+1,color,startColor);
+        Solve(arr,cr-1,cc,color,startColor);
+        Solve(arr,cr,cc-1,color,startColor);
 
     }
 }
