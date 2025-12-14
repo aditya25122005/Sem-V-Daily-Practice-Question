4794fb3876301f5c618e0346e4d7af2ca1190e09
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-12-14 08:49:04 +0530

    Time: 22 ms (62.40%) | Memory: 47.8 MB (53.60%) - LeetSync

--- 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.java
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.java
@@ -1,45 +1,18 @@
 class Solution {
     public int numberOfWays(String corridor) {
-
-        long mod=1000000007;
-        List<Integer> ll= new ArrayList<>();
+        int seat=0;
+        int last=0;
+        long ways=1;
+        int mod=1000000007;
         for(int i=0;i<corridor.length();i++){
             if(corridor.charAt(i)=='S'){
-                ll.add(i);
+                seat++;
+                if(seat>2 && seat%2==1){// It is starting of a new Segment also it is not a first segment
+                    ways=(ways*(i-last))%mod;
+                }
+                last=i;
             }
         }
-        if(ll.size()%2!=0 || ll.size()==0){
-            return 0;
-        }
-        long ways=1;
-        int curr=0;
-        for(int i=1;i<ll.size()-1;i+=2){
-            int plant=0;
-            plant+= (ll.get(i+1)-ll.get(i)-1);
-             ways=(ways*(plant+1))%mod;
-        }
-        return (int)(ways%mod);
-        // int n=corridor.length();
-        // int ways=1;
-        // int mode=0;// start
-        // int lastEnd=0;
-        // for(int i=0;i<corridor.length();i++){
-        //     if(i!=n-1 && corridor.charAt(i)=='S' && mode==0){ // pehli seat
-        //         //
-        //         int plant=0;
-        //         if(lastEnd!=0){
-        //         for(int p=lastEnd+1;p<i;p++){
-        //             if(corridor.charAt(p)=='P')plant++;
-        //         }
-        //         }
-        //         ways*= (plant+1);
-        //         mode=1;// find last seat
-        //     }
-        //     if(corridor.charAt(i)=='S' && mode==1){// Aakhri seat
-        //         mode=0;
-        //         lastEnd=i;
-        //     }
-        // }
-        // return ways;
+        return seat>1 && seat%2!=1 ? (int)ways:0;
     }
 }
