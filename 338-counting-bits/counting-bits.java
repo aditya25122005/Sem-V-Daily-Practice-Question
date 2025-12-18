c697350dd64376e1e3ac2a08d7841692fb88094a
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-12-19 00:11:35 +0530

    Time: 2 ms (96.20%) | Memory: 48.7 MB (32.70%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -9,11 +9,13 @@ class Solution {
     public static int count1(int n){
         int c=0;
         while(n!=0){
-            if((n&1) == 1){
-                c++;
+            n=n&(n-1);
+            c++;
+            // if((n&1) == 1){
+            //     c++;
                
-            }
-            n=n>>1;
+            // }
+            // n=n>>1;
            
         }
         return c;
