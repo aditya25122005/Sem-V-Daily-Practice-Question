c49922059ceb7d428fc3e1dcd009c629e3710e10
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-12-19 16:29:35 +0530

    Time: 67 ms (2.08%) | Memory: 113.7 MB (6.31%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int [][] dp= new int[prices.length][2];
+        for(int []D:dp) Arrays.fill(D,-1);
+        return Solve(prices,0,0,0,dp);
+    }
+    public static int Solve(int[] arr, int idx, int prev, int count, int[][]dp){
+        if(count==1){
+            return 0;
+        }
+        if(idx==arr.length){
+            return 0;
+        }
+        if(dp[idx][prev]!=-1) return dp[idx][prev];
+
+        int a=0;
+        int b=0;
+        int c=0;
+        int d=0;
+
+        if(prev==0){
+            a=-arr[idx]+Solve(arr,idx+1,1,count,dp);
+            b=Solve(arr,idx+1,0,count,dp);
+        }
+        else{
+            c=arr[idx]+Solve(arr,idx+1,0,count+1,dp);
+            d=Solve(arr,idx+1,1,count,dp);
+        }
+        return dp[idx][prev] = Math.max(Math.max(a,b),Math.max(c,d));
+    }
+}
