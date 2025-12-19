a09b259b496674ad6d71817f3614c3b9fe7ed0a0
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-12-19 16:39:35 +0530

    Time: 3 ms (8.75%) | Memory: 49.4 MB (5.08%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,33 +1,29 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[][] dp= new int[prices.length][2];
-        for(int []A:dp){
-            Arrays.fill(A,-1);
-        }
-        return Solve(prices,0,0,dp);
+        int [][] dp= new int[prices.length][2];
+        for(int []D:dp) Arrays.fill(D,-1);
+        return Solve(prices,0,0,0,dp);
     }
+      public static int Solve(int[] arr, int idx, int prev, int count, int[][]dp){
 
-    // prev ->0 Buy
-    // prev ->1 Sell
-    public static int Solve(int [] prices, int idx, int prev, int[][] dp){ 
-        if(idx==prices.length){
+        if(idx==arr.length){
             return 0;
         }
-        if(dp[idx][prev]!=-1){
-            return dp[idx][prev];
-        }
-        int a=0; 
+        if(dp[idx][prev]!=-1) return dp[idx][prev];
+
+        int a=0;
         int b=0;
-        int sell=0;
-        int notSell=0;
-        if(prev==0){// khareedo
-        a= -prices[idx] + Solve(prices,idx+1,1,dp);
-        b=Solve(prices,idx+1,0,dp); // Abhi bhi nahi kharida
+        int c=0;
+        int d=0;
+
+        if(prev==0){
+            a=-arr[idx]+Solve(arr,idx+1,1,count,dp);
+            b=Solve(arr,idx+1,0,count,dp);
         }
-        if(prev==1){// Sell it
-        sell = prices[idx]+Solve(prices,idx+1,0,dp);
-        notSell= Solve(prices,idx+1,1,dp);
+        else{
+            c=arr[idx]+Solve(arr,idx+1,0,count+1,dp);
+            d=Solve(arr,idx+1,1,count,dp);
         }
-        return dp[idx][prev] = Math.max(Math.max(a,b),Math.max(sell,notSell));
+        return dp[idx][prev] = Math.max(Math.max(a,b),Math.max(c,d));
     }
 }
