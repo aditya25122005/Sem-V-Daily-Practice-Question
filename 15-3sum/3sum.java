e773ab01fcef9557e0826df874da5503c664397e
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-12-19 22:49:42 +0530

    Time: 43 ms (23.33%) | Memory: 59.6 MB (19.89%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,43 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans= new ArrayList<>();
+        int n= nums.length;
         Arrays.sort(nums);
-        Solve(nums,ans);
-        return ans;
-
-    }
-    public static void Solve(int[] nums, List<List<Integer>> ans){
-        for(int i=0;i<nums.length-2;i++){
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
-            }
-            int left=i+1;
-            int right=nums.length-1;
-            while(left<right){
-            int curr=nums[i]+nums[left]+nums[right];
-            if(curr==0){
-                List<Integer> ll= new ArrayList<>();
-                ll.add(nums[i]);
-                ll.add(nums[left]);
-                ll.add(nums[right]);
-                ans.add(new ArrayList<>(ll));
-                left++;
-                right--;
-                while(left<right && nums[left]==nums[left-1]) left++;
-                while(left<right && nums[right]==nums[right+1]) right--;
-
-            }else if(curr<0){
-                left++;
-                while(left<right && nums[left]==nums[left-1]) left++; 
-            }
-            else{
-                right--;
-                while(left<right && nums[right]==nums[right+1]) right--;
-            }
-
-
-            }
+        List<List<Integer>> ll= new ArrayList<>();
+        for(int i=0;i<n-2;i++){
+        if(i>0 && nums[i]==nums[i-1]){
+            continue;
         }
+        int l=i+1;
+        int r=n-1;
+        while(l<r){
+        if(nums[i]+nums[l]+nums[r]==0){
+            ll.add(Arrays.asList(nums[i],nums[l],nums[r]));
+            l++;
+            r--;
+            while(l<r && nums[l]==nums[l-1]) l++;
+            while(l<r && nums[r]==nums[r+1]) r--; 
+        }
+        else if(nums[i]+nums[l]+nums[r]<0){
+            l++;
+            while(l<r && nums[l]==nums[l-1]) l++;
+        }
+        else{
+            r--;
+            while(l<r && nums[r]==nums[r+1]) r--; 
+        }
+        }
+
+      }
+      return ll;
 
     }
 }
