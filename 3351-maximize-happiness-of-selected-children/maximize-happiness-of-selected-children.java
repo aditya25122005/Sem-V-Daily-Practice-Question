c2d1f95876656352ef3c687c9554c56e01ae10e1
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-12-25 10:44:28 +0530

    Time: 43 ms (43.55%) | Memory: 109.3 MB (22.58%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.java
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.java
@@ -1,14 +1,13 @@
 class Solution {
     public long maximumHappinessSum(int[] happiness, int k) {
-        PriorityQueue<Integer> pq= new PriorityQueue<>(Collections.reverseOrder());
-        for(int num:happiness){
-            pq.add(num);
-        }
+        int n=happiness.length;
+        Arrays.sort(happiness);
         int minus=0;
         long ans=0;
-        while(k>0){
-            if(pq.peek()>=minus){
-                ans+=pq.poll()-minus;
+        
+        for(int i=n-1;i>=0;i--){
+            if(k>0 && happiness[i]-minus>=0){
+                ans+=happiness[i]-minus;
                 minus++;
                 k--;
             }
@@ -17,5 +16,22 @@ class Solution {
             }
         }
         return ans;
+        // PriorityQueue<Integer> pq= new PriorityQueue<>(Collections.reverseOrder());
+        // for(int num:happiness){
+        //     pq.add(num);
+        // }
+        // int minus=0;
+        // long ans=0;
+        // while(k>0){
+        //     if(pq.peek()>=minus){
+        //         ans+=pq.poll()-minus;
+        //         minus++;
+        //         k--;
+        //     }
+        //     else{
+        //         return ans;
+        //     }
+        // }
+        // return ans;
     }
 }
