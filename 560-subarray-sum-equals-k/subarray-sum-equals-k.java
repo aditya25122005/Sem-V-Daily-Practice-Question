dadfc7c96afe4ec9d1e796cfbe62e131208dc112
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-12-26 19:26:20 +0530

    Time: 25 ms (46.56%) | Memory: 49.8 MB (5.85%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,20 +1,25 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int[] pre= new int[nums.length+1];
-        pre[0]=0;
-        for(int i=1;i<=nums.length;i++){
-            pre[i]=pre[i-1]+nums[i-1];
+        HashMap<Integer,Integer> map= new HashMap<>();
+        map.put(0,1);
+        int[] pre= new int[nums.length];
+        pre[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            pre[i]=pre[i-1]+nums[i];
         }
+        // Pre => [1,2,3] k=2;
         int c=0;
-          // 1  2  3
-          for(int i=0;i<pre.length;i++){
-            for(int j=i+1;j<pre.length;j++){
-                if(pre[j]-pre[i]==k) c++;
-                
-            }
+        for(int i=0;i<nums.length;i++){
+          // if pre[i]-pre[j]==k
+          // check if we have seen -> pre[i]-k * how many times
+
+          if(map.containsKey(pre[i]-k)){
+            c+= map.get(pre[i]-k);
           }
-          return c;
-
-
-     }
+          
+          map.put(pre[i],map.getOrDefault(pre[i],0)+1);
+    
+        }
+        return c;
+    }
 }
