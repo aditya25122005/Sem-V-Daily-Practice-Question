00f3430375123df2eeb6c575907dbe66e5907cf0
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-12-26 22:21:29 +0530

    Time: 6 ms (20.81%) | Memory: 43.7 MB (9.98%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,26 +1,24 @@
 class Solution {
     public int hIndex(int[] citations) {
-        int n=citations.length;
+        Arrays.sort(citations);
+        int n= citations.length;
+
         int lo=0;
         int hi=n-1;
         int ans=0;
-        Arrays.sort(citations);
-        //Binary serch on h
         while(lo<=hi){
-            int mid=lo+(hi-lo)/2; 
-            if(citations[mid]==n-mid){
+            int mid=lo+(hi-lo)/2;
+            if(citations[mid]== n-mid){
                 ans=n-mid;
                 return ans;
             }
-            else if(citations[mid]< n-mid){
+            else if(citations[mid]<n-mid){
                 lo=mid+1;
             }
             else{
-               hi=mid-1;
+                hi=mid-1;
             }
-
-
         }
-        return n-lo; // lo is the index from which the condition becomes true so we have total n-lo elements
+        return n-lo;
     }
 }
