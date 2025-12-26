e6f67ba0250641b48cd8bb462590788900f7a17d
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-12-26 23:05:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (71.35%) - LeetSync

--- 289-game-of-life/game-of-life.java
+++ 289-game-of-life/game-of-life.java
@@ -1,73 +1,68 @@
 class Solution {
     public void gameOfLife(int[][] board) {
-        int n= board.length;
-        int m= board[0].length;
-        // int [][] grid= new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                
-                int live=countLive(board,i,j);
-                if(board[i][j]==0){
-                    if(live==3){
-                        // grid[i][j]=1;
-                        board[i][j]=2;
+        int n = board.length;
+        int m = board[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                int live = countLive(board, i, j);
+                if (board[i][j] == 0) {
+                    if (live == 3) {
+                        board[i][j] = 2;
                     }
                 }
                 // -1 -> live se dead
                 // 2 when dead to live
-                else{
-                    if(live<2) board[i][j]=-1;
-                    else if(live==2 || live==3) board[i][j]=1;
-                    else if(live>3) board[i][j]=-1;
+                else {
+                    if (live < 2)
+                        board[i][j] = -1;
+                    else if (live == 2 || live == 3)
+                        board[i][j] = 1;
+                    else if (live > 3)
+                        board[i][j] = -1;
                 }
             }
         }
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++){
-        //         board[i][j]=grid[i][j];
-        //     }
-        // }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(board[i][j]>=1){
-                    board[i][j]=1;
-                }
-                else if(board[i][j]<=0){
-                    board[i][j]=0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] >= 1) {
+                    board[i][j] = 1;
+                } else if (board[i][j] <= 0) {
+                    board[i][j] = 0;
                 }
             }
         }
-       
-        
+
     }
-   
-    public static int countLive(int [][]arr, int cr, int cc){
-             int live=0;
-            if(cr!=0 && (arr[cr-1][cc]==1 || arr[cr-1][cc]==-1)){
-                live++;
-            }
-            if(cr!=arr.length-1 && (arr[cr+1][cc]==1 || arr[cr+1][cc]==-1)){
-                live++;
-            }
-            if(cc!=0 && (arr[cr][cc-1]==1 || arr[cr][cc-1]==-1)){
-                live++;
-            }
-            if(cc!=arr[0].length-1 && (arr[cr][cc+1]==1 || arr[cr][cc+1]==-1 )){
-                live++;
-            }
-            if(cr!=0 && cc!=0 && (arr[cr-1][cc-1]==1 || arr[cr-1][cc-1]==-1)){
-                live++;
-            }
-            if(cr!=arr.length-1 && cc!=0 && (arr[cr+1][cc-1]==1)){
-                live++;
-            }
-            if(cr!=0 && cc!=arr[0].length-1 && (arr[cr-1][cc+1]==1 || arr[cr-1][cc+1]==-1 )){
-                live++;
-            }
-            if(cr!=arr.length-1 && cc!=arr[0].length-1 &&(arr[cr+1][cc+1]==1 || 
-            arr[cr+1][cc+1]==-1) ){
-                live++;
-            }
-            return live;
+
+    public static int countLive(int[][] arr, int cr, int cc) {
+        int live = 0;
+        if (cr != 0 && (arr[cr - 1][cc] == 1 || arr[cr - 1][cc] == -1)) {
+            live++;
         }
+        if (cr != arr.length - 1 && (arr[cr + 1][cc] == 1 || arr[cr + 1][cc] == -1)) {
+            live++;
+        }
+        if (cc != 0 && (arr[cr][cc - 1] == 1 || arr[cr][cc - 1] == -1)) {
+            live++;
+        }
+        if (cc != arr[0].length - 1 && (arr[cr][cc + 1] == 1 || arr[cr][cc + 1] == -1)) {
+            live++;
+        }
+        if (cr != 0 && cc != 0 && (arr[cr - 1][cc - 1] == 1 || arr[cr - 1][cc - 1] == -1)) {
+            live++;
+        }
+        if (cr != arr.length - 1 && cc != 0 && (arr[cr + 1][cc - 1] == 1)) {
+            live++;
+        }
+        if (cr != 0 && cc != arr[0].length - 1 && (arr[cr - 1][cc + 1] == 1 || arr[cr - 1][cc + 1] == -1)) {
+            live++;
+        }
+        if (cr != arr.length - 1 && cc != arr[0].length - 1 && (arr[cr + 1][cc + 1] == 1 ||
+                arr[cr + 1][cc + 1] == -1)) {
+            live++;
+        }
+        return live;
+    }
 }
