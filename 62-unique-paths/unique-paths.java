3780d4dd6d0e2a0be1188640b5d1655a9a301f3a
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-12-27 22:44:24 +0530

    Time: 1 ms (14.63%) | Memory: 43.1 MB (8.91%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,21 +1,25 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp= new int[m][n];
-        for(int [] a:dp) Arrays.fill(a, -1);
-        return Count(0, 0, m-1, n-1,dp);
-    }
-    public static int Count(int cr, int cc, int er, int ec,int [][]dp){
-        if(cr==er && cc==ec){
-            return 1;
+        int [][] dp= new int[m][n];
+        for(int [] A:dp){
+            Arrays.fill(A,-1);
         }
-        if(cr<0 || cr>er || cc<0 || cc>ec){
+        return Solve(0,0,m,n,dp);
+    }
+    public static int Solve(int cr, int cc, int m, int n, int[][] dp){
+        if(cr<0 || cc<0 || cr>=m || cc>=n){
             return 0;
+        }
+        if(cr==m-1 && cc==n-1){
+            return 1;
         }
         if(dp[cr][cc]!=-1) return dp[cr][cc];
         int ans=0;
-        ans+=Count(cr+1,cc,er,ec,dp);
-        ans+=Count(cr, cc+1, er, ec,dp);
+        int [][] dir={{0,1},{1,0}};
+        for(int []D:dir){
+            ans+= Solve(cr+D[0],cc+D[1],m,n,dp);
+        }
 
-        return dp[cr][cc]=ans;
+        return dp[cr][cc]  =ans;
     }
 }
