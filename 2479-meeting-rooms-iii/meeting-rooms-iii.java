f9dce5f0c0a9e50d6d2bb40127415f4b607ed0c8
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2025-12-27 12:43:33 +0530

    Time: 50 ms (98.30%) | Memory: 125.9 MB (82.06%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.java
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.java
@@ -1,46 +1,45 @@
 class Solution {
     public int mostBooked(int n, int[][] meetings) {
         Arrays.sort(meetings,(a,b)->a[0]-b[0]);
-     int [] ans= new int[n];
-     long [][] rooms= new long[n][2];
-     for(int[] M:meetings){
-        int start= M[0];
-        int end=M[1];
-        int duration= end-start;
-        int room=-1;
-        for(int i=0;i<rooms.length;i++){
-            if(rooms[i][1]<=start){
-                ans[i]++;
-                rooms[i][1]=end;
-                room=i;
-                break;
-            }
-        }
-        
-        if(room==-1){
-            
-            long earliest=Long.MAX_VALUE;
-            int idx=-1;
-            for(int i=0;i<rooms.length;i++){
-                if(rooms[i][1]<earliest){
-                    earliest=rooms[i][1];
-                    idx=i;
+        long [] endR= new long[n];
+        int [] ans= new int[n];
+        for(int []M:meetings){
+            int start=M[0];
+            int end=M[1];
+            int duration=end-start;
+            int room=-1;
+            for(int i=0;i<endR.length;i++){
+                if(endR[i]<=start){
+                    room=i;
+                    ans[i]++;
+                    endR[i]=end;
+                    break;
                 }
             }
-            ans[idx]++;
-            rooms[idx][1]+=duration;
-        }
+            if(room==-1){
+                // earliest free room
+                long min_end_time=Long.MAX_VALUE;
+                int idx=-1;
+                for(int i=0;i<endR.length;i++){
+                    if(endR[i]<min_end_time){
+                        min_end_time=endR[i];
+                        idx=i;
+                    }
+                }
+                ans[idx]++;
+                endR[idx]+= duration;
 
-
-     }   
-     int max=0;
-     int idx=0;
-     for(int i=0;i<ans.length;i++){
-        if(ans[i]>max){
-            max=ans[i];
-            idx=i;
+            }
         }
-     }
-     return idx;
+        int res=-1;
+        int max=0;
+        for(int i=0;i<ans.length;i++){
+            if(ans[i]>max){
+                max=ans[i];
+                res=i;
+            }
+        }
+        return res;
+
     }
 }
