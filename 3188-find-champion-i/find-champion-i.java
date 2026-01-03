7b0a75dc2e4bdbc39957f8881c19048ef8f68bc2
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-03 19:35:47 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (5.68%) - LeetSync

--- 3188-find-champion-i/find-champion-i.java
+++ 3188-find-champion-i/find-champion-i.java
@@ -1,21 +1,30 @@
 class Solution {
     public int findChampion(int[][] grid) {
-        int champion=0;
-        HashSet<Integer> loser= new HashSet<>();
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid.length;j++){
-                if(i==j) continue;
-                if(grid[i][j]==1){
-                    loser.add(j);
-                }
+        // int champion=0;
+        // HashSet<Integer> loser= new HashSet<>();
+        // for(int i=0;i<grid.length;i++){
+        //     for(int j=0;j<grid.length;j++){
+        //         if(i==j) continue;
+        //         if(grid[i][j]==1){
+        //             loser.add(j);
+        //         }
 
-            }
-        }
+        //     }
+        // }
+        // for(int i=0;i<grid.length;i++){
+        //     if(!loser.contains(i)){
+        //         return i;
+        //     }
+        // }
+        // return 0;
+
+        int champion=0;
         for(int i=0;i<grid.length;i++){
-            if(!loser.contains(i)){
-                return i;
+            if(i==champion) continue;
+            if(grid[champion][i]==0){
+                champion=i;
             }
         }
-        return 0;
+        return champion;
     }
 }
