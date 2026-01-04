943842fcc5c0ca0aa5620a50e8aaae72c937cfe7
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-04 20:21:50 +0530

    Time: 17 ms (56.31%) | Memory: 47.3 MB (9.85%) - LeetSync

--- 1284-four-divisors/four-divisors.java
+++ 1284-four-divisors/four-divisors.java
@@ -1,26 +1,33 @@
 class Solution {
     public int sumFourDivisors(int[] nums) {
         HashMap<Integer,Integer> map= new HashMap<>();
-        HashSet<Integer> bad= new HashSet<>();
+
         int ans=0;
         for(int num:nums){
-            if(bad.contains(num)) continue;
+
             if(map.containsKey(num)){
                 ans+= map.get(num);
                 continue;
             }
-            int c=0;
-            int curr=0;
-            for(int i=1;i<=num;i++){
-                if(num%i==0){
+            int c=2;
+            int curr=1+num;
+            for(int i=2;i*i<=num;i++){
+                int j= num/i;
+            if(num%i==0){
+                if(i==j){
                     c++;
                     curr+=i;
-
-                    if(c>4){
-                        bad.add(num);
-                        break;
-                    }
                 }
+                else{
+                    c+=2;
+                    curr+= i;
+                    curr += j;
+                }
+
+                if(c>4){
+                    break;
+                }
+            }
                 
             }
             if(c==4){
