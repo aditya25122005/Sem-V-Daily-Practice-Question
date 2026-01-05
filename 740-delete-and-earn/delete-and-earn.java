1361940fae63f01d42e3f7d70d5fc1c7816d1bc4
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-05 17:32:52 +0530

    Time: 4 ms (58.19%) | Memory: 47 MB (45.06%) - LeetSync

---
+++ 740-delete-and-earn/delete-and-earn.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        int max=0;
+        int min=0;
+        
+        for(int num:nums){
+            max=Math.max(max,num);
+            min=Math.min(min,num);
+        }
+        int [] dp= new int[max+1];
+        Arrays.fill(dp,-1);
+        int [] points= new int[max+1];
+        for(int num:nums){
+            points[num]+= num;
+        }
+        return Solve(points,min,dp);
+
+    }
+    public static int Solve(int[] points, int idx, int[] dp){
+        if(idx>=points.length){
+            return 0;
+        }
+        // if(idx==points.length-1){
+        //     return points[points.length-1];
+        // }
+        if(dp[idx]!=-1){
+            return dp[idx];
+        }
+        int a= points[idx]+Solve(points,idx+2,dp);
+        int b=Solve(points,idx+1,dp);
+         return dp[idx]=Math.max(a,b);
+    }
+}
