636161071c8201b3a7a50e0250bb9b36823dd28a
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-05 12:47:32 +0530

    Time: 5 ms (91.52%) | Memory: 68.3 MB (13.94%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.java
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.java
@@ -5,9 +5,14 @@ class Solution {
         int min=Integer.MAX_VALUE;
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
-                total+= Math.abs(matrix[i][j]);
-                min=Math.min(min,Math.abs(matrix[i][j]));
-                if(matrix[i][j]<0) negCount++;
+                int val= matrix[i][j];
+                if(val<0){
+                    negCount++;
+                    val=-val;
+                }
+                total+= val;
+                min=Math.min(min,val);
+                
             }
         }
         if(negCount%2==0){
