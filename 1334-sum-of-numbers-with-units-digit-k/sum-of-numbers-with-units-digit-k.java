34cf94a39dcdedbbbf63516108894b1163b37ee1
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-07 00:51:05 +0530

    Time: 81 ms (3.41%) | Memory: 53.7 MB (5.68%) - LeetSync

---
+++ 1334-sum-of-numbers-with-units-digit-k/sum-of-numbers-with-units-digit-k.java
@@ -0,0 +1,47 @@
+class Solution {
+    // 1, 11, 21, 31,-11,-21,-31
+    int k;
+    public int minimumNumbers(int num, int k) {
+        if(num==0) return 0;
+        List<Integer> ll= new ArrayList<>();
+        
+        for(int n=k;n<=num;n++){
+            if(n%10==k){
+                
+                ll.add(n);
+            }
+        }
+        if(k==0){
+            ll.remove(0);
+        }
+        this.k=k;
+        int [][] dp= new int[num+1][ll.size()+1];
+
+            for(int []A:dp){
+                Arrays.fill(A,-1);
+            }
+        
+        int ans=Solve(0,0,num,0,ll,dp);
+        return ans==Integer.MAX_VALUE? -1:ans ;
+
+    }
+    public int Solve(int currUsed, int currSum,int Target, int idx, List<Integer> ll, int[][]dp){
+        if(currSum==Target){
+            return currUsed;
+        }
+        if(currSum>Target){
+            return Integer.MAX_VALUE;
+        }
+        if(idx==ll.size()){
+            return Integer.MAX_VALUE;
+        }
+        if(dp[currSum][idx]!=-1) return dp[currSum][idx];
+        // skip
+        int a= Solve(currUsed,currSum,Target,idx+1,ll,dp);
+        int b=0;
+        //take this 
+            
+            b=Solve(currUsed+1,currSum+ll.get(idx),Target,idx,ll,dp);
+        return dp[currSum][idx] = Math.min(a,b);
+    }
+}
