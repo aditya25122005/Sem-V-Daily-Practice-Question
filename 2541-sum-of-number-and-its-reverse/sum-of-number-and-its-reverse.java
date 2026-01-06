1ac67b341bb75ee6822d0573d3a047bbcdd0bd01
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-07 01:10:08 +0530

    Time: 147 ms (75.20%) | Memory: 42.5 MB (23.23%) - LeetSync

--- 2541-sum-of-number-and-its-reverse/sum-of-number-and-its-reverse.java
+++ 2541-sum-of-number-and-its-reverse/sum-of-number-and-its-reverse.java
@@ -8,15 +8,12 @@ class Solution {
 
     }
     public static int reverse(int num){
-        // int n=0;
-        // while(num!=0){
-        //     int r=num%10;
-        //     n=n*10+ r;
-        //     num/=10;
-        // }
-        // return n;
-        String s=String.valueOf(num);
-        StringBuilder sb= new StringBuilder(s);
-        return Integer.parseInt(sb.reverse().toString());
+        int n=0;
+        while(num!=0){
+            int r=num%10;
+            n=n*10+ r;
+            num/=10;
+        }
+        return n;
     }
 }
