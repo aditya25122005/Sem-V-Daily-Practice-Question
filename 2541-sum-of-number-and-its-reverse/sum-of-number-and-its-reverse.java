90c97b40348b4a0d6eabf1b777832bd8dab9faf0
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-07 01:10:51 +0530

    Time: 76 ms (95.67%) | Memory: 42.3 MB (56.30%) - LeetSync

--- 2541-sum-of-number-and-its-reverse/sum-of-number-and-its-reverse.java
+++ 2541-sum-of-number-and-its-reverse/sum-of-number-and-its-reverse.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean sumOfNumberAndReverse(int num) {
-        for(int i=0;i<=num;i++){
+        for(int i=num/2;i<=num;i++){
             int rev= reverse(i);
             if(rev+i==num) return true;
         }
