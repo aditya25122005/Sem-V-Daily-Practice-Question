3e56d4d62549ed666196ecd2665abc3199fb1fcc
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-08 10:00:49 +0530

    Time: 10 ms (77.78%) | Memory: 48.9 MB (8.89%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.java
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.java
@@ -1,25 +1,26 @@
 class Solution {
     public int maxDotProduct(int[] nums1, int[] nums2) {
-        int n=nums1.length;
-        int m= nums2.length;
-        int[][] dp= new int[n][m];
-        for(int [] a:dp){
-            Arrays.fill(a,Integer.MIN_VALUE);
-        }
-        return DOT(nums1, nums2, 0,0,dp);
+        Integer[][] dp= new Integer[nums1.length][nums2.length];
+        return Solve(nums1,nums2,0,0,dp);
     }
-    public static int DOT(int [] nums1, int[] nums2, int i, int j, int [][] dp){
-        if(i==nums1.length || j== nums2.length){
+    public static int Solve(int[]nums1, int[]nums2, int i, int j,Integer[][]dp){
+        if(i==nums1.length || j==nums2.length){
             return Integer.MIN_VALUE;
         }
-        if(dp[i][j]!=Integer.MIN_VALUE){
+        if(dp[i][j]!=null){
             return dp[i][j];
         }
-        int ans=nums1[i]*nums2[j]; // take only one pair
-        int f= ans + Math.max(0,DOT(nums1, nums2, i+1, j+1,dp)); // continue add ing old value
-        int s= DOT(nums1, nums2, i+1,j,dp); // skip this i and check for other pairs 
-        int t= DOT(nums1, nums2, i,j+1,dp);  // skip this j ,,  ,,  ,,
-        return dp[i][j]= Math.max(Math.max(ans,f),Math.max(s,t));  // Return max of all
-        
+        int B=0;
+        int C=0;
+        int D=0;
+        int ans= nums1[i]*nums2[j];
+      
+        B = ans + Math.max(0,Solve(nums1,nums2,i+1,j+1,dp));
+        C = Solve(nums1,nums2,i,j+1,dp);
+        D = Solve(nums1,nums2,i+1,j,dp);
+
+        int res= Math.max(Math.max(D,B),Math.max(C,ans));
+        return dp[i][j]=res;
+
     }
 }
