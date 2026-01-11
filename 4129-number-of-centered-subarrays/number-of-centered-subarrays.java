ec7adea0538986edb279a4220f711cd3f129006b
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-11 22:08:13 +0530

    Time: 87 ms (63.78%) | Memory: 46.9 MB (70.63%) - LeetSync

--- 4129-number-of-centered-subarrays/number-of-centered-subarrays.java
+++ 4129-number-of-centered-subarrays/number-of-centered-subarrays.java
@@ -1,28 +1,14 @@
 class Solution {
     public int centeredSubarrays(int[] nums) {
         int count=0;
-        int [] pre= new int[nums.length];
-        pre[0]=nums[0];
-        for(int i=1;i<nums.length;i++){
-            pre[i] = pre[i-1] + nums[i];
-        }
         for(int i=0;i<nums.length;i++){
+            int curr=0;
+            HashSet<Integer> hh= new HashSet();
             for(int j=i;j<nums.length;j++){
-                int curr=0;
-                HashSet<Integer> hh= new HashSet<>();
-                if(i==0) curr= pre[j];
-                else{
-                    curr= pre[j]-pre[i-1];
-                }
-             
-
-                for(int k=i;k<=j;k++){
-                    if(nums[k]==curr){
-                        count++;
-                        break;
-                    }
-                }
-            }       
+                hh.add(nums[j]);
+                curr+= nums[j];
+                if(hh.contains(curr)) count++;
+            }
         }
         return count;
     }
