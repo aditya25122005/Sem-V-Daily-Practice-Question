25adf4ecb0437c759e2495ca7efb5445ec69cbb5
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-11 19:26:26 +0530

    Time: 26 ms (11.87%) | Memory: 48.7 MB (79.57%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -1,51 +1,60 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
-        int [] ht= new int[matrix[0].length];
-
+        int n= matrix.length;
+        int m=matrix[0].length;
         int ans=0;
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
+        int [] ht= new int[m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(matrix[i][j]=='0'){
                     ht[j]=0;
                 }
                 else{
                     ht[j]++;
                 }
+
             }
-            ans=Math.max(ans,Largest(ht));
-        }
-        return ans;
-    }
-    public static int Largest(int[] ht){
-        int ans=0;
-        Stack<Integer> st= new Stack<>();
-        for(int i=0;i<ht.length;i++){
-            while(!st.isEmpty() && ht[i]<ht[st.peek()]){
-                int h=ht[st.pop()];
-                int r=i;
-                if(st.isEmpty()){
-                    ans=Math.max(ans,h*r);
-                }else{
-                    int l=st.peek();
-                    int width=r-l-1;
-                    ans=Math.max(ans,h*width);
-                }
-            }
-            st.push(i);
-        }
-        int r=ht.length;
-        while(!st.isEmpty()){
-            int h= ht[st.pop()];
-            int width;
-            if(st.isEmpty()){
-                width=r;
-            }else{
-                int l= st.peek();
-                width=r-l-1;
-            }
-            ans= Math.max(ans,h*width);
+            ans=Math.max(ans,largestRectangle(ht));
         }
         return ans;
 
+
+    }
+    public static int largestRectangle(int[] ht){
+        Stack<Integer> st= new Stack<>();
+        int [] PSE= new int[ht.length];
+        for(int i=0;i<ht.length;i++){
+            while(!st.isEmpty() && ht[st.peek()]>=ht[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                PSE[i]=0;
+            }
+            else{
+                PSE[i]=st.peek()+1;
+            }
+            st.push(i);
+        }
+        st.clear();
+        int [] NSE= new int[ht.length];
+        for(int i=ht.length-1;i>=0;i--){
+            while(!st.isEmpty() && ht[st.peek()]>=ht[i]){
+                st.pop();
+                
+            }
+            if(st.isEmpty()){
+                NSE[i]=ht.length-1;
+            }
+            else{
+                NSE[i]=st.peek()-1;
+            }
+            st.push(i);
+        }
+        int max=0;
+        for(int i=0;i<ht.length;i++){
+            int curr= ht[i]*(NSE[i]-PSE[i]+1);
+            max=Math.max(max,curr);
+        }
+        return max;
     }
 }
