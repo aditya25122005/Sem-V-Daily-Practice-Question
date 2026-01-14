4ddd74d69f50bad4d40127129707d9f9e9c28312
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-14 12:46:03 +0530

    Time: 1 ms (99.77%) | Memory: 48.2 MB (14.37%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -1,22 +1,23 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        int [] ans=new int[2];
-        int idx=0;
+        int xor=0;
+        for(int num:nums){
+            xor^=num;
+        }
+
+        int mask= xor&(~(xor-1));
+        // int mask= xor &((-1*xor));
+
+
+        int a=0;
         for(int i=0;i<nums.length;i++){
-            boolean found=false;
-            for(int j=0;j<nums.length;j++){
-                if(i!=j && nums[i]==nums[j]){
-                    found=true;
-                    break;
-                }
-            }
-            if(!found){
-                ans[idx++]=nums[i];
-                if(idx==2){
-                    break;
-                }
+            if((mask&nums[i])!=0){
+                a=a^nums[i];
             }
         }
-        return ans;
+        int b = xor ^ a;
+        return new int[]{a,b};
+
+
     }
 }
