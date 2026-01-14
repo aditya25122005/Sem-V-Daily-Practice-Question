7ffae44f0d44bc130f602b10062db0c14601e971
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-15 01:08:32 +0530

    Time: 80 ms (5.14%) | Memory: 52.2 MB (5.14%) - LeetSync

--- 1334-sum-of-numbers-with-units-digit-k/sum-of-numbers-with-units-digit-k.java
+++ 1334-sum-of-numbers-with-units-digit-k/sum-of-numbers-with-units-digit-k.java
@@ -1,49 +1,40 @@
 class Solution {
-    // 1, 11, 21, 31,--
-    int k;
     public int minimumNumbers(int num, int k) {
-        if(num==0) return 0;
+        if(num==0){
+            return 0;
+        }
         List<Integer> ll= new ArrayList<>();
-        
-        for(int n=k;n<=num;n++){
-            if(n%10==k){
-                
-                ll.add(n);
+        for(int i=k;i<=num;i++){
+            if(i%10==k){
+                ll.add(i);
             }
         }
         if(k==0){
             ll.remove(0);
         }
-        this.k=k;
-        int [][] dp= new int[num+1][ll.size()+1];
-
-            for(int []A:dp){
-                Arrays.fill(A,-1);
-            }
-        
-        int ans=Solve(0,num,0,ll,dp);
-        return ans==Integer.MAX_VALUE? -1:ans ;
+        int [][] dp= new int[ll.size()][num+1];
+        for(int [] A:dp) Arrays.fill(A,-1);
+        int ans=Solve(ll,0,num,0,dp);
+        return ans==Integer.MAX_VALUE? -1 :ans;
 
     }
-    public int Solve(int currSum,int Target, int idx, List<Integer> ll, int[][]dp){
-        if(currSum==Target){
+    public static int Solve(List<Integer> ll, int idx, int target, int currSum, int [][] dp){
+        if(currSum==target){
             return 0;
         }
-        if(currSum>Target){
+        if(currSum>target){
             return Integer.MAX_VALUE;
         }
         if(idx==ll.size()){
             return Integer.MAX_VALUE;
         }
-        if(dp[currSum][idx]!=-1) return dp[currSum][idx];
-        // skip
-        int a= Solve(currSum,Target,idx+1,ll,dp);
-        int b=0;
-        //take this 
-            b=Solve(currSum+ll.get(idx),Target,idx,ll,dp);
-            if(b!=Integer.MAX_VALUE){
-                b+=1;
-            }
-        return dp[currSum][idx] = Math.min(a,b);
+        if(dp[idx][currSum]!=-1) return dp[idx][currSum];
+        int A= Solve(ll,idx+1,target,currSum,dp);
+        int B=0;
+        B=Solve(ll,idx,target,currSum+ll.get(idx),dp);
+        if(B!=Integer.MAX_VALUE){
+            B+=1;
+        }
+        return dp[idx][currSum] = Math.min(A,B);
     }
 }
