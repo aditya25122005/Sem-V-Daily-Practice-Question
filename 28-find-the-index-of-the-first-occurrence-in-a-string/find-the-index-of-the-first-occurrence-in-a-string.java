5da5e52eb27da307eca6009036ca654a38c7b3d7
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-16 23:53:07 +0530

    Time: 2 ms (12.85%) | Memory: 43.1 MB (37.36%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -2,8 +2,7 @@ class Solution {
     public int strStr(String s1, String s2) {
         // return s1.indexOf(s2);
         if(s1.length()<s2.length()) return -1;
-        for(int i=0;i<s1.length();i++){
-            
+        for(int i=0;i<=s1.length()-s2.length();i++){
             if(s1.charAt(i)==s2.charAt(0)){
             int start=i;
             boolean isPossible=true;
@@ -17,7 +16,6 @@ class Solution {
                     covered++;
                   
                         idx++;
-                    
                 }
                 if(covered<s2.length()) isPossible=false;
 
