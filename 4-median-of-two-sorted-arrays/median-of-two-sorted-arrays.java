f49f6e164c69be5509a97e11ffe1412de9f210a6
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-17 01:22:18 +0530

    Time: 2 ms (45.98%) | Memory: 48.9 MB (60.82%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,42 +1,58 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n= nums1.length;
-        int m= nums2.length;
-        int [] arr= new int[n+m];
-        int i=0;
-        int j=0;
-        int k=0;
-        while(i<n && j<m){
-            if(nums1[i]<nums2[j]){
-                arr[k]=nums1[i];
-                i++;
-                k++;
+
+        // we take always nums1 as smallerst array to apply binary search on choosing no. of elements from nums1 
+        if(nums1.length>nums2.length){
+            return findMedianSortedArrays(nums2,nums1);
+        }
+        int m=nums1.length;
+        int n=nums2.length;
+
+
+        int lo=0; // take 0 elements from part 1 and ALL from part 2
+        int hi=m; // take m elements from part 1 and rest from part 2
+        // we have to apply binary search on , no. of elements chosen from shorter array
+
+        while(lo<=hi){
+            int cut1= lo+(hi-lo)/2; //No. of elements to take in LEFT part from nums1
+            int cut2= (m+n+1)/2 - cut1;  // no. of elements to take in LEFT part from nums2
+            // cut1+cut2 = half of the total elements (left part)
+            /*
+            nums1: [1, 3 | 8]
+            nums2: [7, 9 | 10, 11]
+             */
+
+            // Largest element in left part of nums1
+            int maxLeftA=(cut1==0)? Integer.MIN_VALUE : nums1[cut1-1];
+
+            // smallest element in right part of nums1
+            int minRightA= (cut1==m)?Integer.MAX_VALUE : nums1[cut1];
+
+            // Largest element in left side of nums2
+            int maxLeftB=(cut2==0)? Integer.MIN_VALUE : nums2[cut2-1];
+
+            // smallest element in right side of nums2
+            int minRightB=(cut2==n)? Integer.MAX_VALUE : nums2[cut2];
+
+
+            if(maxLeftA <= minRightB && maxLeftB<= minRightA){
+                if((m+n)%2==1){
+                    return Math.max(maxLeftA,maxLeftB);
+                }
+                else{
+                    return (Math.max(maxLeftA,maxLeftB)+Math.min(minRightA,minRightB))/2.0;
+                }
+            }
+            else if(maxLeftA>minRightB){
+                hi=cut1-1;
             }
             else{
-                arr[k]=nums2[j];
-                j++;
-                k++;
+                lo=cut1+1;
             }
-        }
-        while(i<n){
-            arr[k]=nums1[i];
-            i++;
-            k++;
-        }
-        while(j<m){
-            arr[k]=nums2[j];
-            j++;
-            k++;
-        }
-        if(arr.length%2==1){
-            int idx=arr.length/2;
-            return (double)arr[idx];
-        }
-        else{
-            int a= arr.length/2;
-            int b= a-1;
-            return (double)(arr[a]+arr[b])/2;
-        }
 
+        }
+        return 0.0;
+
+        
     }
 }
