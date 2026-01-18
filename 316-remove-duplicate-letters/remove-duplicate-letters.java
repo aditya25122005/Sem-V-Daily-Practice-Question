3f9d154af0dd21cccf1bc5feb1c596a72ea656ae
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-18 15:40:07 +0530

    Time: 2 ms (86.58%) | Memory: 44 MB (29.80%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -2,7 +2,7 @@ class Solution {
     public String removeDuplicateLetters(String s) {
         Stack<Character> st= new Stack<>();
         int [] freq= new int [26];
-        int [] used= new int[26];
+
         boolean [] seen= new boolean [26];
         for(char ch:s.toCharArray()){
             freq[ch-'a']++;
@@ -12,7 +12,7 @@ class Solution {
             freq[ch-'a']--;
             if(seen[ch-'a']) continue;
             while(!st.isEmpty() && st.peek()>ch && freq[st.peek()-'a']>=1){
-                used[st.peek()-'a']--;
+
                 seen[st.peek()-'a']=false;
                 st.pop();
                 
@@ -28,9 +28,9 @@ class Solution {
         }
         sb.reverse();
         for(int i=sb.length()-1;i>0;i--){
-            if(sb.charAt(i)==sb.charAt(i-1) || used[sb.charAt(i)-'a']>1){
-                if(used[sb.charAt(i)-'a'] >1){
-                    used[sb.charAt(i)-'a']--;
+            if(sb.charAt(i)==sb.charAt(i-1) || freq[sb.charAt(i)-'a']>1){
+                if(freq[sb.charAt(i)-'a'] >1){
+                    freq[sb.charAt(i)-'a']--;
                     sb.deleteCharAt(i);
                 }
             }
