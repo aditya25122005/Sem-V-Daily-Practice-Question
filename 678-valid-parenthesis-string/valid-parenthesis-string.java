9424424793c4028a761b4b86aae8e61bb5e7d709
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-20 21:40:30 +0530

    Time: 1 ms (27.83%) | Memory: 42.8 MB (55.88%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,42 +1,32 @@
 class Solution {
     public boolean checkValidString(String s) {
-        int n= s.length();
-        Boolean [][][] dp= new Boolean[n][n+1][n+1];
-     
-        if(s.length()==1){
-            if(s.charAt(0)=='*') return true;
-            return false;
-        }
-        return Solve(s,0,0,0,dp);
-    }
-    public static boolean Solve(String s, int idx, int open, int close,Boolean[][][]dp){
-        if(close>open || open>s.length()/2 || close>s.length()/2){
-            return false;
-        }
-        if(idx==s.length()){
-            if(open==close){
-                return true;
+        Stack<Integer> open= new Stack<>();
+        Stack<Integer> star= new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(') open.push(i);
+            else if(s.charAt(i)=='*') star.push(i);
+            else{
+                if(open.isEmpty() && star.isEmpty()){
+                    return false; // Neither star or open brac. Avail. for close brac.
+                }
+                if(!open.isEmpty()){
+                    open.pop();
+                }
+                else if(!star.isEmpty()){
+                    star.pop();
+                }
             }
-            return false;
         }
-        if(dp[idx][open][close]!=null){
-            return dp[idx][open][close];
+        while(!star.isEmpty() && !open.isEmpty()){
+            if(star.peek() < open.peek()){
+                return false;
+            }
+            else{
+                open.pop();
+                star.pop();
+                if(open.isEmpty()) return true;
+            }
         }
-                
-        boolean a= false;
-        boolean b=false;
-        boolean c=false;
-        if(s.charAt(idx)=='('){
-            a= Solve(s,idx+1,open+1,close,dp);
-        }
-        if(s.charAt(idx)==')'){
-            b= Solve(s,idx+1,open,close+1,dp);
-        }
-        if(s.charAt(idx)=='*'){
-            c= Solve(s,idx+1,open+1,close,dp) || 
-                Solve(s,idx+1,open,close+1,dp) ||
-                Solve(s,idx+1,open,close,dp);
-        }
-        return dp[idx][open][close] = (a|| b|| c);
+        return open.isEmpty();
     }
 }
