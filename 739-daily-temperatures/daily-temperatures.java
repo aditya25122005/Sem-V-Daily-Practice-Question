33c705a4f650bf63949127751f45b727b14557d8
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-20 14:29:36 +0530

    Time: 76 ms (29.81%) | Memory: 106.7 MB (66.49%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,24 +1,22 @@
 class Solution {
-    public int[] dailyTemperatures(int[] temp) {
-        int [] res= new int[temp.length];
-        int n=res.length;
-        res[n-1]=0;
-        int min=temp[n-1];
-        // next greater element
+    public int[] dailyTemperatures(int[] arr) {
         Stack<Integer> st= new Stack<>();
-        HashMap<Integer,Integer> map= new HashMap<>();
-        for(int i=0;i<temp.length;i++){
-            while(!st.isEmpty() && temp[i]>temp[st.peek()]){
-                map.put(st.pop(),i);
+        int n = arr.length;
+        int [] ans= new int[n];
+
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && arr[st.peek()]<=arr[i]){
+                st.pop();
             }
+            if(st.isEmpty()){
+                ans[i] = 0;
+            }
+            else{
+                ans[i] = st.peek()-i;
+            }
+
             st.push(i);
         }
-        for(int i=0;i<res.length;i++){
-            if(map.containsKey(i)){
-                res[i]=map.get(i)-i;
-            }
-        }
-        return res;
-
+        return ans;
     }
 }
