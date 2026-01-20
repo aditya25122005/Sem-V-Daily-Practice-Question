6c17b140fcf31b1cf0f8f610e53b9fbe11e24eca
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-20 16:20:36 +0530

    Time: 7 ms (13.73%) | Memory: 47.5 MB (7.85%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,46 +1,45 @@
 class Solution {
-    public int[] asteroidCollision(int[] asteroids) {
-        int n= asteroids.length;
-        Stack<Integer> st= new Stack<>();
-
-
+    public int[] asteroidCollision(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        int n= arr.length;
+       
         for(int i=0;i<n;i++){
-            if(st.isEmpty() || st.peek()<0 && asteroids[i]>0 || sameSign(st.peek(),asteroids[i])){
-                st.push(asteroids[i]); // if curr aste. has same sign as of prev. one then push on stack,
+            if(st.isEmpty() || (st.peek()<0 && arr[i]>0) || samesign(arr[i],st.peek())){
+                st.push(arr[i]);
             }
             else{
-                while(!st.isEmpty() && asteroids[i]<0 && st.peek()>0 && st.peek()<-asteroids[i]){  
-                    st.pop();
+
+                while(!st.isEmpty() && !samesign(arr[i],st.peek()) && Math.abs(arr[i])> Math.abs(st.peek())){
+                    st.pop();// pop all values from top of stack that are smaller than current asteroid 
                 }
-                if(st.isEmpty() || st.peek()<0){
-                    st.push(asteroids[i]);
+                if(st.isEmpty() || st.peek()<0){ // if negative on top then we can safely add arr[i]
+                    st.push(arr[i]);
                 }
-                else if(st.peek()==Math.abs(asteroids[i])){
+                else if(st.peek()==Math.abs(arr[i])){
                     st.pop();
                 }
             }
-                
-            
-             
         }
-        int idx=0;
+
+
         int [] ans= new int[st.size()];
+        int idx=0;
         while(!st.isEmpty()){
             ans[idx++] = st.pop();
         }
         reverse(ans);
         return ans;
+
+
     }
-    public static boolean sameSign(int a, int b){
-        if(a>0 && b>0){
-            return true;
-        }
+    public static boolean samesign(int a, int b){
         if(a<0 && b<0) return true;
+        if(a>0 && b>0) return true;
         return false;
     }
-    public static void reverse(int [] arr){
+    public static void reverse(int[] arr){
         int i=0;
-        int j=arr.length-1;
+        int j= arr.length-1;
         while(i<j){
             int temp=arr[i];
             arr[i]=arr[j];
