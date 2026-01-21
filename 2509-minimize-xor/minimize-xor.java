fcac9fc25c994e2377d4c46cebfafff9d526abd9
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-21 12:37:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (8.86%) - LeetSync

---
+++ 2509-minimize-xor/minimize-xor.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minimizeXor(int num1, int num2) {
+        int set=0;
+        while(num2>0){
+            set++;
+            num2= (num2 &(num2-1));
+        }
+        int x=0;
+        for(int i=30;i>=0;i--){
+            int mask= (1<<i);
+            if((num1&mask)!=0){
+                x |= mask;
+                set--;
+                if(set==0){
+                    return x;
+                }
+            }
+        }
+        for(int i=0;i<=30;i++){
+            int mask= (1<<i);
+            if((num1&mask)==0){
+                x|= mask;
+                set--;
+                if(set==0){
+                    return x;
+                }
+            }
+        }
+        return x;
+    }
+}
