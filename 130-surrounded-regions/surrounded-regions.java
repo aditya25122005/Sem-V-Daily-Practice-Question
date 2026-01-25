48bed10cdec41e9d6e526d3e69402265c224c66e
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-25 15:29:27 +0530

    Time: 2 ms (82.49%) | Memory: 47.8 MB (65.11%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -2,56 +2,52 @@ class Solution {
     public void solve(char[][] board) {
         int n= board.length;
         int m= board[0].length;
-        boolean [][] visited= new boolean[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if((i==0 || j==0 || i==n-1 || j==m-1) && board[i][j]=='O'){
-                    // kardoChange=true;
-                    Search(i,j,board);
-                    
-                    // if(kardoChange){
-                    //     for(int r=0;r<n;r++){
-                    //         for(int c=0;c<m;c++){
-                    //             if(board[r][c]=='#') board[r][c]='X';
-                    //         }
-                    //     }
-                    // }
-                    // else{
-                    //     for(int r=0;r<n;r++){
-                    //         for(int c=0;c<m;c++){
-                    //             if(board[r][c]=='#') board[r][c]='O';
-                    //         }
-                    //     }
-                    // }
-
-                }
+        int r=0;
+        for(int j=0;j<m;j++){
+            if(board[r][j]=='O'){
+                DFS(board,r,j);
             }
         }
-        for(int r=0;r<n;r++){
-            for(int c=0;c<m;c++){
-                if(board[r][c]=='#'){
-                    board[r][c]='O';
+        r= n-1;
+        for(int j=0;j<m;j++){
+            if(board[r][j]=='O'){
+                DFS(board,r,j);
+            }
+        }
+        int c=0;
+        for(r=0;r<n;r++){
+            if(board[r][c]=='O'){
+                DFS(board,r,c);
+            }
+        }
+        c=m-1;
+        for(r=0;r<n;r++){
+            if(board[r][c]=='O'){
+                DFS(board,r,c);
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='#'){
+                    board[i][j]='O';
                 }
-                else{
-                    board[r][c]='X';
+                else if(board[i][j]=='O'){
+                    board[i][j]='X';
                 }
             }
         }
 
     }
-    // boolean kardoChange;
-    public  void Search(int cr, int cc, char[][]board){
-        if(cr<0 || cc<0 || cr>=board.length || cc>= board[0].length || board[cr][cc]=='#' ||board[cr][cc]=='X'){
+    public static void DFS(char[][] board, int cr, int cc){
+        if(cr<0 || cc<0 || cr>= board.length || cc>= board[0].length || board[cr][cc]=='X' || board[cr][cc]=='#'){
             return;
         }
-        // if((cr==0 || cc==0 || cr==board.length-1 || cc==board[0].length-1) && board[cr][cc]=='O' ){
-        //     kardoChange=false;
-        // }
-        board[cr][cc]='#';
+            board[cr][cc]='#';
 
-        Search(cr+1,cc,board);
-        Search(cr-1,cc,board);
-        Search(cr,cc-1,board);
-        Search(cr,cc+1,board);
+            DFS(board,cr+1,cc);
+            DFS(board,cr-1,cc);
+            DFS(board,cr,cc+1);
+            DFS(board,cr,cc-1);
+        
     }
 }
