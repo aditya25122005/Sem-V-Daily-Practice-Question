1b752a1b82b35e90e6e17d008105624d0df21227
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-25 19:56:56 +0530

    Time: 4 ms (98.56%) | Memory: 47.7 MB (35.76%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,95 +1,69 @@
 class Solution {
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        List<List<Integer>> ans= new ArrayList<>();
-        int n=heights.length;
-        int m= heights[0].length;
+        List<List<Integer>> ll= new ArrayList<>();
 
+        int n= heights.length;
+        int m= heights[0].length;
+        boolean [][] reachPacific= new boolean[n][m];
+        boolean [][] reachAtlantic = new boolean[n][m];
+
+        int r=0;
+        for(int j=0;j<m;j++){
+            Pacific(heights,reachPacific,r,j);
+        }
+        int c=0;
+        for(r=0;r<n;r++){
+            Pacific(heights,reachPacific,r,c);
+        }
+
+
+        r=n-1;
+        for(int j=0;j<m;j++){
+            Atlantic(heights,reachAtlantic,r,j);
+        }
+        c=m-1;
+        for(r=0;r<n;r++){
+            Atlantic(heights,reachAtlantic,r,c);
+        }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(i==0 && j==m-1){
-                    ans.add(Arrays.asList(i,j));
-                }
-                else if(i==n-1 && j==0){
-                    ans.add(Arrays.asList(i,j));
-                }
-                else{
-                    boolean [][] visited= new boolean[n][m];
-                    boolean P= reachPacific(heights,i,j,visited);
-                    
-                    visited= new boolean[n][m];
-                    boolean A= reachAtlantic(heights,i,j,visited);
-                    if(P && A){
-                        ans.add(Arrays.asList(i,j));
-                    }
+                if(reachPacific[i][j] && reachAtlantic[i][j]){
+                    ll.add(Arrays.asList(i,j));
                 }
             }
         }
-        return ans;
-    }
-    public static boolean reachPacific(int[][] grid, int cr, int cc, boolean [][] visited){
-        
-
-        if(cr==0 || cc==0){
-            return true;
-        }
-        if(cc>=grid[0].length || cr>= grid.length || cr<0 || cc<0 || visited[cr][cc]){
-            return false;
-        }
-        visited[cr][cc]= true;
-        boolean first=false;
-        boolean second=false;
-        boolean third=false;
-        boolean fourth=false;
-        if( cr-1>=0 && grid[cr-1][cc]<= grid[cr][cc]){
-            first=reachPacific(grid,cr-1,cc,visited);
-        }
-        if( cc-1>=0 && grid[cr][cc-1]<= grid[cr][cc]){
-            second=reachPacific(grid,cr,cc-1,visited);
-        }
-        if( cc+1<grid[0].length && grid[cr][cc+1]<= grid[cr][cc]){
-            third= reachPacific(grid,cr,cc+1,visited);
-        }
-        if(cr+1<grid.length && grid[cr+1][cc]<= grid[cr][cc]){
-            fourth = reachPacific(grid,cr+1,cc,visited);
-        }
-
-        return first || second || third || fourth;
-    }
-    public static boolean reachAtlantic(int[][] grid, int cr, int cc, boolean [][] visited){
-       
-
-        int n=grid.length;
-        int m= grid[0].length;
-        
-        if(cr==n-1 || cc==m-1){
-            return true;
-        }
-
-        if(cc>=grid[0].length || cr>= grid.length || cr<0 || cc<0 || visited[cr][cc]){
-            return false;
-        }
-        visited[cr][cc]= true;
-        boolean first=false;
-        boolean second=false;
-        boolean third=false;
-        boolean fourth=false;
-        if(cr+1<grid.length && grid[cr+1][cc]<= grid[cr][cc]){
-            first = reachAtlantic(grid,cr+1,cc,visited);
-        }
-        if(cc+1<grid[0].length && grid[cr][cc+1]<= grid[cr][cc]){
-            second = reachAtlantic(grid,cr,cc+1,visited);
-        }
-
-        if(cr-1>=0 &&grid[cr-1][cc]<= grid[cr][cc]){
-            third=reachAtlantic(grid,cr-1,cc,visited);
-        }
-        if(cc-1>=0 && grid[cr][cc-1]<= grid[cr][cc]){
-            fourth=reachAtlantic(grid,cr,cc-1,visited);
-        }
-
-
-        return first || second || third || fourth;
+    return ll;
 
     }
+    public static void Pacific(int [][] grid, boolean[][] canReach, int cr, int cc){
+        int [] r={-1,1,0,0};
+        int [] c={0,0,1,-1};
+        if(canReach[cr][cc]){
+            return;
+        } 
+        canReach[cr][cc]=true;
+        for(int k=0;k<r.length;k++){
+            int nr= cr+r[k];
+            int nc= cc+c[k];
+            if(nr>=0 && nr<grid.length && nc>=0 && nc<grid[0].length && grid[nr][nc]>=grid[cr][cc]){
+                Pacific(grid,canReach,nr,nc);
+            }
+        }
+    }
 
+    public static void Atlantic(int [][] grid, boolean[][] canReach, int cr, int cc){
+        int [] r={-1,1,0,0};
+        int [] c={0,0,1,-1};
+        if(canReach[cr][cc]){
+            return;
+        } 
+        canReach[cr][cc]=true;
+        for(int k=0;k<r.length;k++){
+            int nr= cr+r[k];
+            int nc= cc+c[k];
+            if(nr>=0 && nr<grid.length && nc>=0 && nc<grid[0].length && grid[nr][nc]>=grid[cr][cc]){
+                Atlantic(grid,canReach,nr,nc);
+            }
+        }
+    }
 }
