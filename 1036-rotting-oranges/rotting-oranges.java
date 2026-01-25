93f4440b1607434f639f5a4c8a18da61981b6dfe
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-25 16:48:01 +0530

    Time: 2 ms (90.81%) | Memory: 44.2 MB (65.10%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,45 +2,44 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int n= grid.length;
         int m= grid[0].length;
-        int fresh=0;
-
         Queue<int[]> q= new LinkedList<>();
+        int ans=0;
+        int [] r={-1,1,0,0};
+        int [] c={0,0,1,-1};
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                    fresh++;
-                }
-                else if(grid[i][j]==2){
+                if(grid[i][j]==2){
                     q.add(new int[]{i,j,0});
                 }
             }
         }
-        if(fresh==0){
-            return 0;
-        }
-        int [][] dir= {{0,1},{0,-1},{1,0},{-1,0}};
-        int maxTime=0;
-        while(!q.isEmpty()){
-            int[] A= q.poll();
-            int r=A[0];
-            int c=A[1];
-            int t=A[2];
-            maxTime=Math.max(maxTime,t);
+                while(!q.isEmpty()){
+                    int [] A= q.poll();
+                    int cr= A[0];
+                    int cc= A[1];
+                    int time= A[2];
+                    ans=Math.max(ans,time);
 
-            for(int [] d:dir){
-                int nr=r+d[0];
-                int nc=c+d[1];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1){
-                    grid[nr][nc]=2;
-                    q.add(new int[]{nr,nc,t+1});
-                    fresh--;
+                    for(int k=0;k<r.length;k++){
+                        int nr= cr+ r[k];
+                        int nc= cc+ c[k];
+                        if(nr>=0 && nc>=0 && nr<n && nc<m && grid[nr][nc]==1){
+                            grid[nr][nc]=2;// make it unfresh
+                            q.add(new int[]{nr,nc,time+1});
+                        }
+                    }
+
+                }
+      
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    return -1;
                 }
             }
         }
-        if(fresh>0){
-            return -1;
-        }
-        return maxTime;
+        return ans;
 
     }
 }
