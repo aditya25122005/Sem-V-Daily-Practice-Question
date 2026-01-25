64724e365a7d7b1917c0119cb1c44c774a98574b
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-25 14:51:42 +0530

    Time: 3 ms (30.27%) | Memory: 46.5 MB (77.14%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,27 +1,38 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
+        int n= grid.length;
+        int m= grid[0].length;
+        Queue<int[]> q= new LinkedList<>();
+        int [] r={-1,1,0,0};
+        int [] c={0,0,-1,1};
         int ans=0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
+        int curr=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
-                    count=0;
-                    Solve(grid,i,j);
-                    ans=Math.max(ans,count);
+                    curr=1;
+                    q.add(new int[]{i,j});
+                    grid[i][j]=0;
+                    while(!q.isEmpty()){
+                        int [] A= q.poll();
+                        int cr= A[0];
+                        int cc= A[1];
+                        for(int k=0;k<r.length;k++){
+                            int nr= cr+r[k];
+                            int nc= cc+c[k];
+                            if(nr>=0 && nc>=0 && nr<n && nc<m && grid[nr][nc]==1){
+                                q.add(new int[]{nr,nc});
+                                curr++;
+                                grid[nr][nc]=0;
+                            }
+                        }
+                    }
+                    ans=Math.max(ans,curr);
+                    
+
                 }
             }
         }
         return ans;
-    }
-    static int count;
-    public static void Solve(int[][] grid, int cr, int cc){
-        if(cr<0 || cr>=grid.length || cc<0 || cc>=grid[0].length || grid[cr][cc]==0){
-            return;
-        }
-        count++;
-        grid[cr][cc]=0;
-        Solve(grid,cr+1,cc);
-        Solve(grid,cr-1,cc);
-        Solve(grid,cr,cc-1);
-        Solve(grid,cr,cc+1);
     }
 }
