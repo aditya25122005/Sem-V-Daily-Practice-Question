80559bf075353f893b852142aa916b793e2ff6ad
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-26 00:30:53 +0530

    Time: 14 ms (77.49%) | Memory: 49.4 MB (42.79%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,15 +1,14 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int n=mat.length;
+        int n= mat.length;
         int m= mat[0].length;
-        int [][] ans= new int[n][m];
-
+        int [][] ans= new int[n][m];// --> jaha pehle se 0 hai vaha 0 distance
         Queue<int[]> q= new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
                     ans[i][j]=0;
-                    q.add(new int[]{i,j});
+                    q.add(new int[]{i,j,0});
                 }
                 else{
                     ans[i][j]=-1;
@@ -19,22 +18,22 @@ class Solution {
         int [] r={-1,1,0,0};
         int [] c={0,0,1,-1};
         while(!q.isEmpty()){
-            int [] rm=q.poll();
-            int row= rm[0];
-            int col= rm[1];
+            int [] A= q.poll();
+            int cr= A[0];
+            int cc= A[1];
+            int dis= A[2];
 
-            for(int i=0;i<r.length;i++){
-                int nr=row+r[i];
-                int nc=col+c[i];
-
-                if(nr>=0 && nc>=0 && nr<n && nc<m && ans[nr][nc]==-1){
-                    ans[nr][nc]=ans[row][col]+1;
-                    q.add(new int[]{nr,nc});
+            for(int k=0;k<r.length;k++){
+                int nr= cr+ r[k];
+                int nc= cc+ c[k];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && ans[nr][nc]==-1){
+                    q.add(new int[]{nr,nc,dis+1});
+                    ans[nr][nc]=dis+1;
                 }
             }
+
+
         }
         return ans;
-        
     }
-
 }
