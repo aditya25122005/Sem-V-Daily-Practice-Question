dff0303d45a9492c39b59ae238d0d1ff1450f424
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-26 00:07:11 +0530

    Time: 1482 ms (5.00%) | Memory: 49.4 MB (31.57%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,45 +1,42 @@
 class Solution {
     public int ladderLength(String begin, String end, List<String> wordList) {
-        if(!wordList.contains(end)) return 0;
         Queue<String> q= new LinkedList<>();
         HashSet<String> visited= new HashSet<>();
-        q.add(begin);
         int count=1;
+        q.add(begin);
         while(!q.isEmpty()){
-            int size=q.size();
-            for(int s=0;s<size;s++){
+            // ek level par kitne contenders hai -> Jinka 1 character different hai 1 sath pop
+            int size= q.size();
+            for(int i=0;i<size;i++){
                 String rm= q.poll();
-
-                if(visited.contains(rm)){
-                    continue;
-                }
+                if(visited.contains(rm)) continue;
                 visited.add(rm);
+
                 if(rm.equals(end)){
                     return count;
                 }
-                for(String s2:wordList){
-                    if(isValid(rm,s2) && !visited.contains(s2)){
-                        q.add(s2);
+            
+                for(String str:wordList){
+                    if(!visited.contains(str) && canBe(str,rm)){
+                        q.add(str);
                     }
                 }
-
             }
             count++;
-
         }
         return 0;
-    }
-    public static boolean isValid(String s1, String s2){
-        boolean NotEqual=false;
-        int count=0;
-        for(int i=0;i<s1.length();i++){
 
-            if(s1.charAt(i)!=s2.charAt(i)){
-                count++;
-                if(count>1) return false;
+    }
+    public static boolean canBe(String s1, String s2){
+        char[] a1= s1.toCharArray();
+        char[] a2= s2.toCharArray();
+        int diff=0;
+        for(int i=0;i<a1.length;i++){
+            if(a1[i]!=a2[i]){
+                diff++;
+                if(diff>1) return false;
             }
         }
-        if(count==1) return true;
-        return false;
+        return true;
     }
 }
