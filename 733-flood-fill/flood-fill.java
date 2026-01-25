c1ebb93e9c6a4b51c83ed760b49203e3506d9ae0
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-25 15:07:45 +0530

    Time: 2 ms (12.60%) | Memory: 47.2 MB (7.88%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,22 +1,37 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if(image[sr][sc]==color){
-            return image;
-        }
-        // image[sr][sc]=color;
-        Solve(image,sr,sc,color,image[sr][sc]);
-        return image;
-    }
-    public static void Solve(int[][] arr, int cr, int cc, int color, int startColor){
-        if(cr<0 || cc<0 || cr>=arr.length || cc>= arr[0].length || arr[cr][cc]!=startColor){
-            return;
-        }
-        arr[cr][cc]= color;
-    
-        Solve(arr,cr+1,cc,color,startColor);
-        Solve(arr,cr,cc+1,color,startColor);
-        Solve(arr,cr-1,cc,color,startColor);
-        Solve(arr,cr,cc-1,color,startColor);
+        int n= image.length;
+        int m= image[0].length;
+        Queue<int[]> q= new LinkedList<>();
 
+        boolean [][] visited = new boolean[n][m];
+
+        int [] r = {-1,1,0,0};
+        int [] c = {0,0,1,-1};
+        
+
+        q.add(new int[]{sr,sc,image[sr][sc]});
+            while(!q.isEmpty()){
+                int [] A= q.poll();
+                int cr= A[0];
+                int cc= A[1];
+                int old= A[2];
+                visited[cr][cc]=true;
+             
+
+                for(int k=0;k<r.length;k++){
+                    int nr= cr+r[k];
+                    int nc= cc+c[k];
+                    if(nr>=0 && nc>=0 && nr<n && nc<m && image[nr][nc]==old && !visited[nr][nc]){
+                        q.add(new int[]{nr,nc,old});
+                        visited[nr][nc] = true;
+                        image[nr][nc]=color;
+                    }
+                }
+
+
+            }
+        image[sr][sc]=color;
+        return image;
     }
 }
