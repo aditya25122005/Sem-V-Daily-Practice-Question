ad6eba72dc33ddb880a7e451aa8a40e7060b5d51
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-25 23:44:03 +0530

    Time: 16 ms (61.16%) | Memory: 48 MB (23.64%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,37 +1,36 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
-        int []r={-1,1,0,0,-1,-1,1,1};
-        int []c={0,0,-1,1,-1,1,-1,1};
-        if(grid[n-1][m-1]==1 || grid[0][0]==1) return -1;
-        boolean[][] visited = new boolean[n][m];
+        int n= grid.length;
+        int m= grid[0].length;
+
+        if(grid[0][0]==1 || grid[n-1][m-1]==1) return -1;
+
+        int [] r={-1,1,0,0,-1,-1,1,1};
+        int [] c={0,0,1,-1,-1,1,-1,1};
+        int ans=Integer.MAX_VALUE;
+        boolean [][] visited= new boolean[n][m];
+
         Queue<int[]> q= new LinkedList<>();
-        q.add(new int[] {0,0,1});
+        q.add(new int[]{0,0,1});
+        visited[0][0]= true;
         while(!q.isEmpty()){
-            int []A=q.poll();
-            if(visited[A[0]][A[1]]) {
-                continue;
+            int [] A= q.poll();
+            int cr= A[0];
+            int cc= A[1];
+            int dist= A[2];
+            if(cr==n-1 && cc==m-1){
+                ans= Math.min(ans,dist);
             }
-            visited[A[0]][A[1]] = true;
-
-            if(A[0]==n-1 && A[1]==m-1){
-                return A[2];
-            }
-
-            for(int i=0;i<8;i++){
-                int nr=A[0]+r[i];
-                int nc=A[1]+c[i];
-                
-                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==0){
-                    if(!visited[nr][nc]){
-                    q.add(new int[]{nr,nc,A[2]+1});
-                    }
+            for(int k=0;k<r.length;k++){
+                int nr= cr+r[k];
+                int nc= cc+c[k];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==0 && visited[nr][nc]==false){
+                    visited[nr][nc]=true;
+                    q.add(new int[]{nr,nc,dist+1});
                 }
-                
             }
         }
-        return -1;
-
+        return ans==Integer.MAX_VALUE? -1:ans;
+    
     }
 }
