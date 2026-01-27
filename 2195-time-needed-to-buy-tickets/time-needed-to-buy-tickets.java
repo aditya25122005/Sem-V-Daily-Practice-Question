9b99486aae6c7b30dc0ac362dc6ffd06dae146ca
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-27 14:43:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (78.33%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -1,26 +1,33 @@
 class Solution {
     public int timeRequiredToBuy(int[] arr, int k) {
-
-        Queue<int[]> q= new LinkedList<>();
-        for(int i=0;i<arr.length;i++){
-            q.add(new int[]{arr[i],i});
+        int ans=0;
+        for(int i=0;i<=k;i++){
+            ans+= Math.min(arr[i],arr[k]);
         }
-        int count=0;
-        while(!q.isEmpty()){
-            int [] rm= q.poll();
-            // if(rm[1]==k){
-                count++;
-            // }
+        for(int i=k+1;i<arr.length;i++){
+            ans+= Math.min(arr[i],arr[k]-1); // we dont need to make elements 0 that are ahead of k
+        }
+        return ans;
+        // Queue<int[]> q= new LinkedList<>();
+        // for(int i=0;i<arr.length;i++){
+        //     q.add(new int[]{arr[i],i});
+        // }
+        // int count=0;
+        // while(!q.isEmpty()){
+        //     int [] rm= q.poll();
+        //     // if(rm[1]==k){
+        //         count++;
+        //     // }
             
-            rm[0]--;
-            if(rm[1]==k && rm[0]==0){
-                break;
-            }
-            if(rm[0]>0){
-                q.add(new int[]{rm[0],rm[1]});
-            }
-        }
-        return count;
+        //     rm[0]--;
+        //     if(rm[1]==k && rm[0]==0){
+        //         break;
+        //     }
+        //     if(rm[0]>0){
+        //         q.add(new int[]{rm[0],rm[1]});
+        //     }
+        // }
+        // return count;
 
     }
 }
