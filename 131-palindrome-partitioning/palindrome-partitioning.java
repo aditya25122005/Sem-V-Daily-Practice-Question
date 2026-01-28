7e942a19233fa041d025040609c51884097bcb2c
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-29 00:39:14 +0530

    Time: 8 ms (73.55%) | Memory: 65.3 MB (50.30%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,31 +1,39 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> ll= new ArrayList<>();
-        List<String> inner= new ArrayList<>();
-        Solve(s,0,ll,inner);
-        return ll;
+        List<List<String>> ans= new ArrayList<>();
+        List<String> ll= new ArrayList<>();
+        Solve(s,0,ans,ll);
+        return ans;
+
+
     }
-    public static void Solve(String s, int idx, List<List<String>> ll, List<String> inner){
+    public static void Solve(String s, int idx,  List<List<String>> ans, List<String> ll){
         if(idx==s.length()){
-            ll.add(new ArrayList<>(inner));
+            ans.add(new ArrayList<>(ll));
             return;
         }
-        for(int cut=idx+1;cut<=s.length();cut++){
-            if(isPalindrome(s,idx,cut-1)){
-                inner.add(s.substring(idx,cut));
-                Solve(s,cut,ll,inner);
-                inner.remove(inner.size()-1);
+
+        for(int cut=idx+1;cut<= s.length(); cut++){
+            if(isPalindrome(s,idx,cut)){
+                ll.add(s.substring(idx,cut));
+                Solve(s,cut,ans,ll);
+
+                ll.remove(ll.size()-1);
             }
+            // else{
+            //     Solve(s,idx+1,ans,ll);
+            // }
         }
     }
     public static boolean isPalindrome(String s, int i, int j){
-        while(i<j){
-            if(s.charAt(i)!=s.charAt(j)){
+        while(i<j-1){
+            if(s.charAt(i)!= s.charAt(j-1)){
                 return false;
-
             }
-            i++;
-            j--;
+            else{
+                i++;
+                j--;
+            }
         }
         return true;
     }
