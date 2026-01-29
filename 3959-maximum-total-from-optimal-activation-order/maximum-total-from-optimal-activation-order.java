b6ffa90539d45fa5d5c83012d58fefb63d69f07b
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-29 22:10:26 +0530

    Time: 139 ms (28.18%) | Memory: 153.7 MB (80.00%) - LeetSync

---
+++ 3959-maximum-total-from-optimal-activation-order/maximum-total-from-optimal-activation-order.java
@@ -0,0 +1,40 @@
+class Solution {
+    public long maxTotal(int[] value, int[] limit) {
+        int n= value.length;
+        int [][] arr = new int[n][2];
+        for(int i=0;i<n;i++){
+            arr[i][0]=limit[i];
+            arr[i][1]=value[i];
+        } 
+        Arrays.sort(arr,(a,b)->{
+            if(a[0]!=b[0]){
+                return a[0]-b[0];
+            }
+            return b[1]-a[1];
+        });
+        int global=0;
+        PriorityQueue<Integer> pq= new PriorityQueue<>();
+
+        long ans=0;
+        for(int i=0;i<arr.length;i++){
+            int lim= arr[i][0];
+            int v= arr[i][1];
+            if(pq.size()<lim && lim>global){
+                ans+=(long)v;
+                pq.add(lim);
+
+                int x= pq.size();
+                global= Math.max(global,x);
+
+                while(!pq.isEmpty() && pq.peek()<=x){
+                    pq.poll();
+                }
+            }
+           
+           
+        }
+
+
+        return ans;
+    }
+}
