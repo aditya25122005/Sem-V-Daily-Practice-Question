c3d0a066cc35e7835fdf848223a3b8c85ec5d6f1
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-29 18:47:29 +0530

    Time: 1 ms (99.45%) | Memory: 49.1 MB (11.39%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,6 +1,6 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if(nums2.length<nums1.length){
+        if(nums2.length< nums1.length){
             return findMedianSortedArrays(nums2,nums1);
         }
         int n= nums1.length;
@@ -13,29 +13,26 @@ class Solution {
             int cut2= (n+m+1)/2 -cut1;
 
 
-            int maxLeftA= cut1==0?Integer.MIN_VALUE: nums1[cut1-1];
-            int minRightA = cut1==n?Integer.MAX_VALUE: nums1[cut1];
-            int maxLeftB = cut2==0? Integer.MIN_VALUE: nums2[cut2-1];
-            int minRightB = cut2==m? Integer.MAX_VALUE: nums2[cut2];
+            int left_Max_A= (cut1==0)?Integer.MIN_VALUE : nums1[cut1-1];
+            int left_Max_B= (cut2==0)?Integer.MIN_VALUE : nums2[cut2-1];
 
-            if(maxLeftA<=minRightB && maxLeftB<= minRightA){
-                // odd elements
-                // Jo merged array banegi usme max element mid par hoga
-                if((m+n)%2==1){
-                    return Math.max(maxLeftA, maxLeftB);
+            int right_Min_A= (cut1==n)? Integer.MAX_VALUE : nums1[cut1];
+            int right_Min_B= (cut2==m)? Integer.MAX_VALUE : nums2[cut2];
+
+            if(left_Max_A <= right_Min_B && left_Max_B <= right_Min_A){
+                if((n+m)%2==1){
+                    return Math.max(left_Max_A,left_Max_B);
                 }
                 else{
-                    return (Math.max(maxLeftA,maxLeftB)+Math.min(minRightA,minRightB))/2.0;
+                    return (Math.max(left_Max_A, left_Max_B)+ Math.min(right_Min_A,right_Min_B))/2.0;
                 }
             }
-            else if(maxLeftA>minRightB){
+            else if(left_Max_A> right_Min_B){
                 hi=cut1-1;
             }
             else{
                 lo=cut1+1;
             }
-
-
         }
         return 0.0;
     }
