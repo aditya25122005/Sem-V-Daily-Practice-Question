4806d6b86f915c12c5b93dee86d260f05ef1d674
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-01-30 22:34:23 +0530

    Time: 489 ms (6.81%) | Memory: 48.7 MB (5.02%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.java
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.java
@@ -1,84 +1,85 @@
 class Solution {
     public long minimumCost(String s, String t, char[] original, char[] changed, int[] cost) {
-       HashMap<Character,HashMap<Character,Integer>> map= new HashMap<>();
-       for(char ch='a';ch<='z';ch++){
+        HashMap<Character, HashMap<Character, Integer>> map = new HashMap<>();
+        for (char ch = 'a'; ch <= 'z'; ch++) {
             map.put(ch, new HashMap<>());
-       }
+        }
 
-       for(int i=0;i<original.length;i++){
-        char ch= original[i];
-        map.get(ch).put(changed[i],
-        Math.min(cost[i], map.get(ch).getOrDefault(changed[i],Integer.MAX_VALUE)));
-       }
+        for (int i = 0; i < original.length; i++) {
+            char ch = original[i];
+            map.get(ch).put(changed[i],
+                    Math.min(cost[i], map.get(ch).getOrDefault(changed[i], Integer.MAX_VALUE)));
+        }
 
-       long[][] dp= new long[26][26];
-       for(long [] A: dp){
-        Arrays.fill(A,-1);
-       }
+        long[][] dp = new long[26][26];
+        for (long[] A : dp) {
+            Arrays.fill(A, -1);
+        }
 
-       long ans=0;
+        long ans = 0;
 
-       for(int i=0;i<s.length();i++){
-        if(s.charAt(i) != t.charAt(i)){
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != t.charAt(i)) {
 
-            int x= s.charAt(i)-'a';
-            int y= t.charAt(i)-'a';
-            if(dp[x][y]!=-1){
-                if(dp[x][y]==Integer.MAX_VALUE){
-                    return -1;
-                }
-                ans+= dp[x][y];
-                continue;
-            }
-            boolean mark=false;
-            PriorityQueue<Pair> pq= new PriorityQueue<>((a,b)->Long.compare(a.cost,b.cost));
-            HashSet<Character> visited = new HashSet<>();
-            pq.add(new Pair(s.charAt(i),0));
-            while(!pq.isEmpty()){
-                // remove
-                Pair rm= pq.poll();
-                char c = rm.ch;
-                long p = rm.cost;
-
-                // ignore
-                if(visited.contains(c)){
+                int x = s.charAt(i) - 'a';
+                int y = t.charAt(i) - 'a';
+                if (dp[x][y] != -1) {
+                    if (dp[x][y] == Integer.MAX_VALUE) {
+                        return -1;
+                    }
+                    ans += dp[x][y];
                     continue;
                 }
+                boolean mark = false;
+                PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> Long.compare(a.cost, b.cost));
+                HashSet<Character> visited = new HashSet<>();
+                pq.add(new Pair(s.charAt(i), 0));
+                while (!pq.isEmpty()) {
+                    // remove
+                    Pair rm = pq.poll();
+                    char c = rm.ch;
+                    long p = rm.cost;
 
-                // Mark Visited
-                visited.add(c);
+                    // ignore
+                    if (visited.contains(c)) {
+                        continue;
+                    }
 
-                // Self work
-                if(c==t.charAt(i)){
-                    mark=true;
-                    ans+= p;
-                    dp[x][y] = p;
-                    break;
-                }
+                    // Mark Visited
+                    visited.add(c);
 
-                // Add Unvisited nbrs
-                for(char nbrs: map.get(c).keySet()){
-                    if(!visited.contains(nbrs)){
-                        long price = p+map.get(c).get(nbrs);
-                        pq.add(new Pair(nbrs,price));
+                    // Self work
+                    if (c == t.charAt(i)) {
+                        mark = true;
+                        ans += p;
+                        dp[x][y] = p;
+                        break;
+                    }
+
+                    // Add Unvisited nbrs
+                    for (char nbrs : map.get(c).keySet()) {
+                        if (!visited.contains(nbrs)) {
+                            long price = p + map.get(c).get(nbrs);
+                            pq.add(new Pair(nbrs, price));
+                        }
                     }
                 }
-            }
-            if(!mark){
-                dp[x][y]=Integer.MAX_VALUE;
-                return -1;
+                if (!mark) {
+                    dp[x][y] = Integer.MAX_VALUE;
+                    return -1;
+                }
             }
         }
-       }
-       return ans;
+        return ans;
     }
-    class Pair{
+
+    class Pair {
         char ch;
         long cost;
 
-        public Pair(char ch, long cost){
-            this.ch= ch;
-            this.cost=cost;
+        public Pair(char ch, long cost) {
+            this.ch = ch;
+            this.cost = cost;
         }
     }
 }
