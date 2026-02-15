8eae4c81e27bd48ac376b2954b3b233237af52eb
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-02-15 11:25:01 +0530

    Time: 7 ms (43.54%) | Memory: 47.1 MB (27.07%) - LeetSync

--- 4212-toggle-light-bulbs/toggle-light-bulbs.java
+++ 4212-toggle-light-bulbs/toggle-light-bulbs.java
@@ -1,15 +1,27 @@
 class Solution {
     public List<Integer> toggleLightBulbs(List<Integer> bulbs) {
-        boolean[] status= new boolean[101];
-        for(int bulb:bulbs){
-            status[bulb] = !status[bulb];
+        HashMap<Integer,Integer> map= new HashMap<>();
+        for(int num: bulbs){
+            map.put(num,map.getOrDefault(num,0)+1);
         }
-        HashSet<Integer> hh= new HashSet<>(bulbs);
         List<Integer> ans= new ArrayList<>();
-        for(int bulb:hh){
-            if(status[bulb]) ans.add(bulb);
+        for(int key: map.keySet()){
+            if(map.get(key)%2==1){
+                ans.add(key);
+            }
         }
         Collections.sort(ans);
         return ans;
+        // boolean[] status= new boolean[101];
+        // for(int bulb:bulbs){
+        //     status[bulb] = !status[bulb];
+        // }
+        // HashSet<Integer> hh= new HashSet<>(bulbs);
+        // List<Integer> ans= new ArrayList<>();
+        // for(int bulb:hh){
+        //     if(status[bulb]) ans.add(bulb);
+        // }
+        // Collections.sort(ans);
+        // return ans;
     }
 }
