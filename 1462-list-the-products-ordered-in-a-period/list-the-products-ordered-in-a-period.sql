d127fff404204cbe567763053ef35b3189804c89
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-02-15 18:45:35 +0530

    Time: 726 ms (40.74%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+SELECT p.product_name , SUM(o.unit) as unit
+FROM Products p
+LEFT JOIN 
+Orders o
+ON p.product_id = o.product_id
+WHERE YEAR(o.order_date)='2020' AND MONTH(o.order_date)='02' 
+GROUP BY p.product_id
+HAVING unit>=100;
+
+
