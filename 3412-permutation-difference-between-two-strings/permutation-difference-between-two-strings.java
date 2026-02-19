c3c4f6bfe3f9c4d5edb62baa737a766f51bd123d
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-02-19 16:20:08 +0530

    Time: 2 ms (46.33%) | Memory: 43.7 MB (94.26%) - LeetSync

--- 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.java
+++ 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.java
@@ -1,14 +1,21 @@
 class Solution {
     public int findPermutationDifference(String s, String t) {
-        HashMap<Character,Integer> map1= new HashMap<>();
-        HashMap<Character,Integer> map2= new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            map1.put(s.charAt(i),i);
-            map2.put(t.charAt(i),i);
-        }
+        // HashMap<Character,Integer> map1= new HashMap<>();
+        // HashMap<Character,Integer> map2= new HashMap<>();
+        // for(int i=0;i<s.length();i++){
+        //     map1.put(s.charAt(i),i);
+        //     map2.put(t.charAt(i),i);
+        // }
+        // int ans=0;
+        // for(char ch: s.toCharArray()){
+        //     ans+= Math.abs(map1.get(ch)-map2.get(ch));
+        // }
+        // return ans;
         int ans=0;
-        for(char ch: s.toCharArray()){
-            ans+= Math.abs(map1.get(ch)-map2.get(ch));
+        for(char ch:s.toCharArray()){
+            int a=s.indexOf(ch);
+            int b=t.indexOf(ch);
+            ans+= Math.abs(a-b);
         }
         return ans;
        
