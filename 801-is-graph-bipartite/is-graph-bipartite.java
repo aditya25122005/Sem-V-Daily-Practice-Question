c9536a504ff533914fa527e890dce42a58b93ce7
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-02-23 16:16:07 +0530

    Time: 13 ms (6.83%) | Memory: 46.6 MB (99.54%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,46 +1,48 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        return isPartite(graph);
-    }
-    public  boolean isPartite(int[][] graph){
-        HashMap<Integer,Integer> visited= new HashMap<>();
-        Queue<BipartitePair> q= new LinkedList<>();
+        
+        HashMap<Integer,List<Integer>> map = new HashMap<>();
         for(int i=0;i<graph.length;i++){
-            if(visited.containsKey(i)){
-                continue;
+            map.put(i, new ArrayList<>());
+        }
+        for(int i=0;i<graph.length;i++){
+            for(int j=0;j<graph[i].length;j++){
+                map.get(i).add(graph[i][j]);
             }
-            q.add(new BipartitePair(i,0));
-            while(!q.isEmpty()){
-                BipartitePair rp=q.poll();
-                int dis=rp.dis;
-                int vtx=rp.vtx;
-
-                if(visited.containsKey(vtx)){
-                    if(visited.get(vtx)!=dis){
-                        return false;
+        }
+        HashMap<Integer,Integer> visited= new HashMap<>();
+        Queue<Pair> q= new LinkedList<>();
+        for(int src : map.keySet()){
+            if(visited.containsKey(src)) continue;
+            if(!visited.containsKey(src)){
+                // visited.put(src,0);
+                q.add(new Pair(src,0));
+                while(!q.isEmpty()){
+                    Pair rm= q.poll();
+                    if(visited.containsKey(rm.vtx)){
+                        if(visited.get(rm.vtx)!= rm.dis) return false;
+                        continue;
                     }
-                    continue;
-                }
-
-                visited.put(vtx,dis);
-
-                for(int nbrs:graph[vtx]){
-                    if(!visited.containsKey(nbrs)){
-                        q.add(new BipartitePair(nbrs,dis+1));
+                    visited.put(rm.vtx,rm.dis);
+                    for(int nbrs: map.get(rm.vtx)){
+                        if(!visited.containsKey(nbrs)){
+                            q.add(new Pair(nbrs, rm.dis+1));
+                        }
                     }
+
+                    
                 }
             }
         }
         return true;
+    }
+    class Pair{
+        int vtx;
+        int dis;
 
+        public Pair(int vtx, int dis){
+            this.vtx=vtx;
+            this.dis=dis;
+        }
     }
-    class BipartitePair{
-    int vtx;
-    int dis;
-    public BipartitePair(int vtx,int dis){
-        this.vtx=vtx;
-        this.dis=dis;
-    }
-}
-    
 }
