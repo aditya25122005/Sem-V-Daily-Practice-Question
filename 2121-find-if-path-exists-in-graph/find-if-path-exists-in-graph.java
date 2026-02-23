e9fe5c5cac914eab98ad1ebb9fdf25d37de471fd
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-02-23 15:25:02 +0530

    Time: 178 ms (21.50%) | Memory: 252.5 MB (90.82%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,42 +1,30 @@
 class Solution {
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
-        if(source==destination){
-            return true;
-        }
+    public boolean validPath(int n, int[][] edges, int src, int des) {
         HashMap<Integer,List<Integer>> map= new HashMap<>();
         for(int i=0;i<n;i++){
             map.put(i,new ArrayList<>());
         }
-        for(int i=0;i<edges.length;i++){
-            int a= edges[i][0];
-            int b= edges[i][1];
-            map.get(a).add(b);
-            map.get(b).add(a);
+        for(int [] E: edges){
+            int u= E[0];
+            int v= E[1];
+            map.get(u).add(v);
+            map.get(v).add(u);
         }
-        
-        HashSet<Integer> visited= new HashSet<>();
         Queue<Integer> q= new LinkedList<>();
-        q.add(source);
+        q.add(src);
+        HashSet<Integer> visited= new HashSet<>();
         while(!q.isEmpty()){
-            // Remove
             int rm= q.poll();
-            // Ignore
             if(visited.contains(rm)){
                 continue;
             }
-            // Mark Visited
             visited.add(rm);
-            // Self Work
-            if(rm==destination){
-                return true;
-            }
-            // Add Unvisited nbrs
-            for(int nbrs:map.get(rm)){
+            if(rm==des) return true;
+            for(int nbrs : map.get(rm)){
                 if(!visited.contains(nbrs)){
                     q.add(nbrs);
                 }
             }
-            
         }
         return false;
     }
