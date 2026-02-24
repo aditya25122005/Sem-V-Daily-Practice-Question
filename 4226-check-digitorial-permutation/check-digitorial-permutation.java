7dda6176ca62e2a8a9d3f870b6281f91ad8084e6
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-02-24 15:25:00 +0530

    Time: 3 ms (46.20%) | Memory: 43.7 MB (23.04%) - LeetSync

--- 4226-check-digitorial-permutation/check-digitorial-permutation.java
+++ 4226-check-digitorial-permutation/check-digitorial-permutation.java
@@ -1,25 +1,19 @@
 class Solution {
     public boolean isDigitorialPermutation(int n) {
-        int [] fact= {1,1,2,6,24,120,720,5040,40320,362880};
-        int num=n;
-        int sum=0;
-        while(n!=0){
-            int d= n%10;
-            sum+= fact[d];
-            n/=10;
-        }
-        int [] a= new int[10];
-        int [] b= new int[10];
-        for(char ch: String.valueOf(sum).toCharArray()){
-            a[ch-'0']++;
-        }
-        for(char ch: String.valueOf(num).toCharArray()){
-            b[ch-'0']++;
-        }
-        return Arrays.equals(a,b);
+          int [] fact = {1,1,2,6,24,120,720,5040,40320,362880};
 
+          int num=n;
+          int sum=0;
+          while(num!=0){
+            int rem= num%10;
+            sum+= fact[rem];
+            num/=10;
+          }
 
-
-
+          char [] a= String.valueOf(sum).toCharArray();
+          char [] b= String.valueOf(n).toCharArray();
+          Arrays.sort(a);
+          Arrays.sort(b);
+          return Arrays.equals(a,b);
     }
 }
