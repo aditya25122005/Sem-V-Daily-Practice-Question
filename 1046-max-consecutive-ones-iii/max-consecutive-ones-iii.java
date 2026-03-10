1d89899d4a996eee51a49217e7399a4232def2c1
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-03-10 10:46:48 +0530

    Time: 4 ms (47.43%) | Memory: 52.2 MB (71.84%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,60 +1,42 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        // return Solve(nums,k);
-        return Solve2(nums,k);
-    }
-    public static int Solve(int[] nums, int k){
+        int used=0;
         int si=0;
         int ei=0;
-        int curr=0;
         int ans=0;
-        int used=0;
+        int curr=0;
         while(ei<nums.length){
             if(nums[ei]==1){
                 curr++;
-            }else if(nums[ei]==0){
+                ans=Math.max(ans,curr);
+            }
+            else{
                 if(used<k){
                     curr++;
                     used++;
+                    
                 }
-
                 else{
-                while(used==k){
-                    if(nums[si]==0){
-                        used--;
+                    ans=Math.max(ans,curr);
+                    while(used>=k){
+                        if(nums[si]==0){
+                            used--;
+                            
+                        }
+                        curr--; // Decrement even if it is 1 or 0
+                        si++;
                     }
-                    si++;
-                    curr--; 
+                    // include current element in window
+                    used++;
+                    curr++;
                 }
-                curr++;
-                used++;
-            } 
             }
             ans=Math.max(ans,curr);
             ei++;
-       }
-       return ans;
-    }
-    public static int Solve2(int[] nums, int k){// maintain zero count 
-        int zero=0;
-        int si=0;
-        int ei=0;
-        int ans=0;
-        while(ei<nums.length){
-            if(nums[ei]==0){
-                zero++;
-            }
-            while(zero>k){
-                if(nums[si]==0){
-                    zero--;
-                }
-                si++;
-            }
-            
-            ans=Math.max(ans,ei-si+1);
-            ei++;
+
+
         }
         return ans;
-
+        
     }
 }
