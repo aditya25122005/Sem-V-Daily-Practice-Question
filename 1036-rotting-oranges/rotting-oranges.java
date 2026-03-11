f9b94d7db1cd46fd388ccb99ad8a1d4808836412
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-03-11 16:07:01 +0530

    Time: 2 ms (90.93%) | Memory: 44.3 MB (38.09%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,45 +1,53 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
+        
         int n= grid.length;
         int m= grid[0].length;
+        boolean [][] visited= new boolean [n][m];
+
         Queue<int[]> q= new LinkedList<>();
-        int ans=0;
-        int [] r={-1,1,0,0};
-        int [] c={0,0,1,-1};
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    q.add(new int[]{i,j,0});
+                    q.add(new int []{i,j,0});
+                    visited[i][j]=true;
                 }
             }
         }
-                while(!q.isEmpty()){
-                    int [] A= q.poll();
-                    int cr= A[0];
-                    int cc= A[1];
-                    int time= A[2];
-                    ans=Math.max(ans,time);
+        // int time=0;
+        int ans=0;
+        int [] r={-1,1,0,0};
+        int [] c={0,0,-1,1};
+        while(!q.isEmpty()){
+            int [] rm = q.poll();
+            int cr= rm[0];
+            int cc= rm[1];
+            int time= rm[2];
+            boolean fresh=false;
+            for(int i=0;i<r.length;i++){
+                int nr= cr+r[i];
+                int nc= cc+c[i];
+                int nt= time+1;
+                if(nr>=0 && nc>=0 && nr<n && nc<m && !visited[nr][nc] && grid[nr][nc]==1){
+                    fresh=true;
+                    visited[nr][nc]=true;
+                    grid[nr][nc]=2;
+                    ans=Math.max(ans,nt);
 
-                    for(int k=0;k<r.length;k++){
-                        int nr= cr+ r[k];
-                        int nc= cc+ c[k];
-                        if(nr>=0 && nc>=0 && nr<n && nc<m && grid[nr][nc]==1){
-                            grid[nr][nc]=2;// make it unfresh
-                            q.add(new int[]{nr,nc,time+1});
-                        }
-                    }
-
+                    q.add(new int[]{nr,nc,nt});
                 }
-      
+            }
+            // if(fresh) time++;
 
+        }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                    return -1;
-                }
+                if(grid[i][j]==1) return -1;
             }
         }
         return ans;
+
 
     }
 }
