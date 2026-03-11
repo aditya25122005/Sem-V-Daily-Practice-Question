8cac3adf49cec9d7551f535ffb9895d65a6d3f83
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-03-11 15:23:15 +0530

    Time: 185 ms (19.10%) | Memory: 48.2 MB (10.41%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,29 +1,32 @@
 class Solution {
     public String longestPalindrome(String s) {
-        String ans="";
-        int n= s.length();
-        boolean[][] isPalindrome= new boolean[n][n];
-        for(int len=1;len<=n;len++){
+        int n=s.length();
+        if(s.length()==1) return s;
+        boolean [][] isPalindrome = new boolean [n][n];
+
+        String ans = "";
+
+        for(int len=1;len<=s.length();len++){
             for(int i=0;i+len-1<n;i++){
-                int j= i+len-1;
-                if(j-i<=2 && s.charAt(i)== s.charAt(j)){
+                int j=i+len-1;
+                if(len <=2 && s.charAt(i)==s.charAt(j)){
                     isPalindrome[i][j]=true;
                 }
-                else if(s.charAt(i) != s.charAt(j)){
-                    isPalindrome[i][j]= false;
+                else if(s.charAt(i)!=s.charAt(j)){
+                    isPalindrome[i][j]=false;
                 }
                 else{
-                    isPalindrome[i][j]= isPalindrome[i+1][j-1];
+                    isPalindrome[i][j] = isPalindrome[i+1][j-1]; 
                 }
             }
         }
 
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isPalindrome[i][j]){
-                    if(j-i+1>ans.length()){
-                        ans= s.substring(i,j+1);
-                    }
+            for(int j=i;j<n;j++){
+
+                int len= j-i+1;
+                if(isPalindrome[i][j] && len>=ans.length()){
+                    ans= s.substring(i,j+1);
                 }
             }
         }
