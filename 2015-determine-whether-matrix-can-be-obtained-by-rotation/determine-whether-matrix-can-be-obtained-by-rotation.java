16bdbcb3c478376ef820d360a38bbe3c60468417
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-03-22 13:05:26 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (73.79%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -1,12 +1,9 @@
 class Solution {
     public boolean findRotation(int[][] mat, int[][] target) {
-        boolean ans= check1(mat,target) || check2(mat,target) || check3(mat,target) || check4(mat,target)
-       || check5(mat,target);
+        boolean ans= check1(mat,target) || check2(mat,target) || check3(mat,target) 
+       || check4(mat,target);
         return ans;
     }
-
-
-
     public static boolean check1(int [][] mat, int [][] target){
         int n= mat.length;
         int m= mat[0].length;
@@ -55,22 +52,6 @@ class Solution {
     }
 
     public static boolean check4(int [][] mat, int[][] target){
-        int n= mat.length;
-        int m= mat[0].length;
-        int I=0;
-
-        for(int j=m-1;j>=0;j--){
-            int J=0;
-            for(int i=n-1;i>=0;i--){
-                if(mat[i][j] != target[I][J]) return false;
-                J++;
-            }
-            I++;
-        }
-        return true;
-    }
-
-    public static boolean check5(int [][] mat, int[][] target){
         int n= mat.length;
         int m= mat[0].length;
         int I=0;
