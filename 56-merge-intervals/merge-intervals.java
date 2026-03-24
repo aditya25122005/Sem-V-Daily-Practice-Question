8bff215eddd08b806bd84a74bb44a0c5dc985e75
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-03-24 22:00:36 +0530

    Time: 8 ms (91.07%) | Memory: 48.8 MB (88.12%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,25 +1,32 @@
 class Solution {
-    public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-        int start = intervals[0][0];
-        int end = intervals[0][1];
-        if(intervals.length==1){
-            return intervals;
-        }
-        List<int[]> ans= new ArrayList<>();
-
-        for(int i=1;i<intervals.length;i++){
-            if(intervals[i][0]<=end){
-                end=Math.max(end,intervals[i][1]);
+    public int[][] merge(int[][] nums) {
+        Arrays.sort(nums,(a,b)->Integer.compare(a[0],b[0]));
+        int currStart= nums[0][0];
+        int currEnd= nums[0][1];
+        List<int[]> ll= new ArrayList<>();
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i+1][0]<= currEnd){
+                currEnd= Math.max(currEnd,nums[i+1][1]);
             }
             else{
-                ans.add(new int[]{start,end});
-                start=intervals[i][0];
-                end=intervals[i][1];
+                ll.add(new int[]{currStart,currEnd});
+                currStart= nums[i+1][0];
+                currEnd=nums[i+1][1];
             }
+            // if(i==nums.length-2){
+            //     ll.add(new int[]{currStart,currEnd});
+            // }
+
         }
-        ans.add(new int[]{start,end});
-        return ans.toArray(new int[ans.size()][]);
-        
+        ll.add(new int[]{currStart,currEnd});
+       int [][] arr= new int[ll.size()][2];
+       int idx=0;
+       for(int[] A: ll){
+        arr[idx][0]=A[0];
+        arr[idx][1]=A[1];
+        idx++;
+       }
+       return arr;
+       
     }
 }
