c6ea10c2196879be4397862f203ac2104f8cdc8f
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-03-26 00:26:18 +0530

    Time: 20 ms (20.25%) | Memory: 52.3 MB (6.10%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,5 +1,9 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
+        int [] cost= new int[n+1];
+        Arrays.fill(cost,-1);
+        cost[0]=0;
+        cost[k]=0;
         HashMap<Integer,HashMap<Integer,Integer>> map= new HashMap<>();
         for(int i=1;i<=n;i++){
             map.put(i, new HashMap<>());
@@ -7,35 +11,43 @@ class Solution {
         for(int [] E: times){
             int u= E[0];
             int v= E[1];
-            int cost= E[2];
-            map.get(u).put(v,cost);
+            int time = E[2];
+            map.get(u).put(v,time);
         }
-        List<Integer> ll= new ArrayList<>();
-        PriorityQueue<Pair> pq= new PriorityQueue<>((a,b)-> Integer.compare(a.cost,b.cost));
-        pq.add(new Pair(k,0));
+        PriorityQueue<Pair> pq= new PriorityQueue<>((a,b)->Integer.compare(a.cost,b.cost));
         HashSet<Integer> visited= new HashSet<>();
+
+        pq.add(new Pair(k,0));
         while(!pq.isEmpty()){
             Pair rm= pq.poll();
-            if(visited.contains(rm.vtx)) continue;
+            if(visited.contains(rm.vtx)){
+                continue;
+            }
             visited.add(rm.vtx);
-            ll.add(rm.cost);
             for(int nbrs: map.get(rm.vtx).keySet()){
+                int newCost= rm.cost+map.get(rm.vtx).get(nbrs);
                 if(!visited.contains(nbrs)){
-                    int cost= map.get(rm.vtx).get(nbrs)+ rm.cost;
-                    pq.add(new Pair(nbrs,cost));
+                    if(cost[nbrs]==-1 || cost[nbrs]> newCost){
+                        cost[nbrs]=newCost;
+                        pq.add(new Pair(nbrs,newCost));
+                    }
+                    
                 }
+                // cost[nbrs]=Math.min(cost[nbrs],newCost);
             }
         }
+
+        boolean hasNeg=false;
         int ans=Integer.MIN_VALUE;
-        for(int num: ll){
-            ans= Math.max(num,ans);
+        for(int num: cost){
+            if(num==-1) return -1;
+            ans=Math.max(ans,num);
         }
-        if(ll.size()<n) return -1;
-        return ans==Integer.MIN_VALUE? -1:ans;
+        return ans;
 
 
     }
-    class Pair{
+    static class Pair{
         int vtx;
         int cost;
         public Pair(int vtx, int cost){
