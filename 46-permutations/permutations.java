f8c91a9ef65dd0341f6edcbcdc8d8c635e32ef51
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-03-25 08:42:43 +0530

    Time: 1 ms (85.13%) | Memory: 45.6 MB (40.34%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -2,30 +2,25 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans= new ArrayList<>();
         List<Integer> ll= new ArrayList<>();
-        boolean [] taken= new boolean[nums.length];
-        Solve(nums,ans,ll,taken);
+        boolean [] isTaken = new boolean[nums.length];
+        Solve(nums,isTaken,ll,ans);
         return ans;
+
     }
-    public static void Solve(int[] arr,List<List<Integer>> ans,List<Integer> ll, 
-    boolean [] taken ){
-        if(ll.size()==arr.length){
+    public static void Solve(int [] nums,boolean [] isTaken,List<Integer> ll,List<List<Integer>> ans){
+        if(ll.size()==nums.length){
             ans.add(new ArrayList<>(ll));
             return;
         }
-        
-
-
-        for(int i=0;i<arr.length;i++){
-            if(!taken[i]){
-                ll.add(arr[i]);
-                taken[i]= true;
-                Solve(arr,ans,ll,taken);
-
-                taken[i]= false;
-                ll.remove(ll.size()-1); // backtrack
+        for(int i=0;i<nums.length;i++){
+            if(!isTaken[i]){
+                ll.add(nums[i]);
+                isTaken[i]=true;
+                Solve(nums,isTaken,ll,ans);
+                ll.remove(ll.size()-1);
+                isTaken[i]=false;
             }
+            //Solve(nums,i+1,isTaken,ll,ans);
         }
-       
-        
     }
 }
