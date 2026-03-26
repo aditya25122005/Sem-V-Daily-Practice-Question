b55a05264d642dd678848a90471d6fa34acc03da
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-03-26 22:44:02 +0530

    Time: 23 ms (93.92%) | Memory: 49.1 MB (18.34%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,16 +1,15 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         HashMap<Integer,Integer> map= new HashMap<>();
-        //curr prefix sum kitni baar mila hai wo store karenge
-
-        map.put(0,1); // 0 mila hai 1 baar
-        int curr=0;
+        map.put(0,1);
         int ans=0;
+        int curr=0;
         for(int num: nums){
-            curr+=num;
+            curr+= num;
             if(map.containsKey(curr-k)){
                 ans+= map.get(curr-k);
-            }   
+            }
+
             map.put(curr,map.getOrDefault(curr,0)+1);
         }
         return ans;
