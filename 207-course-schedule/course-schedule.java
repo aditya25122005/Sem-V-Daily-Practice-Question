350dc3de7513e2f7194487cc33d2f05f0c504a1e
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-03-26 09:50:09 +0530

    Time: 9 ms (20.83%) | Memory: 46.9 MB (83.69%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,23 +1,19 @@
 class Solution {
-    public boolean canFinish(int num, int[][] preq) {
-        HashMap<Integer,List<Integer>> graph= new HashMap<>();
-        for(int i=0;i<num;i++){
-            graph.put(i,new ArrayList<>());
+    public boolean canFinish(int numCourses, int[][] pre) {
+        HashMap<Integer,List<Integer>>map= new HashMap<>();
+        for(int i=0;i<numCourses;i++){
+            map.put(i, new ArrayList<>());
+
         }
-        for(int i=0;i<preq.length;i++){
-            int a= preq[i][0];
-            int b= preq[i][1];
-            graph.get(a).add(b);
+        int [] in= new int[numCourses];
+        for(int [] E: pre){
+            int u= E[0];
+            int v= E[1];
+            map.get(v).add(u);
+            in[u]++;
         }
-        int[] in= new int[num];
-        for(int v1:graph.keySet()){
-            for(int v2:graph.get(v1)){
-                in[v2]++;
-            }
-        }
+
         Queue<Integer> q= new LinkedList<>();
-        int c=0;
-        // HashSet<Integer> visited= new HashSet<>();
         for(int i=0;i<in.length;i++){
             if(in[i]==0){
                 q.add(i);
@@ -25,16 +21,21 @@ class Solution {
         }
         while(!q.isEmpty()){
             int rm= q.poll();
-            c++;
-            
-            for(int nbrs:graph.get(rm)){
+
+            for(int nbrs:map.get(rm)){
                 in[nbrs]--;
                 if(in[nbrs]==0){
                     q.add(nbrs);
                 }
             }
-
         }
-        return c==num;
+        boolean Ok=true;
+        for(int num:in){
+            if(num!=0) {
+                Ok=false;
+                break;
+            }
+        }
+        return Ok;
     }
 }
