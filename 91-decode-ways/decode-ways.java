5b469ac214b402ab671f281f0ecc3f1654b33e03
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-03-31 14:58:22 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (12.38%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,36 +1,33 @@
 class Solution {
     public int numDecodings(String s) {
-        Integer [] memo= new Integer[s.length()];
-        if(s.charAt(0)=='0') return 0;
-        return Solve(s,0,memo);
+        Integer[] dp= new Integer[s.length()];
+        return Solve(s,0,dp);
     }
+
     public static int Solve(String s, int idx,Integer[] dp){
         if(idx==s.length()){
             return 1;
         }
         if(dp[idx]!=null) return dp[idx];
-        
-        char ch= s.charAt(idx);
+
         int A=0;
-        if(ch=='0'){
-            return 0;
-        }
-        if(ch!=0){
+        int B=0;
+        int C=0;
+        if(s.charAt(idx)>='1' && s.charAt(idx)<='9'){
             A= Solve(s,idx+1,dp);
         }
-        int B=0;
-        if(ch=='0'){
-            return 0;
-        }
-        if(s.charAt(idx)!='0' && idx<s.length()-1){
-            int num= (s.charAt(idx)-'0')*10 + (s.charAt(idx+1)-'0');
-            if(num>=10 && num<=26){
+        if(s.charAt(idx)=='1'){
+            if(idx+1 < s.length() && s.charAt(idx+1)>='0' && s.charAt(idx+1)<='9'){
                 B= Solve(s,idx+2,dp);
             }
-           
+        }
+        if(s.charAt(idx)=='2'){
+            if(idx+1< s.length() && s.charAt(idx+1)>='0' && s.charAt(idx+1)<='6'){
+                C= Solve(s,idx+2,dp);
+            }
         }
 
-        return dp[idx] = A+B;
+        return dp[idx] = A+B+C;
 
     }
 }
