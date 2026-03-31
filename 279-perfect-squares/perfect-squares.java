c66b20c768ff9a604224d4940eb59b977939378c
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-03-31 15:13:44 +0530

    Time: 673 ms (5.04%) | Memory: 278.2 MB (5.17%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,38 +1,36 @@
 class Solution {
     public int numSquares(int n) {
-        List<Integer> perfectSq= new ArrayList<>();
-        int last=-1;
+        
+        List<Integer> perSq= new ArrayList<>();
         for(int i=1;i*i<=n;i++){
-            perfectSq.add(i*i);
+            perSq.add(i*i);
         }
-        int [][] dp= new int[perfectSq.size()][n+1];
-        for(int [] A:dp) Arrays.fill(A,-1);
-        return Solve(0,perfectSq,n, dp);
+        Integer [][] dp= new Integer[perSq.size()][n+1];
+        return Solve(perSq,0,n,dp);
+
     }
-    public static int Solve(int idx, List<Integer> ll, int n, int[][] dp){
-       
-        if(n==0){
-            return 0;
-        }
-        if(idx>= ll.size()){
+    public static int Solve(List<Integer> ll, int idx,int target,Integer[][] dp){
+        if(idx>=ll.size()){
             return Integer.MAX_VALUE;
         }
-        if(n<0){
+        if(target<0){
+            return Integer.MAX_VALUE;
+        }
+        if(target==0){
             return 0;
         }
-        if(dp[idx][n]!=-1)return dp[idx][n];
+        if(dp[idx][target] != null) return dp[idx][target];
         int A=Integer.MAX_VALUE;
         int B=Integer.MAX_VALUE;
-        // take this
-        if(ll.get(idx)<=n){
-            int next =  Solve(idx,ll,n-ll.get(idx),dp);
-            if(next!=Integer.MAX_VALUE){
-                A=Math.min(A,1+next);
+        if(ll.get(idx)<= target){
+            int next1=Solve(ll,idx, target-ll.get(idx),dp);
+            if(next1!= Integer.MAX_VALUE){
+                A= 1+ next1;
             }
         }
-        // skip this
-        B = Solve(idx+1,ll,n,dp);
+        B= Solve(ll,idx+1,target,dp);
 
-        return dp[idx][n] = Math.min(A,B);
+        return dp[idx][target] = Math.min(A,B);
+
     }
 }
