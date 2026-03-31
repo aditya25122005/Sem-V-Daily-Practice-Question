37a4513035ca7bda7dee1d7e0c51960131724855
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-03-31 14:29:32 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (78.63%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,8 +1,15 @@
 class Solution {
+    
     public int fib(int n) {
+        Integer[] dp= new Integer[n+1];
+        return Solve(n,dp);
+
+    }
+    public static int Solve(int n, Integer[] dp){ 
         if(n==0 || n==1){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+        if(dp[n] != null) return dp[n];
+        return dp[n]= Solve(n-1,dp)+Solve(n-2,dp);
     }
 }
