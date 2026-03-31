98377bd37f25ceb8d490c83a60672ab8bfc65eea
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-03-31 15:16:48 +0530

    Time: 490 ms (5.42%) | Memory: 47.7 MB (10.44%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -5,11 +5,11 @@ class Solution {
         for(int i=1;i*i<=n;i++){
             perSq.add(i*i);
         }
-        Integer [][] dp= new Integer[perSq.size()][n+1];
+        Integer [] dp= new Integer[n+1];
         return Solve(perSq,0,n,dp);
 
     }
-    public static int Solve(List<Integer> ll, int idx,int target,Integer[][] dp){
+    public static int Solve(List<Integer> ll, int idx,int target,Integer[] dp){
         if(idx>=ll.size()){
             return Integer.MAX_VALUE;
         }
@@ -19,7 +19,7 @@ class Solution {
         if(target==0){
             return 0;
         }
-        if(dp[idx][target] != null) return dp[idx][target];
+        if(dp[target] != null) return dp[target];
         int A=Integer.MAX_VALUE;
         int B=Integer.MAX_VALUE;
         if(ll.get(idx)<= target){
@@ -30,7 +30,7 @@ class Solution {
         }
         B= Solve(ll,idx+1,target,dp);
 
-        return dp[idx][target] = Math.min(A,B);
+        return dp[target] = Math.min(A,B);
 
     }
 }
