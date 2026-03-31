6d61f6a44165c593607900692b6ff30e788ac767
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-03-31 15:52:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (37.72%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,25 +1,22 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int [][] dp= new int[m][n];
-        for(int [] A:dp){
-            Arrays.fill(A,-1);
-        }
+        Integer[][] dp= new Integer[m][n];
         return Solve(0,0,m,n,dp);
     }
-    public static int Solve(int cr, int cc, int m, int n, int[][] dp){
-        if(cr<0 || cc<0 || cr>=m || cc>=n){
+
+    public static int Solve(int cr, int cc, int n, int m,Integer[][] dp){
+        if(cr>=n || cc>=m){
             return 0;
         }
-        if(cr==m-1 && cc==n-1){
+        if(cr==n-1 && cc==m-1){
             return 1;
         }
-        if(dp[cr][cc]!=-1) return dp[cr][cc];
-        int ans=0;
-        int [][] dir={{0,1},{1,0}};
-        for(int []D:dir){
-            ans+= Solve(cr+D[0],cc+D[1],m,n,dp);
-        }
+        if(dp[cr][cc] != null)  return dp[cr][cc];
+        int A=0;
+        int B=0;
+        A=Solve(cr+1,cc,n,m,dp);
+        B=Solve(cr,cc+1,n,m,dp);
+        return dp[cr][cc] = A+B;
 
-        return dp[cr][cc]  =ans;
     }
 }
