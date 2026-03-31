b72ba730488f813de678728519eb8099bf13f9a9
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-03-31 14:40:58 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (15.30%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,35 +1,21 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length==1){
-            return nums[0];
-        }
-        int [] dp= new int[nums.length];
-        Arrays.fill(dp,-1);
-        int robOne=Robber(nums,0,dp);
-        Arrays.fill(dp,-1);
-        int robTwo=Robber2(nums,1,dp);
-        return Math.max(robOne,robTwo);
+        if(nums.length==1) return nums[0];
+        int n= nums.length;
+        Integer[][] dp= new Integer[n][n];
+        return Math.max(Solve(nums,0,nums.length-2,dp), Solve(nums,1,nums.length-1,dp));
     }
-    public static int Robber(int[] nums, int idx,int[] dp){
-        if(idx>=nums.length-1){
+    public static int Solve(int[] nums, int idx, int j,Integer[][] dp){
+        if(idx>j){
             return 0;
         }
-        if(dp[idx]!=-1){
-            return dp[idx];
-        }
-        int rob=nums[idx]+Robber(nums,idx+2,dp);
-        int norob=Robber(nums,idx+1,dp);
-        return dp[idx]=Math.max(rob,norob);
+        if(dp[idx][j]!= null) return dp[idx][j];
+        int A= nums[idx]+ Solve(nums,idx+2,j,dp);
+        int B= Solve(nums,idx+1,j,dp);
+
+        return dp[idx][j] = Math.max(A,B);
     }
-    public static int Robber2(int[] nums, int idx, int[] dp){
-        if(idx>=nums.length){
-            return 0;
-        }
-        if(dp[idx]!=-1){
-            return dp[idx];
-        }
-        int rob=nums[idx]+Robber2(nums,idx+2,dp);
-        int norob=Robber2(nums,idx+1,dp);
-        return dp[idx]=Math.max(rob,norob);
-    }
+
+
+    
 }
