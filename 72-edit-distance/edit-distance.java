5724b786d0f118cdf8c3248e37715de48a74b116
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-04-01 22:29:30 +0530

    Time: 3 ms (98.29%) | Memory: 47 MB (77.53%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,31 +1,29 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int [][] dp= new int[word1.length()+1][word2.length()+1];
-        for(int []a :dp){
-            Arrays.fill(a,-1);
-        }
-        return Solve(word1,word2,0,0,dp);
+        Integer[][] dp= new Integer[word1.length()][word2.length()];
+        return Solve(0,0,word1,word2,dp);
     }
-    public static int Solve(String s1, String s2, int i, int j, int[][] dp){
-        if(i==s1.length()){
-            return s2.length()-j;
+    public static int Solve(int i, int j, String word1, String word2,Integer[][] dp){
+        if(i>= word1.length()){
+            return word2.length()-j;
         }
-        if(j==s2.length()){
-            return s1.length()-i;
+        if(j>= word2.length()){
+            return word1.length()-i;
         }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
+        if(dp[i][j] != null) return dp[i][j];
+
+        int A=Integer.MAX_VALUE;
+        int B=Integer.MAX_VALUE;
+        int C=Integer.MAX_VALUE;
+        if(word1.charAt(i)!= word2.charAt(j)){
+            A= 1+ Solve(i+1,j,word1,word2,dp); // Delete
+            B= 1+ Solve(i+1,j+1,word1,word2,dp); // Replace
+            C= 1+ Solve(i,j+1,word1,word2,dp); // Insert
         }
-        
-        int ans=0;
-        if(s1.charAt(i)==s2.charAt(j)){
-            ans=Solve(s1,s2,i+1,j+1,dp);
-        }else{ // FOOD --> MONEY
-            int D= Solve(s1,s2,i+1,j,dp); // OOD -> MONEY
-            int I= Solve(s1,s2,i,j+1,dp); //FOOD -> ONEY
-            int R= Solve(s1,s2,i+1,j+1,dp); // OOD ->ONEY
-            ans=1+Math.min(D,Math.min(I,R));
+        else{
+            A= Solve(i+1,j+1,word1,word2,dp);
         }
-        return dp[i][j]=ans;
+
+        return dp[i][j] = Math.min(A,Math.min(B,C));
     }
 }
