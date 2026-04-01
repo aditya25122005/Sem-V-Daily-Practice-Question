0b167a7f2a7c5903e01000ad05e8a2309e42448e
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-04-01 09:53:27 +0530

    Time: 76 ms (13.91%) | Memory: 128.2 MB (26.09%) - LeetSync

--- 2846-robot-collisions/robot-collisions.java
+++ 2846-robot-collisions/robot-collisions.java
@@ -1,65 +1,69 @@
 class Solution {
     public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
+        int n= healths.length;
         List<Pair> ll= new ArrayList<>();
-        int n=positions.length;
         for(int i=0;i<n;i++){
-            ll.add( new Pair(positions[i], healths[i], directions.charAt(i),i));
+            ll.add(new Pair(positions[i], healths[i], directions.charAt(i), i));
         }
+        Stack<Pair> st= new Stack<>();
         Collections.sort(ll,(a,b)->Integer.compare(a.pos, b.pos));
-        Stack<Pair> st = new Stack<>();
-        for(int i=0;i<ll.size();i++){
-            Pair rm = ll.get(i);
-            int currP = rm.pos;
-            int currH = rm.health;
-            char currDir = rm.dir;
-            if(st.isEmpty() || st.peek().dir=='L' || currDir=='R'){
+
+
+        for(int i=0;i<n;i++){
+            Pair rm= ll.get(i);
+
+            if(st.isEmpty() || st.peek().dir=='L' || ll.get(i).dir=='R'){
                 st.push(rm);
             }
             else{
-                while(!st.isEmpty() && st.peek().dir=='R' && ll.get(i).health> st.peek().health){
+                while(!st.isEmpty() &&  st.peek().dir=='R' && ll.get(i).dir=='L' && ll.get(i).health> st.peek().health){
+                    // do batein ho sakti hai
                     st.peek().health=0;
                     st.pop();
-                    ll.get(i).health--;            //  R <-- L
-                    if(ll.get(i).health==0) continue;
+                    ll.get(i).health--;
                 }
+
+
                 if(!st.isEmpty() && st.peek().dir=='R'){
-                    if(st.peek().health == ll.get(i).health) {
+                    if(st.peek().health== ll.get(i).health){
                         ll.get(i).health=0;
                         st.peek().health=0;
                         st.pop();
                     }
                     else if(st.peek().health> ll.get(i).health){
-                        ll.get(i).health=0;
                         st.peek().health--;
+                        ll.get(i).health=0;
                     }
                 }
                 else{
                     st.push(rm);
-                }
+                } 
             }
         }
+
+
+
         List<Pair> res= new ArrayList<>();
         for(Pair p: ll){
             if(p.health>0){
                 res.add(p);
             }
         }
-        Collections.sort(res,(a,b)->Integer.compare(a.idx,b.idx));
+        Collections.sort(res,(a,b)-> Integer.compare(a.idx,b.idx));
         List<Integer> ans= new ArrayList<>();
-        for(Pair P: res){
-            ans.add(P.health);
+        for(Pair p: res){
+            ans.add(p.health);
         }
         return ans;
     }
-    public static boolean sameDir(char a, char b){
-        return a==b;
-    }
+
     static class Pair{
         int pos;
         int health;
         char dir;
         int idx;
-        public Pair(int pos, int health, char dir,int idx){
+
+        public Pair(int pos, int health, char dir, int idx){
             this.pos=pos;
             this.health=health;
             this.dir=dir;
