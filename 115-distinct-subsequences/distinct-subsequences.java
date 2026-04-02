991cdc8230359a8d5005a4c13e807afe84796132
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-04-02 18:51:39 +0530

    Time: 26 ms (8.53%) | Memory: 58.1 MB (11.61%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,29 +1,24 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int[][] dp= new int[s.length()][t.length()];
-        for(int [] a:dp){
-            Arrays.fill(a,-1);
-        }
-        return DS(s,t,0,0,dp);
+        Integer [][] dp= new Integer[s.length()][t.length()];
+        return Solve(s,t,0,0,dp);
     }
-    public static int DS(String s, String t, int i, int j, int[][] dp){
+    public static int Solve(String s, String t, int i, int j,Integer[][] dp){
         if(j==t.length()){
             return 1;
         }
-        if(i==s.length()){
+        if(i>=s.length()){
             return 0;
         }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int inc=0;
-        int exc=0;
-        if(s.charAt(i)==t.charAt(j)){
-            inc=DS(s,t,i+1,j+1,dp);
+        if(dp[i][j] != null ) return dp[i][j];
+        int A=0;
+        int B=0;
+        if(s.charAt(i)== t.charAt(j)){
+          A=  Solve(s,t,i+1,j+1,dp);
         }
         
-            exc=DS(s,t,i+1,j,dp);
+           B= Solve(s,t,i+1,j,dp);
         
-        return dp[i][j]=inc+exc;
+        return dp[i][j] = A+B;
     }
 }
