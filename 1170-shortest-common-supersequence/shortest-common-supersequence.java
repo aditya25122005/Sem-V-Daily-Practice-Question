535e58e489cbffc1808ddd2f418b8c1a4caf26bb
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-04-02 20:52:10 +0530

    Time: 21 ms (93.70%) | Memory: 73.7 MB (5.01%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.java
@@ -2,7 +2,7 @@ class Solution {
     public String shortestCommonSupersequence(String str1, String str2) {
         Integer[][] dp= new Integer[str1.length()+1][str2.length()+1];
         
-       Solve(str1, str2, 0, 0,dp);
+       //Solve(str1, str2, 0, 0,dp);
         return build(str1,str2, 0,0, dp);
     }
     public static String build(String s1, String s2, int i, int j, Integer[][] dp){
@@ -12,7 +12,7 @@ class Solution {
         if(j>= s2.length()){
             return s1.substring(i);
         }
-        //if(dp[i][j] == null) Solve(s1, s2, i, j, dp);
+        if(dp[i][j] == null) Solve(s1, s2, i, j, dp);
 
         if(s1.charAt(i)== s2.charAt(j)){
             return s1.charAt(i) + build(s1,s2,i+1,j+1,dp);
