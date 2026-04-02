d6ab7c88a0d037c0653dd71b02f74d2c26105f5c
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-04-02 09:06:55 +0530

    Time: 194 ms (14.61%) | Memory: 278.2 MB (15.73%) - LeetSync

---
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int maximumAmount(int[][] coins) {
+        Integer[][][] dp= new Integer[coins.length][coins[0].length][3];
+        return Solve(0,0,coins,2,dp);
+    }
+    public static int Solve(int cr, int cc, int [][] coins, int K,Integer[][][] dp){
+        if(cr>= coins.length || cc>= coins[0].length || K<0){
+            return Integer.MIN_VALUE;
+        }
+    
+        if(cr== coins.length-1 && cc== coins[0].length-1){
+            if(K>0 && coins[cr][cc]<0){
+                return 0;
+            }
+            return coins[cr][cc];
+        }
+        if(dp[cr][cc][K] != null) return dp[cr][cc][K];
+        
+        int ans=Integer.MIN_VALUE;
+
+        if(coins[cr][cc]<0 && K>0){
+            int next1= Solve(cr+1,cc,coins,K-1,dp);
+            if(next1!=Integer.MIN_VALUE){
+                ans=Math.max(ans,next1);
+            }
+            int next2=Solve(cr,cc+1,coins,K-1,dp);
+            if(next2!=Integer.MIN_VALUE){
+                ans=Math.max(ans,next2);
+            }
+            int next3=Solve(cr+1,cc,coins,K,dp);
+            if(next3!=Integer.MIN_VALUE){
+                ans=Math.max(ans,coins[cr][cc] + next3);
+            }
+            int next4=Solve(cr,cc+1,coins,K,dp);
+            if(next4!=Integer.MIN_VALUE){
+                ans=Math.max(ans,coins[cr][cc]+ next4);
+            }
+        }
+        else{
+            int next3=Solve(cr+1,cc,coins,K,dp);
+            if(next3!=Integer.MIN_VALUE){
+                ans=Math.max(ans,coins[cr][cc] + next3);
+            }
+            int next4=Solve(cr,cc+1,coins,K,dp);
+            if(next4!=Integer.MIN_VALUE){
+                ans=Math.max(ans,coins[cr][cc]+ next4);
+            }
+        }
+        return dp[cr][cc][K] = ans;
+    }   
+}
