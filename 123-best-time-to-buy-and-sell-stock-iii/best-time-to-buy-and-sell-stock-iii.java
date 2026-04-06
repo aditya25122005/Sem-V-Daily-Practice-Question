fda06caa022f39625a67e7c6d993c76afcfd4655
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-04-06 16:22:53 +0530

    Time: 147 ms (8.58%) | Memory: 148.2 MB (7.36%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,28 +1,36 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int N= prices.length;
-        int [][][] dp= new int[N+1][3][2];
-        for(int [][] A:dp){
-            for(int []a:A){
-                Arrays.fill(a,-1);
-            }
+
+
+        int[][][] dp= new int[prices.length][3][2];
+        for(int[][] A: dp){
+            for (int[] d: A) Arrays.fill(d,-1);
         }
-        return Solve(prices,0,2,1,dp);
+        return Solve(prices, 0, 0, 0, dp);
+        // 0-> buy
+        // 1 -> sell
 
     }
-    public static int Solve(int[] prices, int idx, int trans, int buy, int[][][] dp){
-        if(trans==0 || idx==prices.length){
+    public static int Solve(int [] prices, int idx, int Ct, int prev, int [][][] dp){
+        
+        if(idx==prices.length && Ct<=2){
             return 0;
         }
-        if(dp[idx][trans][buy]!=-1){
-            return dp[idx][trans][buy];
+        if(dp[idx][Ct][prev] !=-1) return dp[idx][Ct][prev];
+        int A=0;
+        int B=0;
+        int C=0;
+        int D=0;
+        if(prev==0){
+            if(Ct<2)
+            A = - prices[idx] + Solve(prices, idx+1, Ct+1, 1,dp);
+            B = Solve(prices,idx+1,Ct,0,dp);
         }
-        if(buy==1){
-            return dp[idx][trans][buy] = Math.max(-prices[idx]+Solve(prices,idx+1,trans,0,dp),
-            Solve(prices,idx+1,trans,1,dp));
-        }else{
-            return dp[idx][trans][buy] = Math.max(prices[idx]+Solve(prices,idx+1,trans-1,1,dp),
-            Solve(prices,idx+1,trans,0,dp));
+        else{
+            C = prices[idx] + Solve(prices, idx+1, Ct, 0,dp);
+            D = Solve(prices,idx+1,Ct,1,dp);
         }
+
+        return dp[idx][Ct][prev] = Math.max(Math.max(A,B),Math.max(C,D));
     }
 }
