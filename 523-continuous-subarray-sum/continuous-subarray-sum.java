2bf72c612e2ba54c43061d424fa2b041e417377c
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-04-07 13:57:42 +0530

    Time: 23 ms (93.66%) | Memory: 139.5 MB (79.11%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/continuous-subarray-sum.java
@@ -1,21 +1,22 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-        HashMap<Integer,Integer> map= new HashMap<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
+        // 0, -1
+        // 2, 0
+        //
+        if(nums.length<2) return false;
         map.put(0,-1);
-
-        int sum=0;
-
+        int curr = 0;
         for(int i=0;i<nums.length;i++){
-            sum=(sum+ nums[i])%k;
-            if(map.containsKey(sum)){
-                if(i-map.get(sum)>=2){
+            curr = (curr + nums[i])%k;
+            if(map.containsKey(curr)){
+                if(i-map.get(curr)>=2 ){
                     return true;
                 }
-               
-            } 
+            }
             else{
-                    map.put(sum,i);
-                }
+                map.put(curr,i);
+            }
         }
         return false;
     }
