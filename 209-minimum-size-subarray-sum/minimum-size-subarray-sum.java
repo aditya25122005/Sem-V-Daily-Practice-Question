65849ab4235a572aab1cfd3ceaacf111e152d117
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-04-08 22:14:37 +0530

    Time: 1 ms (99.93%) | Memory: 62.2 MB (40.82%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -3,29 +3,18 @@ class Solution {
         int si=0;
         int ei=0;
         int ans=Integer.MAX_VALUE;
-        int n= nums.length;
-        int sum=0;
-
-        while(ei<n){
-            sum+= nums[ei];
-            if(sum>=target){
+        int curr=0;
+        while(ei<nums.length){
+            curr+= nums[ei];
+            
+            while(curr>=target){
                 ans=Math.min(ans,ei-si+1);
-            }
-
-            while(sum>target && si<ei){
-                sum-=nums[si];
+                curr-= nums[si];
                 si++;
-                    if(sum>=target){
-                    ans=Math.min(ans,ei-si+1);
-                    }
             }
             
-            if(sum>=target){
-                ans=Math.min(ans,ei-si+1);
-            }
             ei++;
         }
-        return ans==Integer.MAX_VALUE? 0:ans ;
-
+        return ans==Integer.MAX_VALUE? 0 : ans;
     }
 }
