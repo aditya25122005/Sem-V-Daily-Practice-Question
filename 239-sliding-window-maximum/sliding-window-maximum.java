1a28e239817a3269126ddb91a0ff5428a2969758
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-04-09 15:27:39 +0530

    Time: 83 ms (9.40%) | Memory: 158.1 MB (10.25%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,16 +1,20 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int[] ans = new int[nums.length-k+1];
-        PriorityQueue<int[]> pq= new PriorityQueue<>((a,b)->b[0]-a[0]);
+        int n= nums.length;
+        int [] ans= new int[n-k+1];
+        int j=0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->Integer.compare(b[0],a[0]));
         for(int i=0;i<nums.length;i++){
             pq.add(new int[]{nums[i],i});
 
+            // agr peek wala element out of range hai then poll it
             while(pq.peek()[1]<= i-k){
                 pq.poll();
             }
-            if(pq.size()>=k){
-                ans[i-k+1]= pq.peek()[0];
+            if(i>=k-1){
+                ans[j++] = pq.peek()[0];
             }
+            
         }
         return ans;
     }
