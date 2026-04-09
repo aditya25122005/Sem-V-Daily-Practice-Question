52f0616fe07eeffb13e17bdf76e9bcd392823bd6
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-04-09 11:46:56 +0530

    Time: 39 ms (18.69%) | Memory: 47.4 MB (6.61%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,37 +1,45 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        HashMap<Character,Integer> freq = new HashMap<>();
-        for(char ch:  s1.toCharArray()){
-            freq.put(ch,freq.getOrDefault(ch,0)+1);
+        HashMap<Character, Integer> freq = new HashMap<>();
+        for (char ch : s1.toCharArray()) {
+            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
         }
 
-        HashMap<Character,Integer> map= new HashMap<>();
-        int si=0;
-        int ei=0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        int si = 0;
+        int ei = 0;
         int matched = 0;
-        while(ei< s2.length()){
+        while (ei < s2.length()) {
             char ch = s2.charAt(ei);
-            map.put(ch,map.getOrDefault(ch,0)+1);
-            if(ei-si+1 == s1.length()){
-                if(isOk(freq,map)){
-                    return true;
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+            if(freq.containsKey(ch) && map.get(ch)<= freq.get(ch)){
+                matched++;
+            }
+
+            while (freq.containsKey(ch) && map.get(ch) > freq.get(ch)) {
+                if (freq.containsKey(s2.charAt(si)) && map.get(s2.charAt(si)) <= freq.get(s2.charAt(si))) {
+                    matched--;
                 }
-                map.put(s2.charAt(si), map.get(s2.charAt(si))-1);
-                if(map.get(s2.charAt(si))==0){
+                map.put(s2.charAt(si), map.get(s2.charAt(si)) - 1);
+                if (map.get(s2.charAt(si)) == 0){
                     map.remove(s2.charAt(si));
                 }
                 si++;
             }
+            if (!freq.containsKey(ch)) {
+                map.clear();
+                si = ei + 1;
+                matched = 0;
+            }
+
+            if (ei - si + 1 == s1.length() && matched == s1.length()) {
+                return true;
+            }
             ei++;
+
         }
         return false;
     }
-    public static boolean isOk(HashMap<Character,Integer> freq, HashMap<Character,Integer> map){
-        for(char ch: freq.keySet()){
-            if(!map.containsKey(ch) || map.get(ch)<freq.get(ch)){
-                return false;
-            }
-        }
-        return true;
-    }
+
 }
