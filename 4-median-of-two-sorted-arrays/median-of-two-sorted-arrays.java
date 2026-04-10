fef6ea63c5adee7bc72600f75b94b56e17ffbe14
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-04-10 21:46:57 +0530

    Time: 1 ms (100.00%) | Memory: 48.8 MB (59.51%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,39 +1,49 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        /* 
+            1,2,4,5
+            3,6,7,8,9  
+        */
+        // Apply Binary Search on shorter Array TO Reduce Time Complexity
         if(nums2.length< nums1.length){
             return findMedianSortedArrays(nums2,nums1);
         }
         int n= nums1.length;
-        int m=nums2.length;
+        int m= nums2.length;
+        int half= (n+m+1)/2;
 
-        int lo=0;
-        int hi=n;
+        // How many elements from Array 1 Will be on Left(Apply Binary Search On this)
+
+        int lo=0;// No element from Arr 1 on left;
+        int hi=n; // All elements from Arr1 On left;
+        double ans=0;
         while(lo<=hi){
-            int cut1= lo+(hi-lo)/2;
-            int cut2= (n+m+1)/2 -cut1;
+            int mid1=(lo+(hi-lo)/2);
+            int mid2=half-mid1;
 
+            int l1= mid1==0?Integer.MIN_VALUE : nums1[mid1-1];
+            int l2= mid2==0?Integer.MIN_VALUE : nums2[mid2-1];
 
-            int left_Max_A= (cut1==0)?Integer.MIN_VALUE : nums1[cut1-1];
-            int left_Max_B= (cut2==0)?Integer.MIN_VALUE : nums2[cut2-1];
+            int r1= mid1==n? Integer.MAX_VALUE : nums1[mid1];
+            int r2= mid2==m? Integer.MAX_VALUE : nums2[mid2];
 
-            int right_Min_A= (cut1==n)? Integer.MAX_VALUE : nums1[cut1];
-            int right_Min_B= (cut2==m)? Integer.MAX_VALUE : nums2[cut2];
-
-            if(left_Max_A <= right_Min_B && left_Max_B <= right_Min_A){
-                if((n+m)%2==1){
-                    return Math.max(left_Max_A,left_Max_B);
+            if(l1<=r2 && l2<=r1){
+                if((n+m)%2==0){
+                    return (Math.max(l1, l2) + Math.min(r1, r2))/2.0;
                 }
                 else{
-                    return (Math.max(left_Max_A, left_Max_B)+ Math.min(right_Min_A,right_Min_B))/2.0;
+                    return (double)Math.max(l1, l2);
                 }
             }
-            else if(left_Max_A> right_Min_B){
-                hi=cut1-1;
+            else if(l1>l2){
+                hi=mid1-1;
             }
             else{
-                lo=cut1+1;
+                lo=mid1+1;
             }
         }
         return 0.0;
+
+
     }
 }
