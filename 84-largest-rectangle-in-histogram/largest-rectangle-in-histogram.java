1995744b5330e7eeaab13f14ef7cdbd271782bc4
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-04-14 11:44:25 +0530

    Time: 106 ms (29.64%) | Memory: 76.6 MB (81.46%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,43 +1,45 @@
 class Solution {
-    public int largestRectangleArea(int[] ht) {
-        int n= ht.length;
-
-        int [] nse= new int[n];
-        int [] pse= new int[n];
+    public int largestRectangleArea(int[] heights) {
+        int N= heights.length;
+        if(N==1){
+            return heights[0];
+        }
+        int [] nse = new int[heights.length];
         Stack<Integer> st= new Stack<>();
-        for(int i=n-1;i>=0;i--){
-            while(!st.isEmpty() && ht[st.peek()]>= ht[i]){
+        for(int i=N-1;i>=0;i--){
+            while(!st.isEmpty() && heights[st.peek()]>= heights[i]){
                 st.pop();
             }
             if(st.isEmpty()){
-                nse[i] = n-1;
+                nse[i]=N-1;
             }
             else{
-                nse[i] = st.peek()-1;
+                nse[i]=st.peek()-1;
             }
             st.push(i);
         }
         st.clear();
-        for(int i=0;i<n;i++){
-            while(!st.isEmpty() && ht[st.peek()]>= ht[i]){
+        int [] pse = new int[N];
+        for(int i=0;i<N;i++){
+            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
                 st.pop();
             }
             if(st.isEmpty()){
-                pse[i] = 0;
+                pse[i]=0;
             }
             else{
-                pse[i] = st.peek()+1;
+                pse[i]= st.peek()+1;
             }
             st.push(i);
         }
 
-        int maxArea= Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            int H= ht[i];
-            int W= Math.max(1,nse[i]-pse[i]+1);
-            int a= H*W;
-            maxArea= Math.max(maxArea,a);
+        int ans=Integer.MIN_VALUE;
+        for(int i=0;i<N;i++){
+            int wt = Math.max(1,nse[i]-pse[i]+1);
+            
+            int ht= heights[i];
+            ans=Math.max(ans, wt*ht);
         }
-        return maxArea;
+        return Math.max(0,ans);
     }
 }
