24ccb90282acd2c6fa0cd8a6e6eb9379fb8015fc
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-04-14 14:00:11 +0530

    Time: 63 ms (49.12%) | Memory: 59.1 MB (27.29%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,37 +1,42 @@
-import java.util.*;
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        int mod=1000000007;
-        int n= arr.length;
-        int [] nextSmaller= new int[n];
-        Stack<Integer> st= new Stack<>();
-        for(int i=n-1;i>=0;i--){
+        int N = arr.length;
+        int [] nsi = new int[arr.length];
+        Stack<Integer> st = new Stack<>();
+        for(int i=arr.length-1;i>=0;i--){
             while(!st.isEmpty() && arr[st.peek()]>= arr[i]){
                 st.pop();
             }
             if(st.isEmpty()){
-                nextSmaller[i]=-1;
+                nsi[i]=arr.length;
             }
             else{
-                nextSmaller[i]=st.peek();
+                nsi[i] = st.peek();
             }
             st.push(i);
         }
-        int [] ans= new int[n];
 
-        for(int i=n-1;i>=0;i--){
-            if(nextSmaller[i]==-1){
-                int times = n-i;
-                ans[i] = (times*arr[i])%1000000007;
+        st.clear();
+        int [] psi = new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            while(!st.isEmpty() && arr[st.peek()]>arr[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                psi[i]=-1;
             }
             else{
-                ans[i]+= (ans[nextSmaller[i]] + ((nextSmaller[i]-i)*arr[i])%1000000007)%mod;
+                psi[i]=st.peek();
             }
+            st.push(i);
         }
-        long total=0;
-        for(int v:ans){
-            total+=v;
+
+        long sum=0;
+        for(int i=0;i<arr.length;i++){
+             int next = nsi[i];
+             int prev = psi[i];
+             sum+= ((long)arr[i]*((next-i)*(i-prev)));
         }
-        return (int) (total%mod);
+        return (int)(sum%1000000007);
     }
 }
