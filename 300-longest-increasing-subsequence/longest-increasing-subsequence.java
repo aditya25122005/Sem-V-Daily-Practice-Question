369d80e8e828d905dfe0f3b91ddf2fa01db2a8b4
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-04-16 10:47:13 +0530

    Time: 3 ms (96.05%) | Memory: 45.9 MB (84.45%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,43 +1,35 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        // // 10, 9,2,5,3,7,101,18
-        // 10, 
-        // 9,
-        // 3,5,7,18 -> Len=4
-
-        // // 0, 1,0,3,2,3
-        // 0,1,
-        // 0,2,3,3-> Len=4;
-        int N= nums.length;
-        int [] helper= new int[N];
-        helper[0]=nums[0];
-        int len=1;
-        for(int i=1;i<N;i++){
-            if(nums[i]>helper[len-1]){
-                helper[len]=nums[i];
-                len++;
+        int [] arr =new int [nums.length];
+        arr[0]=nums[0];
+        int ans=1;
+        int last = nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i] > arr[ans-1]){
+                arr[ans] = nums[i];
+                ans++;
             }
             else{
-                int idx= Search(helper,nums[i],len-1);
-                helper[idx] = nums[i];
+                int idx = findIdx(arr,nums[i],ans-1);
+                arr[idx] = nums[i];
             }
         }
-        return len;
-
+        return ans;
     }
-    public static int Search(int [] helper, int item, int hi){
+
+    public static int findIdx(int [] arr,int item, int hi){
         int lo=0;
-        int idx=-1;
+        int ans=-1;
         while(lo<=hi){
-            int mid= lo+(hi-lo)/2;
-            if(helper[mid]<item){
-                idx=mid;
+            int mid = lo+(hi-lo)/2;
+            if(arr[mid]<item){
+                ans = mid;
                 lo=mid+1;
             }
             else{
-                hi=mid-1;
+                 hi=mid-1;
             }
         }
-        return idx+1;
+        return ans+1;
     }
 }
