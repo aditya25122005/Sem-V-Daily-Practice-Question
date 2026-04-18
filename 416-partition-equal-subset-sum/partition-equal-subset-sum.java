28ad904661ea12cbd4649e1671b945f5fe6115a8
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-04-18 19:26:04 +0530

    Time: 106 ms (11.15%) | Memory: 72.8 MB (26.01%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,24 +1,33 @@
 class Solution {
-    static int total;
     public boolean canPartition(int[] nums) {
-        total=0;
+        int n= nums.length;
+       
+        int total = 0;
         for(int num: nums){
             total+= num;
         }
-        System.out.println(total);
-        if((total%2) ==1 ) return false;
-        Boolean [][] dp= new Boolean[nums.length][total/2 +1];
-        return Solve(nums,0,0,dp);
+        if(total%2==1){
+            return false;
+        }
+         Boolean [][] dp= new Boolean[nums.length][total/2+1];
+        return Solve(nums,0,total/2,dp);
     }
-    public static boolean Solve(int [] nums, int idx, int A,Boolean[][] dp){
-        if(A== total/2) return true;
-        if(A> total/2) return false;
-        if(idx>=nums.length) return false;
-        
-        if(dp[idx][A] !=null) return dp[idx][A];
-        boolean first = Solve(nums, idx+1, A+nums[idx],dp);
-        boolean second= Solve(nums,idx+1, A, dp);
+    public static boolean Solve(int [] nums, int idx, int target,Boolean[][] dp){
+        if(target==0){
+            return true;
+        }
+        if(idx>=nums.length){
+            return false;
+        }
+        if(dp[idx][target] !=null) return dp[idx][target];
+        boolean A=false;
+        boolean B=false;
+        if(target>=nums[idx]){
+            A = Solve(nums,idx+1,target-nums[idx],dp);
+        }
+        B = Solve(nums,idx+1,target,dp);
 
-        return dp[idx][A] = first || second;
+        return dp[idx][target] = A || B;
+
     }
 }
