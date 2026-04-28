6543663103b577f0e715359be01bc13d64050c20
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-04-28 19:17:32 +0530

    Time: 16 ms (41.49%) | Memory: 60.7 MB (55.60%) - LeetSync

--- 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.java
+++ 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.java
@@ -1,39 +1,36 @@
 class Solution {
     public long minCost(int[] nums, int[] cost) {
-        int lo=Integer.MAX_VALUE;
-        int hi=Integer.MIN_VALUE;
-        for(int num:nums){
-            lo=Math.min(lo,num);
-            hi=Math.max(hi,num);
+        int lo = Integer.MAX_VALUE;
+        int hi= Integer.MIN_VALUE;
+        for(int num : nums){
+            lo = Math.min(lo,num);
+            hi = Math.max(hi,num);
         }
-        long res=Long.MAX_VALUE;
+        long ans= Long.MAX_VALUE;
         while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
-            long find=Find(nums,cost,mid);
-            res=Math.min(find,res);
-
-            long left=Find(nums,cost,mid-1);
-            long right=Find(nums,cost,mid+1);
+            int mid = lo+(hi-lo)/2;
+            // make every element equal to mid
+            long res = Solve(mid,nums,cost);
+            ans= Math.min(ans,res);
+            long left = Solve(mid-1,nums,cost);
+            long right = Solve(mid+1,nums,cost);
             if(left<right){
                 hi=mid-1;
-            }else{
+            }
+            else if(right<left){
                 lo=mid+1;
             }
-
-        }
-        return res;
-    }
-    public static long Find(int[] nums, int[] cost, int target){
-        long ans=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                ans+=0;
-            }else{
-                ans+= (long)Math.abs(nums[i]-target)*cost[i];
+            else{
+                return ans;
             }
         }
         return ans;
-
-        
+    }
+    public static long Solve(int target, int [] nums, int [] cost){
+        long total = 0;
+        for(int i=0;i<cost.length;i++){
+            total+= Math.abs(target-nums[i])*(long)cost[i];
+        }
+        return total;
     }
 }
