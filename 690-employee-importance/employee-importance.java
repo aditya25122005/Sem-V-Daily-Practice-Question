cf9ac7b2b49c40929c68c4ac5884000c67339193
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-05-02 15:19:48 +0530

    Time: 3 ms (48.97%) | Memory: 48 MB (59.37%) - LeetSync

---
+++ 690-employee-importance/employee-importance.java
@@ -0,0 +1,33 @@
+/*
+// Definition for Employee.
+class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+};
+*/
+
+class Solution {
+    public int getImportance(List<Employee> employees, int id) {
+        HashMap<Integer, Employee> map = new HashMap<>();
+        for(Employee e: employees){
+            map.put(e.id, e);
+        }
+        Queue<Employee> q = new LinkedList<>();
+        int total=0;
+        for(Employee e : employees){
+            if(e.id == id){
+                q.add(e);
+                break;
+            }
+        }
+        while(!q.isEmpty()){
+            Employee rm = q.poll();
+            total+= rm.importance;
+            for(int nbrs : rm.subordinates){
+                q.add(map.get(nbrs));
+            }
+        }
+        return total;
+    }
+}
