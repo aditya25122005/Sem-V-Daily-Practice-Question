b76ba6afabc9a91b09c82627548de1fab0ce0c69
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-05-02 15:02:44 +0530

    Time: 3 ms (91.63%) | Memory: 41.9 MB (93.21%) - LeetSync

--- 804-rotated-digits/rotated-digits.java
+++ 804-rotated-digits/rotated-digits.java
@@ -1,39 +1,33 @@
 class Solution {
     public int rotatedDigits(int n) {
-        HashMap<Integer,Integer> map= new HashMap<>();
-        map.put(0,0);
-        map.put(1,1);
-        map.put(2,5);
-        map.put(3,null);
-        map.put(4,null);
-        map.put(5,2);
-        map.put(6,9);
-        map.put(7,null);
-        map.put(8,8);
-        map.put(9,6);
-        int count=0;
+        int c = 0;
         for(int i=1;i<=n;i++){
-            int num=i;
-            StringBuilder sb= new StringBuilder();
-
-            boolean isGood=true;
-            while(num>0){
-                int digit=num%10;
-                if(map.get(digit)==null){
-                    isGood=false;
-                    break;
-                }
-                else{
-                    sb.append(map.get(digit));
-                }
-                num/=10;
-            }
-
-            if(isGood){
-                 int n2=Integer.parseInt(sb.reverse().toString());
-                 if(n2!=i) count++;
+            if(!inValid(i) && containsValid(i)){
+                c++;
             }
         }
-        return count;
+        return c;
+    }
+    public static boolean inValid(int n){
+        int num=n;
+        while(num!=0){
+            int rem = num%10;
+            if(rem==3 || rem==4 || rem==7){
+                return true;
+            }
+            num/=10;
+        }
+        return false;
+    }
+    public static boolean containsValid(int n){
+        int num=n;
+        while(num!=0){
+            int rem = num%10;
+            if(rem==6 || rem==9 || rem==2 || rem==5){
+                return true;
+            }
+            num/=10;
+        }
+        return false;
     }
 }
