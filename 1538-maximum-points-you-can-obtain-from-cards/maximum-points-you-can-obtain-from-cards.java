73e0d2dccada842349bfac3f396ce7296cc45660
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-05-07 12:34:56 +0530

    Time: 3 ms (40.61%) | Memory: 61.5 MB (85.70%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
@@ -1,38 +1,29 @@
 class Solution {
-    public int maxScore(int[] card, int k) {
-        int sum=0;
-        for(int num: card){
-            sum+= num;
+    public int maxScore(int[] arr, int k) {
+        int n = arr.length;
+        int size = n-k;
+        int total = 0;
+        for(int num: arr){
+            total+= num;
         }
-        int n= card.length;
+        int si=0;
+        int ei=0;
         int curr=0;
-        int ans=Integer.MAX_VALUE;
-        for(int i=0;i<n-k;i++){
-            curr+= card[i];
+        int minSum = Integer.MAX_VALUE;
+        while(ei<n){
+            curr+= arr[ei];
+            if(ei-si+1 == size){
+                minSum = Math.min(minSum,curr);
+            }
+            while(ei-si+1 > size){
+                curr -= arr[si];
+                si++;
+            }
+            if(ei-si+1 == size){
+                minSum = Math.min(minSum,curr);
+            }
+            ei++;
         }
-        int win =n-k;
-        ans=Math.min(ans,curr);
-        for(int i=n-k;i<n;i++){
-            curr-= card[i-win];
-            curr+= card[i];
-            ans=Math.min(ans,curr);
-        }
-        return sum - ans;
-        
-        // return Solve(cardPoints,0,cardPoints.length-1,k);
+        return total- minSum;
     }
-
-    // public static int Solve(int [] arr, int i, int j, int k){
-    //     if(k==0){
-    //         return 0;
-    //     }
-    //     if(i>j){
-    //         return 0;
-    //     }
-     
-    //     int A= arr[i]+ Solve(arr,i+1,j,k-1);
-    //     int B= arr[j]+ Solve(arr,i,j-1,k-1);
-    //     return  Math.max(A,B);
-
-    // }
 }
