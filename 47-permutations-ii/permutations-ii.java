57dd7bf7e99d1dc89b4627a51474287f097403ef
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-05-08 21:20:22 +0530

    Time: 4 ms (28.91%) | Memory: 46.6 MB (75.89%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -1,30 +1,28 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> ans= new ArrayList<>();
-        List<Integer> ll= new ArrayList<>();
-        boolean [] taken = new boolean[nums.length];
         Arrays.sort(nums);
-        Solve(nums,ans,ll,taken);
+        List<Integer> ll = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        boolean [] taken = new boolean[nums.length];
+        Solve(nums,0,ans,ll,taken);
         return ans;
 
-
     }
-    public static void Solve(int[] nums,List<List<Integer>> ans,List<Integer> ll,boolean [] taken){
+    public static void Solve(int [] nums,int idx ,List<List<Integer>> ans,List<Integer> ll, boolean[]taken){
         if(ll.size()==nums.length){
             ans.add(new ArrayList<>(ll));
             return;
         }
-        
         for(int i=0;i<nums.length;i++){
-            if(i>0 && nums[i]==nums[i-1] && taken[i-1]==false) continue; // pick in order
+            if(i>0 && nums[i]==nums[i-1] && taken[i-1]==true) continue;
             if(!taken[i]){
-                taken[i]= true;
+                taken[i] = true;
                 ll.add(nums[i]);
-                Solve(nums,ans,ll,taken);
-
-                taken[i]=false;
+                Solve(nums,idx,ans,ll,taken);
                 ll.remove(ll.size()-1);
+                taken[i]=false;
             }
         }
+        
     }
 }
