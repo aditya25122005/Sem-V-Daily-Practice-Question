b5bafa9910dc9814bd831cd4f2c6cca4866ba466
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-05-14 18:13:46 +0530

    Time: 4 ms (52.95%) | Memory: 45.2 MB (31.81%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.java
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -1,16 +1,26 @@
 class Solution {
     public boolean isGood(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int num: nums){
-            map.put(num, map.getOrDefault(num,0)+1);
-        }
-        int last = nums.length-1;
-        if(!map.containsKey(last) || map.get(last)!=2) return false;
-        for(int i=1;i<last;i++){
-            if(!map.containsKey(i) || map.get(i)!=1){
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // for(int num: nums){
+        //     map.put(num, map.getOrDefault(num,0)+1);
+        // }
+        // int last = nums.length-1;
+        // if(!map.containsKey(last) || map.get(last)!=2) return false;
+        // for(int i=1;i<last;i++){
+        //     if(!map.containsKey(i) || map.get(i)!=1){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        Arrays.sort(nums);
+        int key = 1;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]!=key){
                 return false;
             }
+            key++;
         }
+        if(nums[nums.length-1] != nums.length-1) return false;
         return true;
     }
 }
