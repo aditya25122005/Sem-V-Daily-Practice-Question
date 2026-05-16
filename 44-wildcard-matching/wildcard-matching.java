f5102ed47b64ee8a5f1c1013031358e27bf6b2c9
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-05-16 19:02:01 +0530

    Time: 139 ms (5.28%) | Memory: 64.1 MB (6.12%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,37 +1,37 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        Boolean [][] dp= new Boolean[s.length()][p.length()];
+        Boolean [][] dp = new Boolean[s.length()][p.length()];
         return Solve(s,p,0,0,dp);
     }
-    public static boolean Solve(String s, String p, int i, int j,Boolean[][] dp){
-    //    boolean ans=false;
+    public static boolean Solve(String s, String p,int i, int j, Boolean[][] dp){
         if(i==s.length() && j==p.length()){
             return true;
         }
-        if(i==s.length()){
-            if(allstar(p,j)){
-                return true;
-            }
-            return false;
+        if(i==s.length() && allStar(p,j)){
+            return true;
         }
-        if(j==p.length()){
-            return false;
-        }
+        if(j>=p.length()) return false;
+        if(i>=s.length()) return false;
         if(dp[i][j] != null) return dp[i][j];
-        boolean ans=false;
-        if(s.charAt(i)== p.charAt(j) || p.charAt(j)=='?'){
-            ans = ans ||  Solve(s,p,i+1,j+1,dp);
+        boolean ans = false;
+        if(s.charAt(i)== p.charAt(j)){
+            ans = ans || Solve(s,p,i+1,j+1,dp);
         }
-        if(p.charAt(j)=='*'){
-            ans = ans || Solve(s,p,i,j+1,dp);
-            ans= ans || Solve(s,p,i+1,j,dp);
-            ans= ans ||  Solve(s,p,i+1,j+1,dp);
+        if(s.charAt(i)!=p.charAt(j)){
+            if(p.charAt(j)=='?'){
+                ans = ans || Solve(s,p,i+1,j+1,dp);
+            }
+            else if(p.charAt(j)=='*'){
+                ans = ans || Solve(s,p,i+1,j,dp);
+                ans = ans || Solve(s,p,i+1,j+1,dp);
+                ans = ans || Solve(s,p,i,j+1,dp);
+            }
         }
         return dp[i][j] = ans;
     }
-    public static boolean allstar(String p, int j){
-        for(int i=j;i<p.length();i++){
-            if(p.charAt(i) != '*') return false;
+    public static boolean allStar(String p, int idx){
+        for(int i=idx;i<p.length();i++){
+            if(p.charAt(i)!='*') return false;
         }
         return true;
     }
