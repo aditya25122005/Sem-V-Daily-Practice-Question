4a4f5884d52963cf38e8c49a07d25ae363d2421c
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-05-17 14:33:11 +0530

    Time: 6 ms (3.61%) | Memory: 45.2 MB (5.46%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,109 +1,95 @@
 class Solution {
     public int myAtoi(String s) {
-        if(twoSigns(s)) return 0;
-        // if(signAfterDigits(s)) return 0;
-        
-        
+        if (BadSigns(s))
+            return 0;
         boolean numReaded = false;
 
-        int i=0;
+        int i = 0;
         boolean isNegative = false;
         long res = 0;
-        while(i<s.length()){
-            if(s.charAt(i)==' '){
+        while (i < s.length()) {
+            if (s.charAt(i) == ' ') {
                 i++;
-            }
-            else{
+            } else {
                 break;
             }
         }
-        while(i<s.length()){
-            if(i<s.length() && numReaded && (s.charAt(i)=='+' || s.charAt(i)=='-')){
+        while (i < s.length()) {
+            if (i < s.length() && numReaded && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
                 break;
             }
-            if(s.charAt(i)==' '){
+            if (s.charAt(i) == ' ') {
                 break;
             }
-            
-            while(i<s.length() && s.charAt(i)==' '){
+
+            while (i < s.length() && s.charAt(i) == ' ') {
                 i++;
             }
-            
-            if(i<s.length() && s.charAt(i)=='+'){
+
+            if (i < s.length() && s.charAt(i) == '+') {
                 i++;
-            }
-            else if(i<s.length() && s.charAt(i)=='-'){
+            } else if (i < s.length() && s.charAt(i) == '-') {
                 isNegative = true;
                 i++;
             }
-            if(i<s.length() && s.charAt(i)==' '){
+            if (i < s.length() && s.charAt(i) == ' ') {
                 break;
             }
-            while(i<s.length() && !numReaded &&  s.charAt(i)=='0'){
+            while (i < s.length() && !numReaded && s.charAt(i) == '0') {
                 numReaded = true;
                 i++;
             }
-            if(i<s.length() && !numReaded && (Character.isLetter(s.charAt(i)) || s.charAt(i)=='.' || s.charAt(i)=='-' )){
+            if (i < s.length() && (Character.isLetter(s.charAt(i)) || s.charAt(i) == '.' || s.charAt(i) == '-')) {
                 break;
             }
-            if(i<s.length() && numReaded &&  (Character.isLetter(s.charAt(i)) || s.charAt(i)=='.' || s.charAt(i)=='-' )){
+
+            if (i < s.length() && numReaded && s.charAt(i) == ' ')
                 break;
-            }
-            if(i<s.length() && numReaded && s.charAt(i)==' ') break;
-            if(i<s.length() && s.charAt(i)>='0' && s.charAt(i)<='9'){
-                int val = s.charAt(i)-'0';
-                res = (long)((res * 10) + val);
-                if(res>Integer.MAX_VALUE) {
-                    if(isNegative) return Integer.MIN_VALUE;
-                    else return Integer.MAX_VALUE;
+            if (i < s.length() && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                int val = s.charAt(i) - '0';
+                res = (long) ((res * 10) + val);
+                if (res > Integer.MAX_VALUE) {
+                    if (isNegative)
+                        return Integer.MIN_VALUE;
+                    else
+                        return Integer.MAX_VALUE;
                 }
                 numReaded = true;
             }
             i++;
         }
-        if(String.valueOf(res).length()>10){
-            if(isNegative) return Integer.MIN_VALUE;
-            else{
-                 return Integer.MAX_VALUE;
+        if (String.valueOf(res).length() > 10) {
+            if (isNegative)
+                return Integer.MIN_VALUE;
+            else {
+                return Integer.MAX_VALUE;
             }
         }
-        
-        if(isNegative){
+
+        if (isNegative) {
             res = -res;
         }
         System.out.println(res);
-        if(res<Integer.MIN_VALUE) return Integer.MIN_VALUE;
-        if(res>Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        if (res < Integer.MIN_VALUE)
+            return Integer.MIN_VALUE;
+        if (res > Integer.MAX_VALUE)
+            return Integer.MAX_VALUE;
         return (int) res;
     }
-    public static boolean twoSigns(String s){
+
+    public static boolean BadSigns(String s) {
         int sign = 0;
         int num = 0;
-        for(char ch: s.toCharArray()){
-            if(ch=='+' || ch=='-'){
+        for (char ch : s.toCharArray()) {
+            if (ch == '+' || ch == '-') {
                 sign++;
-                if(num==0 && sign>1) return true;
-            }
-            else if(ch>='0' && ch<='9'){
+                if (num == 0 && sign > 1)
+                    return true;
+            } else if (ch >= '0' && ch <= '9') {
                 num++;
             }
         }
         return false;
     }
-    public static boolean signAfterDigits(String s){
-        int sidx=-1;
-        int numidx=-1;
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(ch=='+' || ch=='-'){
-                if(numidx!=-1) return true;
-                sidx = i;
-            }
-            if(ch>='0' && ch<='9'){
-                numidx=i;
-            }
-        }
-        if(sidx==-1) return false;
-        return false;
-    }
+
 }
