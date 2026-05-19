8cbd16bde2837634a8b047ddb65feaaafc6303c0
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-05-20 04:37:24 +0530

    Time: 20 ms (6.76%) | Memory: 94.2 MB (5.75%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -1,16 +1,12 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        int i=0;
-        int j=0;
-        while(i<nums1.length && j<nums2.length){
-            if(nums1[i]==nums2[j]){
-                return nums1[i];
-            }
-            if(nums1[i]<nums2[j]){
-                i++;
-            }
-            else{
-                j++;
+        HashMap<Integer,Integer>map = new HashMap<>();
+        for(int num:nums2){
+            map.put(num, map.getOrDefault(num,0)+1);
+        }
+        for(int num:nums1){
+            if(map.containsKey(num)){
+                return num;
             }
         }
         return -1;
