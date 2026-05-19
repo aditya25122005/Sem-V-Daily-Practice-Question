787d13e5e86862f5ddd2e91b670048c8686290cf
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-05-20 04:51:53 +0530

    Time: 195 ms (18.25%) | Memory: 48.5 MB (5.16%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,35 +1,35 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int n=s.length();
-        if(s.length()==1) return s;
-        boolean [][] isPalindrome = new boolean [n][n];
-
-        String ans = "";
-
-        for(int len=1;len<=s.length();len++){
-            for(int i=0;i+len-1<n;i++){
+        int n = s.length();
+        boolean [][] isPalindrome = new boolean[n][n];
+        for(int len=1;len<=n;len++){
+            for(int i=0;i<n-len+1;i++){
                 int j=i+len-1;
-                if(len <=2 && s.charAt(i)==s.charAt(j)){
-                    isPalindrome[i][j]=true;
+                if(len==1){
+                    isPalindrome[i][j] = true;
                 }
-                else if(s.charAt(i)!=s.charAt(j)){
-                    isPalindrome[i][j]=false;
+                else if(len==2 && s.charAt(i)==s.charAt(j)){
+                    isPalindrome[i][j] = true;
+                }
+                else if(s.charAt(i) != s.charAt(j)){
+                    isPalindrome[i][j] = false;
                 }
                 else{
-                    isPalindrome[i][j] = isPalindrome[i+1][j-1]; 
+                    isPalindrome[i][j] = isPalindrome[i+1][j-1];
                 }
+
             }
         }
-
+        String ans = "";
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-
-                int len= j-i+1;
-                if(isPalindrome[i][j] && len>=ans.length()){
-                    ans= s.substring(i,j+1);
+                int len = j-i+1;
+                if(isPalindrome[i][j]){
+                    if(len>ans.length()) ans = s.substring(i,j+1);
                 }
             }
         }
         return ans;
+
     }
 }
