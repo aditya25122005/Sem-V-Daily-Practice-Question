fc27798d31b50ce736e5e2d8cb64b95a3c1fb738
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-05-24 21:28:51 +0530

    Time: 22 ms (77.30%) | Memory: 103.1 MB (70.79%) - LeetSync

--- 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.java
+++ 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.java
@@ -1,52 +1,48 @@
 class Solution {
     public int longestCycle(int[] edges) {
-        return FindLongestCycle(edges);
-    }
-    public static int FindLongestCycle(int[] edges){
-        int [] in= new int[edges.length];
+        int [] in = new int [edges.length];
         for(int i=0;i<edges.length;i++){
-            if(edges[i]!=-1){
+            if(edges[i]==-1) continue;
             in[edges[i]]++;
-            }
         }
-        Queue<Integer> q= new LinkedList<>();
-        for(int i=0;i<in.length;i++){
-            if(in[i]==0){
+
+        Queue<Integer> q = new LinkedList<>();
+        boolean [] vis = new boolean [edges.length];
+        for(int i=0;i<edges.length;i++){
+            if(in[i] == 0){
                 q.add(i);
             }
         }
-        boolean [] visited= new boolean[edges.length];
-            
         while(!q.isEmpty()){
-            int rm= q.poll();
-            visited[rm]=true;
-            if(edges[rm]!= -1){
-                in[edges[rm]]--;
-            }
+            int rm = q.poll();
+            vis[rm] = true;
 
-            if(edges[rm]!=-1){
-            if(in[edges[rm]]==0){
-                q.add(edges[rm]);
+            int nbrs = edges[rm];
+            if(nbrs!=-1){
+                in[nbrs]--;
             }
+            if(nbrs!=-1){
+                if(in[nbrs]==0){
+                    q.add(nbrs);
+                }
             }
         }
-        int ans=-1;
 
-        for(int i=0;i<visited.length;i++){
-            if(visited[i]==false){
-                visited[i]=true;
-                int c=1;
-                int nbrs=edges[i];
+
+        int ans = -1;
+        for(int i=0;i<vis.length;i++){
+            if(!vis[i]){
+                vis[i] = true;
+                int c = 1;
+                int nbrs = edges[i];
                 while(nbrs!=i){
                     c++;
-                    visited[nbrs]=true;
-                    nbrs= edges[nbrs];
+                    vis[nbrs] = true;
+                    nbrs = edges[nbrs]; 
                 }
-                ans=Math.max(ans,c);
+                ans = Math.max(ans,c);
             }
-            
         }
         return ans;
     }
-       
 }
