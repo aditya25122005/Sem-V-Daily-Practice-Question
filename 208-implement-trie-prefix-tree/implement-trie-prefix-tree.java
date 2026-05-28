1e959c3a92ed7d9b20abc0bcb97e377ce58d975a
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-05-28 16:32:14 +0530

    Time: 51 ms (5.07%) | Memory: 63.4 MB (10.03%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -5,62 +5,59 @@ class Trie {
         HashMap<Character,Node> child;
         public Node(char ch){
             this.ch=ch;
-            child= new HashMap<>();
+            child = new HashMap<>();
         }
     }
     private Node root;
 
-    public Trie(){
-        root= new Node('*'); 
+    public Trie() {
+        root = new Node('*');
     }
-    // O(Length of Word)
+    
     public void insert(String word) {
-        Node curr= root;
+        Node curr = root;
         for(int i=0;i<word.length();i++){
-            char ch= word.charAt(i);
+            char ch = word.charAt(i);
             if(curr.child.containsKey(ch)){
-                curr= curr.child.get(ch); 
+                curr = curr.child.get(ch);
             }
             else{
-
-                Node nn= new Node(ch);
-                curr.child.put(ch, nn);
-                curr=nn;
+                Node nn = new Node(ch);
+                curr.child.put(ch,nn);
+                curr= nn;
             }
         }
-        curr.isTerminal=true;
+        curr.isTerminal = true;
     }
+    
     public boolean search(String word) {
-        Node curr= root;
+        Node curr = root;
         for(int i=0;i<word.length();i++){
-            char ch= word.charAt(i);
+            char ch = word.charAt(i);
             if(curr.child.containsKey(ch)){
-                curr= curr.child.get(ch); 
+                curr = curr.child.get(ch);
             }
             else{
-
                 return false;
             }
         }
         return curr.isTerminal;
     }
-    public boolean startsWith(String word) {
-        Node curr= root;
-        for(int i=0;i<word.length();i++){
-            char ch= word.charAt(i);
+    
+    public boolean startsWith(String prefix) {
+        Node curr = root;
+        for(int i=0;i<prefix.length();i++){
+            char ch = prefix.charAt(i);
             if(curr.child.containsKey(ch)){
-                curr= curr.child.get(ch); 
+                curr = curr.child.get(ch);
             }
             else{
-
                 return false;
             }
         }
         return true;
     }
-    
 }
-
 
 /**
  * Your Trie object will be instantiated and called as such:
