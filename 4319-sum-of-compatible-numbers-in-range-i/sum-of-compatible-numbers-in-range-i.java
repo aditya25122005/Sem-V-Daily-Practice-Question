779df987aaa9529dc3ee2bc70e865880907dc591
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-06-07 21:41:06 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (64.51%) - LeetSync

--- 4319-sum-of-compatible-numbers-in-range-i/sum-of-compatible-numbers-in-range-i.java
+++ 4319-sum-of-compatible-numbers-in-range-i/sum-of-compatible-numbers-in-range-i.java
@@ -1,7 +1,7 @@
 class Solution {
     public int sumOfGoodIntegers(int n, int k) {
         int lo = 0;
-        int hi = Math.max(n+k,n*k);
+        int hi = Math.max(n+k,2*k);
         int sum = 0;
         for(int i=lo;i<=hi;i++){
             if(Math.abs(n-i)<=k && (n&i)==0){
