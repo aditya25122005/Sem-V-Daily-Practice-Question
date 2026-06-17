93d03ba56c004eb055643a34b910ce0915763b8b
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-06-17 23:50:51 +0530

    Time: 4 ms (13.86%) | Memory: 46.1 MB (10.77%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,33 +1,27 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans= new ArrayList<>();
-        List<Integer> ll= new ArrayList<>();
         Arrays.sort(candidates);
-        Solve(candidates,0,0,target, ans,ll);
-        return ans;
-    }
-    public static void Solve(int [] coins, int idx, int curr, int target,List<List<Integer>> ans,
-    List<Integer> ll){
-        if(idx==coins.length){
-            if(curr==target){
-                ans.add(new ArrayList<>(ll));
-                return;
-            }
-            return;
-        }
-        if(curr==target){
-            ans.add(new ArrayList<>(ll));
-            return;
-        }
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> ll = new ArrayList<>();
+        Solve(candidates,0,target,res,ll);
+        return res;
 
-        if(target-curr >= coins[idx]){
-            ll.add(coins[idx]);
-            Solve(coins,idx,curr+coins[idx],target,ans,ll);
+
+    }
+    public static void Solve(int [] candidates, int idx, int target, List<List<Integer>> res,List<Integer> ll){
+        if(target==0){
+            res.add(new ArrayList<>(ll));
+            return;
+        }
+        if(idx==candidates.length){
+            return;
+        }
+        if(candidates[idx]<=target){
+            ll.add(candidates[idx]);
+            Solve(candidates,idx,target-candidates[idx],res,ll);
             ll.remove(ll.size()-1);
         }
-        while(idx<coins.length-1 && coins[idx]==coins[idx+1]){
-            idx++;
-        }
-        Solve(coins,idx+1,curr,target,ans,ll);
+        while(idx>0 && candidates[idx]==candidates[idx-1]) idx++;
+        Solve(candidates,idx+1,target,res,ll);
     }
 }
