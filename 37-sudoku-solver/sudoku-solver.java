ace8b92ceeaf8352c137db33fbe8562ca87e0982
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-06-20 19:29:17 +0530

    Time: 181 ms (76.71%) | Memory: 42.8 MB (70.74%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -2,42 +2,43 @@ class Solution {
     public void solveSudoku(char[][] board) {
         Solve(board,0,0);
     }
-    public static boolean Solve(char[][]board, int r, int c){
-        if(c==9){
-            c=0;
-            r++;
+    public boolean Solve(char[][] board, int row, int col){
+       
+        if(col==9){
+            row++;
+            col=0;
         }
-        if(r==9){
+        if(row==9){
             return true;
         }
-        if(board[r][c]!='.'){
-            return Solve(board,r,c+1);
+    
+        if(board[row][col]!='.'){
+           return Solve(board,row,col+1);
         }
         else{
-            for(char ch='1';ch<='9';ch++){
-                if(isPossible(board,r,c,ch)){
-                    board[r][c]=ch;
-                    if(Solve(board,r,c+1)) return true;
-                    board[r][c]='.';
+            for(char ch ='1'; ch<='9'; ch++){
+                if(isPossible(board,row,col,ch)){
+                    board[row][col]=ch;
+                    if(Solve(board,row,col+1)) return true;
+                    board[row][col] = '.';
                 }
             }
         }
         return false;
     }
-    public static boolean isPossible(char[][] board, int row, int col,char ch){
-        for(int c=0;c<board[0].length;c++){
+    public static boolean isPossible(char[][] board, int row, int col, char ch){
+        for(int c = 0;c<9;c++){
             if(board[row][c]==ch) return false;
         }
-        for(int r=0;r<board.length;r++){
+        for(int r=0;r<9;r++){
             if(board[r][col]==ch) return false;
         }
-        int r= row-(row%3);
-        int c= col-(col%3);
+
+        int r  = row - (row%3);
+        int c = col -(col%3);
         for(int i=r;i<r+3;i++){
             for(int j=c;j<c+3;j++){
-                if(board[i][j]==ch){
-                    return false;
-                }
+                if(board[i][j]==ch) return false;
             }
         }
         return true;
