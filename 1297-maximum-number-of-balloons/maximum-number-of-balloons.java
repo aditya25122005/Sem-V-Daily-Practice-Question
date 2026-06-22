88a71f9320d38d08e57d574b631a8f71f6a0c55f
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-06-23 01:12:39 +0530

    Time: 5 ms (57.19%) | Memory: 43.5 MB (61.87%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -7,15 +7,15 @@ class Solution {
             map.put(ch,map.getOrDefault(ch,0)+1);
         }
         if(!map.containsKey('l') || map.get('l')<2) return 0;
-        int ans = Integer.MAX_VALUE;
-        ans = Math.min(ans, map.get('l')/2);
         if(!map.containsKey('o') || map.get('o')<2) return 0;
-        ans = Math.min(ans,map.get('o')/2);
-
         if(!map.containsKey('n')) return 0;
         if(!map.containsKey('b')) return 0;
         if(!map.containsKey('a')) return 0;
-       
+
+        int ans = Integer.MAX_VALUE;
+        ans = Math.min(ans, map.get('l')/2);
+        ans = Math.min(ans,map.get('o')/2);
+
         ans= Math.min(ans,map.get('a'));
         ans= Math.min(ans, map.get('n'));
         ans = Math.min(ans,map.get('b'));
