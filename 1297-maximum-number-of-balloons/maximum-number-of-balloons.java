85cb69fea3ade33e5fa8a362681342654bb7667d
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-06-23 01:10:39 +0530

    Time: 11 ms (34.99%) | Memory: 44.3 MB (8.57%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -1,21 +1,23 @@
 class Solution {
-    public int maxNumberOfBalloons(String s) {
-        String target="balloon";
-        HashMap<Character,Integer> map= new HashMap<>();
-        int ans=Integer.MAX_VALUE;
-        for(char ch: s.toCharArray()){
+    public int maxNumberOfBalloons(String text) {
+        // b a l l o o n
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char ch: text.toCharArray()){
             map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        HashMap<Character,Integer> map2= new HashMap<>();
-        for(char ch:target.toCharArray()){
-            map2.put(ch,map2.getOrDefault(ch,0)+1);
-        }
-        for(char ch:target.toCharArray()){
-            if(!map.containsKey(ch)) return 0;
-            int copy=(int)(Math.floor(map.get(ch)/map2.get(ch)));
-            ans=Math.min(ans,copy);
-        }
+        if(!map.containsKey('l') || map.get('l')<2) return 0;
+        int ans = Integer.MAX_VALUE;
+        ans = Math.min(ans, map.get('l')/2);
+        if(!map.containsKey('o') || map.get('o')<2) return 0;
+        ans = Math.min(ans,map.get('o')/2);
+
+        if(!map.containsKey('n')) return 0;
+        if(!map.containsKey('b')) return 0;
+        if(!map.containsKey('a')) return 0;
        
+        ans= Math.min(ans,map.get('a'));
+        ans= Math.min(ans, map.get('n'));
+        ans = Math.min(ans,map.get('b'));
         return ans;
     }
 }
