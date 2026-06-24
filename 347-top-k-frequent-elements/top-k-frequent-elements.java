1bd34efc2029d00d60985deff0803256b3a594cd
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-06-24 13:46:35 +0530

    Time: 14 ms (70.20%) | Memory: 47.4 MB (90.60%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int num: nums){
             map.put(num, map.getOrDefault(num,0)+1);
@@ -8,11 +9,29 @@ class Solution {
         for(int key: map.keySet()){
             keys.add(key);
         }
-        Collections.sort(keys,(a,b)->Integer.compare(map.get(b), map.get(a)));
-        int [] arr = new int[k];
-        for(int i=0;i<arr.length;i++){
-            arr[i] = keys.get(i);
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->Integer.compare(a.freq,b.freq));
+        for(int key: map.keySet()){
+            int val = key;
+            int freq = map.get(key);
+            pq.add(new Pair(val,freq));
+            if(pq.size()>k) pq.poll();
+        }
+        int[] arr = new int[k];
+        int idx = 0;
+        while(k>0){
+            arr[idx++] = pq.poll().val;
+            k--;
         }
         return arr;
+
+
+    }
+    static class Pair{
+        int val;
+        int freq;
+        public Pair(int val, int freq){
+            this.val=val;
+            this.freq = freq;
+        }
     }
 }
