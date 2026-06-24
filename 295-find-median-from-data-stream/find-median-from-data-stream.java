c88df8a8f9a8418c38860bd67ee72e2eafcdd08f
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-06-24 13:05:40 +0530

    Time: 225 ms (7.69%) | Memory: 111.5 MB (36.70%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -1,32 +1,32 @@
 class MedianFinder {
-    PriorityQueue<Integer> left;
-    PriorityQueue<Integer> right;
+    PriorityQueue<Integer> pq1;
+    PriorityQueue<Integer> pq2;
 
     public MedianFinder() {
-        left= new PriorityQueue<>(Collections.reverseOrder());// we want max element at top
-        right= new PriorityQueue<>();// we want min element at top
+        pq1 = new PriorityQueue<>(Collections.reverseOrder());
+        pq2 = new PriorityQueue<>();
     }
-    
+    //  1  2                      //   3
     public void addNum(int num) {
-        left.add(num);
-        right.add(left.poll());
-        if(right.size()>left.size()){
-            left.add(right.poll());
+        pq1.add(num);
+        pq2.add(pq1.poll());
+        if(pq2.size()> pq1.size()){
+            pq1.add(pq2.poll());
         }
-
-        // first add each element in left
-        // then to balance we transfer from right to left
-
-        // In the end either both PQ will be equal or left will be +1 than right
-        //If equal ans is simple peek() of both /2.0
-        // else ans will be peek of left;
+        
     }
     
     public double findMedian() {
-        if(left.size()==right.size()){
-            return (left.peek()+right.peek())/2.0;
+        int size1 = pq1.size();
+        int size2 = pq2.size();
+        
+
+        if(size1==size2){
+            return (pq1.peek()+pq2.peek())/2.0;
         }
-        return (double) left.peek();
+        else{
+            return pq1.peek()/1.0;
+        }       
     }
 }
 
