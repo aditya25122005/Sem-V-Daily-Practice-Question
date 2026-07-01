c2b0ae925caf3aa4e9d1a8a9718defe18b232c3c
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-07-02 00:35:54 +0530

    Time: 9 ms (19.09%) | Memory: 47 MB (77.51%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,41 +1,34 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] pre) {
-        HashMap<Integer,List<Integer>>map= new HashMap<>();
-        for(int i=0;i<numCourses;i++){
+    public boolean canFinish(int num, int[][] pre) {
+        // b->a
+        int [] in = new int[num];
+        HashMap<Integer,List<Integer>> map = new HashMap<>();
+        for(int i=0;i<num;i++){
             map.put(i, new ArrayList<>());
-
         }
-        int [] in= new int[numCourses];
-        for(int [] E: pre){
-            int u= E[0];
-            int v= E[1];
+        for(int [] A: pre){
+            int u = A[0];
+            int v = A[1];
             map.get(v).add(u);
             in[u]++;
         }
-
         Queue<Integer> q= new LinkedList<>();
         for(int i=0;i<in.length;i++){
-            if(in[i]==0){
+            if(in[i] == 0){
                 q.add(i);
             }
         }
+        int c = 0;
         while(!q.isEmpty()){
-            int rm= q.poll();
-
-            for(int nbrs:map.get(rm)){
+            int rm = q.poll();
+            c++;
+            for(int nbrs: map.get(rm)){
                 in[nbrs]--;
-                if(in[nbrs]==0){
-                    q.add(nbrs);
-                }
+                if(in[nbrs]==0) q.add(nbrs);
             }
         }
-        boolean Ok=true;
-        for(int num:in){
-            if(num!=0) {
-                Ok=false;
-                break;
-            }
-        }
-        return Ok;
+        return c==num;
+
+
     }
 }
