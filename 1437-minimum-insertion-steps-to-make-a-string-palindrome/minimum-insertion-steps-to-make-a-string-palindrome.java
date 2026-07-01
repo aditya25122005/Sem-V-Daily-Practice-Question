8b932e8eda67047ed8a3eb202e77b3a8d6a4c7a0
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-07-01 19:41:50 +0530

    Time: 22 ms (71.07%) | Memory: 47 MB (64.04%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,22 +1,22 @@
 class Solution {
     public int minInsertions(String s) {
-        Integer [][] dp= new Integer[s.length()][s.length()];
+        Integer[][] dp = new Integer[s.length()][s.length()];
         return Solve(s,0,s.length()-1,dp);
     }
-    public static int Solve(String s, int i, int j,Integer [][] dp){
+    public static int Solve(String s, int i , int j,Integer[][] dp ){
         if(i>=j){
             return 0;
         }
-        if(dp[i][j] != null) return dp[i][j];
-        int ans=Integer.MAX_VALUE;
         if(s.charAt(i)==s.charAt(j)){
-            ans= Math.min(ans, Solve(s,i+1,j-1,dp));
+            return Solve(s,i+1,j-1,dp);
         }
+        if(dp[i][j] != null) return dp[i][j];
         else{
-            ans= Math.min(ans, 1+ Solve(s,i+1,j,dp));
-            ans= Math.min(ans, 1+ Solve(s,i,j-1,dp));
+            int A = Integer.MAX_VALUE;
+            int B = Integer.MAX_VALUE;
+            A = 1+Solve(s,i+1,j,dp);
+            B = 1+Solve(s,i,j-1,dp);
+            return dp[i][j] = Math.min(A,B);
         }
-        return dp[i][j] = ans;
-
     }
 }
