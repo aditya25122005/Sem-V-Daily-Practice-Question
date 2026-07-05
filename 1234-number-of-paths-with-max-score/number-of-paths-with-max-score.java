bc59cf42b90baaa48707c3f6df2046d99cf4e06d
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-07-05 19:31:28 +0530

    Time: 1209 ms (5.88%) | Memory: 287.3 MB (7.06%) - LeetSync

--- 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.java
+++ 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.java
@@ -1,80 +1,72 @@
 class Solution {
-    static int mod=1000000007;
+    int n;
+    int m;
+    int k;
+    int mod;
+
     public int[] pathsWithMaxScore(List<String> board) {
-        
-        int []ans= new int[2];
-        int m=board.get(0).length();
-        int n=board.size();
-        mod=1000000007;
-       
-        int[][] dp= new int[n][m];
-        
-        
-        for(int [] A:dp){
-            Arrays.fill(A,-1);
-        }
-        char[][] grid= new char[board.size()][board.get(0).length()];
-        for(int i=0;i<grid.length;i++){
-            String s=board.get(i);
+        n = board.size();
+        m = board.get(0).length();
+        mod = 1000000007;
+
+        char[][] grid = new char[n][m];
+        for(int i=0;i<n;i++){
+            String s = board.get(i);
             for(int j=0;j<m;j++){
-                grid[i][j]=s.charAt(j);
+                grid[i][j] = s.charAt(j);
             }
         }
-        ans[0] = Solve(grid,n-1,m-1,dp);
-        if(ans[0]<0) return new int[]{0,0};
-        int[][][] dp2= new int[n][m][ans[0]+1];
-        for(int i=0;i<dp2.length;i++){
-            for(int []A:dp2[i]){
-                Arrays.fill(A,-1);
-            }
-        }
+
+        Integer[][] dp = new Integer[n][m];
+        int ans = Solve(grid,n-1,m-1,dp);
+        k = ans;
+        if(ans<0) return new int[]{0,0};
+        Integer[][][] dp2 = new Integer[n][m][ans+1];
         
-        ans[1]= numPath(grid,n-1,m-1,ans[0],0,dp2)%mod;
-        return ans;
+        int ans2 = Solve2(grid,n-1,m-1,0,dp2);
+        if(ans2==0){
+            return new int[]{0,0};
+        }
+        return new int[]{ans, ans2};
+
 
     }
-    public static int Solve(char[][] grid, int cr, int cc,int[][]dp){
+    public  int Solve2(char[][] grid, int cr, int cc, int sum,Integer[][][] dp2){
+        if(cr<0 || cc<0 || grid[cr][cc]=='X' || sum>k){
+            return 0;
+        }
+        if(cr==0 && cc==0){
+            if(sum==k){
+                return 1;
+            }
+            return 0;
+        }
+
+        sum+= (cr==n-1 && cc==m-1)? 0: grid[cr][cc]-'0';
+
+        if(dp2[cr][cc][sum] != null) return dp2[cr][cc][sum];
+
+        int A = Solve2(grid,cr-1,cc,sum,dp2);
+        int B = Solve2(grid,cr-1,cc-1,sum,dp2);
+        int C = Solve2(grid,cr,cc-1,sum,dp2);
+
+        return dp2[cr][cc][sum] = (A + B + C)%mod;
+    }
+
+    public int Solve(char[][] grid, int cr, int cc,Integer[][] dp ){
         if(cr<0 || cc<0 || grid[cr][cc]=='X'){
-            return -1000000000;
+            return Integer.MIN_VALUE;
         }
         if(cr==0 && cc==0){
             return 0;
         }
-        if(dp[cr][cc]!=-1) return dp[cr][cc];
-        int val=0;
-        if(grid[cr][cc]!='S'){
-            val=grid[cr][cc]-'0';
-        }
-        else{
-            val=0;
-        }
-        
-        int a=Solve(grid,cr-1,cc-1,dp);
-        int b=Solve(grid,cr,cc-1,dp);
-        int c=Solve(grid,cr-1,cc,dp);
+        if(dp[cr][cc]!=null) return dp[cr][cc];
+        int add = (cr==n-1 && cc==m-1)? 0: grid[cr][cc]-'0';
+        int A = add + Solve(grid,cr-1,cc,dp);
+        int B = add + Solve(grid,cr-1,cc-1,dp);
+        int C = add + Solve(grid,cr,cc-1,dp);
 
-        return dp[cr][cc] =val+ Math.max(a,Math.max(b,c)) ;
-    }
-   
-    public static int numPath(char[][] grid, int cr, int cc, int ans, long curr, int[][][]dp){
-        if(cr<0 || cc<0 || grid[cr][cc]=='X'){
-            return 0;
-        }
-        
-        if(cr==0 && cc==0){
-            if(curr==ans)return 1;
-            return 0;
-        }
-        if(grid[cr][cc]!='S' && grid[cr][cc]!='E'){
-            curr+= grid[cr][cc]-'0';
-        }
-        if(curr>ans) return 0;
-        if(dp[cr][cc][(int)curr]!=-1) return dp[cr][cc][(int)curr];
-        long cnt=0;
-        cnt+=numPath(grid,cr-1,cc-1,ans,curr,dp);
-        cnt+=numPath(grid,cr,cc-1,ans,curr,dp);
-        cnt+=numPath(grid,cr-1,cc,ans,curr,dp);
-        return dp[cr][cc][(int)curr]=(int)cnt %mod;
-
+        return dp[cr][cc] = Math.max(A,Math.max(B,C));
+       
     }
 }
