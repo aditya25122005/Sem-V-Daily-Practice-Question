1c92dc7f90dc5315aedc9e2c5512f2ada2927ad4
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-07-09 10:23:53 +0530

    Time: 7 ms (57.58%) | Memory: 46.9 MB (31.17%) - LeetSync

--- 1273-compare-strings-by-frequency-of-the-smallest-character/compare-strings-by-frequency-of-the-smallest-character.java
+++ 1273-compare-strings-by-frequency-of-the-smallest-character/compare-strings-by-frequency-of-the-smallest-character.java
@@ -2,18 +2,17 @@ class Solution {
     public int[] numSmallerByFrequency(String[] queries, String[] words) {
         int Q = queries.length;
         int N = words.length;
-        int [] qf = new int[Q];
+        int[] qf = new int[Q];
         int idx = 0;
-        for(String word : queries){
-            char [] a = word.toCharArray();
+        for (String word : queries) {
+            char[] a = word.toCharArray();
             Arrays.sort(a);
             char c = a[0];
             int cnt = 1;
-            for(int i=1;i<a.length;i++){
-                if(a[i] == c){
+            for (int i = 1; i < a.length; i++) {
+                if (a[i] == c) {
                     cnt++;
-                }
-                else{
+                } else {
                     break;
                 }
             }
@@ -21,20 +20,17 @@ class Solution {
             idx++;
         }
 
-        //Arrays.sort(qf);
-
-        int [] wf = new int[N];
+        int[] wf = new int[N];
         idx = 0;
-        for(String word: words){
-            char [] a = word.toCharArray();
+        for (String word : words) {
+            char[] a = word.toCharArray();
             Arrays.sort(a);
             char ch = a[0];
             int cnt = 1;
-            for(int i=1;i<a.length;i++){
-                if(a[i] == ch){
+            for (int i = 1; i < a.length; i++) {
+                if (a[i] == ch) {
                     cnt++;
-                }
-                else{
+                } else {
                     break;
                 }
             }
@@ -42,38 +38,33 @@ class Solution {
             idx++;
         }
         Arrays.sort(wf);
-        // [2,3] --> [1,2,3,4]
-        for(int num : qf) System.out.print(num+ " ");
-        System.out.println();
-        for(int num: wf) System.out.print(num + " ");
-        int [] res = new int[Q];
-        idx=0;
 
-        for(int val1 : qf){
-            int ansIdx = Search(wf,val1);
-            if(ansIdx<0 || ansIdx>N) res[idx++]=0;
-            else{
-            res[idx++] = N-ansIdx;
+        int[] res = new int[Q];
+        idx = 0;
+
+        for (int val1 : qf) {
+            int ansIdx = Search(wf, val1);
+            if (ansIdx < 0 || ansIdx > N) {
+                res[idx++] = 0;
+            } else {
+                res[idx++] = N - ansIdx;
             }
         }
         return res;
-
     }
-    public static int Search(int [] wf, int target){
-        int lo = 0;
-        int hi = wf.length-1;
-        int res = wf.length;
-        while(lo<=hi){
-            int mid = lo+(hi-lo)/2;
-            if(wf[mid]>target){
-                res = mid;
-                hi = mid-1;
-            } 
-            
-            else{
-                lo=mid+1;
-            }
 
+    public static int Search(int[] wf, int target) {
+        int lo = 0;
+        int hi = wf.length - 1;
+        int res = wf.length;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (wf[mid] > target) {
+                res = mid;
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
         }
         return res;
     }
