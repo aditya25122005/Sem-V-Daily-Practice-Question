ba126d4d7fb5d6b7002624e01dc495ee7c5511b9
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-07-16 15:54:25 +0530

    Time: 9 ms (4.00%) | Memory: 80.4 MB (5.33%) - LeetSync

---
+++ 1690-maximum-length-of-subarray-with-positive-product/maximum-length-of-subarray-with-positive-product.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int getMaxLen(int[] nums) {
+        List<Integer> ll = new ArrayList<>();
+        int negative = 0;
+        int res = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                ll.add(nums[i]);
+                if(nums[i]<0){
+                    negative++;
+                }
+            }
+            else{
+                if(ll.isEmpty()) continue;
+                if(negative%2==0) res = Math.max(res,ll.size());
+                else{
+                    int fidx = -1;
+                    for(int j=0;j<ll.size();j++){
+                        if(ll.get(j)<0){
+                            fidx = j;
+                            break;
+                        }
+                    }
+                    res = Math.max(res,ll.size()-fidx-1);
+                    int lidx = -1;
+                    for(int j=ll.size()-1;j>=0;j--){
+                        if(ll.get(j)<0){
+                            lidx = j;
+                            break;
+                        }
+                    }
+                    res = Math.max(res,lidx);
+                }
+                negative = 0;
+                ll = new ArrayList<>();
+
+            }
+        }
+        
+                if(negative%2==0) res = Math.max(res,ll.size());
+                else{
+                    int fidx = -1;
+                    for(int j=0;j<ll.size();j++){
+                        if(ll.get(j)<0){
+                            fidx = j;
+                            break;
+                        }
+                    }
+                    res = Math.max(res,ll.size()-fidx-1);
+                    int lidx = -1;
+                    for(int j=ll.size()-1;j>=0;j--){
+                        if(ll.get(j)<0){
+                            lidx = j;
+                            break;
+                        }
+                    }
+                    res = Math.max(res,lidx);
+                }
+                negative = 0;
+                ll = new ArrayList<>();
+        return res;
+    }
+}
