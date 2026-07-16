8c9da60a2c8d53f32fc422e5131b814883f7560b
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-07-16 16:44:20 +0530

    Time: 19 ms (65.85%) | Memory: 47.7 MB (10.34%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,15 +1,75 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
-        long sum = 0;
-        for(int i=0;i<nums.length;i++){
-            int max = Integer.MIN_VALUE;
-            int min = Integer.MAX_VALUE;
-            for(int j=i;j<nums.length;j++){
-                max = Math.max(max,nums[j]);
-                min = Math.min(min,nums[j]);
-                sum+= (max-min);
+        int n = nums.length;
+        int [] ngi = new int[nums.length];
+        int [] pgi = new int[nums.length];
+        int [] nsi = new int[nums.length];
+        int [] psi = new int[nums.length];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && nums[st.peek()]<nums[i]){
+                st.pop();
             }
+            if(st.isEmpty()){
+                ngi[i] = n;
+            }
+            else{
+                ngi[i] = st.peek();
+            }
+            st.push(i);
         }
-        return sum;
+        st.clear();
+
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && nums[st.peek()]>nums[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                nsi[i] = n;
+            }
+            else{
+                nsi[i] = st.peek();
+            }
+            st.push(i);
+        }
+
+        st.clear();
+
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && nums[st.peek()]>=nums[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                psi[i] = -1;
+            }
+            else{
+                psi[i] = st.peek();
+            }
+            st.push(i);
+        }
+        st.clear();
+
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && nums[st.peek()]<=nums[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                pgi[i] = -1;
+            }
+            else{
+                pgi[i] = st.peek();
+            }
+            st.push(i);
+        }
+        long minSum = 0;
+        for(int i=0;i<n;i++){
+            minSum+= (long)nums[i]*(long)(nsi[i]-i)*(long)(i-psi[i]);
+        }
+        long maxSum = 0;
+        for(int i=0;i<n;i++){
+            maxSum+= (long)nums[i]*(long)(ngi[i]-i)*(long)(i-pgi[i]);
+        }
+        return maxSum-minSum;
     }
 }
