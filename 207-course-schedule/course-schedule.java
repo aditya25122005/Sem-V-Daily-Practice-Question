a75027049c3cb943242354525997271afa3088ec
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-07-19 18:27:23 +0530

    Time: 8 ms (25.58%) | Memory: 47.1 MB (65.27%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean canFinish(int num, int[][] pre) {
-        // b->a
-        int [] in = new int[num];
+        // bi --> ai
         HashMap<Integer,List<Integer>> map = new HashMap<>();
+        int [] in = new int[num];
         for(int i=0;i<num;i++){
             map.put(i, new ArrayList<>());
         }
@@ -12,23 +12,23 @@ class Solution {
             map.get(v).add(u);
             in[u]++;
         }
-        Queue<Integer> q= new LinkedList<>();
+        Queue<Integer> q = new LinkedList<>();
         for(int i=0;i<in.length;i++){
-            if(in[i] == 0){
+            if(in[i]==0){
                 q.add(i);
             }
         }
-        int c = 0;
+        int pop = 0;
         while(!q.isEmpty()){
             int rm = q.poll();
-            c++;
+            pop++;
             for(int nbrs: map.get(rm)){
                 in[nbrs]--;
-                if(in[nbrs]==0) q.add(nbrs);
+                if(in[nbrs]==0){
+                    q.add(nbrs);
+                }
             }
         }
-        return c==num;
-
-
+        return pop == num;
     }
 }
