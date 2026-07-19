4deaf96744ef3fa9180e4b08179ba075e07ecaa0
Aditya Maheshwari <144265921+aditya25122005@users.noreply.github.com>
2026-07-19 19:46:14 +0530

    Time: 24 ms (10.53%) | Memory: 50.8 MB (6.63%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,58 +1,41 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        int [] cost= new int[n+1];
-        Arrays.fill(cost,-1);
-        cost[0]=0;
-        cost[k]=0;
-        HashMap<Integer,HashMap<Integer,Integer>> map= new HashMap<>();
+        if(n==1) return 0;
+        HashMap<Integer,HashMap<Integer,Integer>> map = new HashMap<>();
         for(int i=1;i<=n;i++){
             map.put(i, new HashMap<>());
         }
         for(int [] E: times){
-            int u= E[0];
-            int v= E[1];
-            int time = E[2];
-            map.get(u).put(v,time);
+            int u = E[0];
+            int v = E[1];
+            int wt = E[2];
+            map.get(u).put(v,wt);
         }
-        PriorityQueue<Pair> pq= new PriorityQueue<>((a,b)->Integer.compare(a.cost,b.cost));
-        HashSet<Integer> visited= new HashSet<>();
-
+        int ans = 0;
+        HashSet<Integer> vis = new HashSet<>();
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->Integer.compare(a.dis,b.dis));
         pq.add(new Pair(k,0));
         while(!pq.isEmpty()){
-            Pair rm= pq.poll();
-            if(visited.contains(rm.vtx)){
-                continue;
-            }
-            visited.add(rm.vtx);
+            Pair rm = pq.poll();
+            if(vis.contains(rm.vtx)) continue;
+            vis.add(rm.vtx);
+            ans = Math.max(ans,rm.dis);
             for(int nbrs: map.get(rm.vtx).keySet()){
-                int newCost= rm.cost+map.get(rm.vtx).get(nbrs);
-                if(!visited.contains(nbrs)){
-                    if(cost[nbrs]==-1 || cost[nbrs]> newCost){
-                        cost[nbrs]=newCost;
-                        pq.add(new Pair(nbrs,newCost));
-                    }
-                    
+                if(!vis.contains(nbrs)){
+                    int newCost = rm.dis+map.get(rm.vtx).get(nbrs);
+                    pq.add(new Pair(nbrs,newCost));
                 }
-                // cost[nbrs]=Math.min(cost[nbrs],newCost);
             }
         }
-
-        boolean hasNeg=false;
-        int ans=Integer.MIN_VALUE;
-        for(int num: cost){
-            if(num==-1) return -1;
-            ans=Math.max(ans,num);
-        }
+        if(vis.size() !=n) return -1;
         return ans;
-
-
     }
     static class Pair{
         int vtx;
-        int cost;
-        public Pair(int vtx, int cost){
+        int dis;
+        public Pair(int vtx, int dis){
             this.vtx=vtx;
-            this.cost=cost;
+            this.dis = dis;
         }
     }
 }
